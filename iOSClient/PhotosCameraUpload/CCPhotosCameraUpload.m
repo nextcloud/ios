95ec7d67f9171e7ae3f139c0e497bb795c16137d
Rui <rygen@users.noreply.github.com>
2017-01-16 14:48:09 +0100

    Revert commit 6ec6c99

--- iOSClient/PhotosCameraUpload/CCPhotosCameraUpload.m
+++ iOSClient/PhotosCameraUpload/CCPhotosCameraUpload.m
@@ -307,15 +307,11 @@
 
 - (void)emptyDataSetDidTapButton:(UIScrollView *)scrollView
 {    
-    [PHPhotoLibrary requestAuthorization:^(PHAuthorizationStatus status) {
-        
-        // If the user has previously granted or denied photo library access permission, it executes the handler block when called; otherwise, it displays an alert and executes the block only after the user has responded to the alert.
-        CCManageCameraUpload *viewController = [[CCManageCameraUpload alloc] initWithNibName:nil bundle:nil];
+    CCManageCameraUpload *viewController = [[CCManageCameraUpload alloc] initWithNibName:nil bundle:nil];
         UINavigationController *navigationController = [[UINavigationController alloc] initWithRootViewController:viewController];
         
         [navigationController setModalPresentationStyle:UIModalPresentationFullScreen];
         [self presentViewController:navigationController animated:YES completion:nil];
-    }];
 }
 
 #pragma --------------------------------------------------------------------------------------------
