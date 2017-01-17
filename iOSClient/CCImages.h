619db7c121ec0f5affe2fda618131af968576969
Marino Faggiana <ios@nextcloud.com>
2017-01-17 12:30:43 +0100

    remove old images

--- iOSClient/CCImages.h
+++ iOSClient/CCImages.h
@@ -21,14 +21,53 @@
 //  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 //
 
-// Immagini
-
-#ifdef CC
-#define image_launchscreen                      @"cryptocloudlaunchscreen"
-#endif
-
 #ifdef NC
+
 #define image_launchscreen                      @"nextcloudlaunchscreen"
+#define image_activeCameraUpload_en             @"activeCameraUploadNextcloud_en"
+#define image_activeCameraUpload_it             @"activeCameraUploadNextcloud_it"
+#define image_attention                         @"attention"
+#define image_audio                             @"audio"
+#define image_audiocrypto                       @"audiocrypto"
+#define image_bkfull                            @"bkfull"
+#define image_bkempty                           @"bkempty"
+#define image_brandCameraUpload                 @"nextcloudCameraUpload"
+#define image_brandOffline                      @"nextcloudOffline"
+#define image_brandLocal                        @"nextcloudLocal"
+#define image_brandNavigationController         @"nextcloud_navigationcontroller"
+#define image_brandNavigationController1        @"nextcloud_navigationcontroller1"
+#define image_brandNavigationController2        @"nextcloud_navigationcontroller2"
+#define image_brandNavigationController3        @"nextcloud_navigationcontroller3"
+#define image_brandNavigationControllerOffline  @"nextcloud_navigationcontroller_offline"
+#define image_brandSfondoiPad                   @"nextcloud_sfondo_iPad"
+#define image_checked                           @"checkedNextcloud"
+#define image_decryptedSelectedFiles            @"decryptedSelectedFilesNextcloud"
+#define image_deleteSelectedFiles               @"deleteSelectedFilesNextcloud"
+#define image_downloadSelectedFiles             @"downloadSelectedFilesNextcloud"
+#define image_drag                              @"dragNextcloud"
+#define image_encryptedSelectedFiles            @"encryptedSelectedFilesNextcloud"
+#define image_folderphotocamera                 @"folderphotocameraNextcloud"
+#define image_MenuDirectoryOnTop                @"menuDirectoryOnTopNextcloud"
+#define image_MenuGroupByAlphabetic             @"MenuGroupByAlphabeticNextcloud"
+#define image_MenuGroupByDate                   @"MenuGroupByDateNextcloud"
+#define image_MenuGroupByTypeFile               @"MenuGroupByTypeFileNextcloud"
+#define image_MenuOrdeyByDate                   @"MenuOrdeyByDateNextcloud"
+#define image_MenuOrderByFileName               @"MenuOrderByFileNameNextcloud"
+#define image_MenuOrdinamentoDiscendente        @"MenuOrdinamentoDiscendenteNextcloud"
+#define image_MenuOrdinamentoAscendente         @"MenuOrdinamentoAscendenteNextcloud"
+#define image_moveSelectedFiles                 @"moveSelectedFilesNextcloud"
+#define image_quickActionPhotos                 @"quickActionPhotosNextcloud"
+#define image_quickActionUploadClear            @"quickActionUploadClearNextcloud"
+#define image_quickActionUploadEncrypted        @"quickActionUploadEncryptedNextcloud"
+#define image_quickActionUploadOffline          @"quickActionUploadOfflineNextcloud"
+#define image_seleziona                         @"selezionaNextcloud"
+#define image_saveSelectedFiles                 @"saveSelectedFilesNextcloud"
+#define image_settingsMail                      @"settingsMailNextcloud"
+#define image_settingsManagePhotos              @"settingsManagePhotosNextcloud"
+#define image_settingsManagePhotosChange        @"settingsManagePhotosChangeNextcloud"
+#define image_tabBarFile                        @"tabBarNextcloud"
+#define image_thumbs                            @"thumbsNextcloud"
+
 #endif
 
 #define image_actionSheetRename                 @"renameNextcloud"
@@ -40,87 +79,10 @@
 #define image_actionSheetOpenIn                 @"openInNextcloud"
 #define image_actionSheetOffline                @"actionSheetOffline"
 #define image_actionSheetLocal                  @"localStorageNextcloud"
-
-#ifdef CC
-#define image_activeCameraUpload_en             @"activeCameraUpload_en"
-#define image_activeCameraUpload_it             @"activeCameraUpload_it"
-#endif
-
-#ifdef NC
-#define image_activeCameraUpload_en             @"activeCameraUploadNextcloud_en"
-#define image_activeCameraUpload_it             @"activeCameraUploadNextcloud_it"
-#endif
-
-#define image_attention                         @"attention"
-#define image_audio                             @"audio"
-#define image_audiocrypto                       @"audiocrypto"
-
-#define image_bkfull                            @"bkfull"
-#define image_bkempty                           @"bkempty"
-
-#ifdef CC
-
-#define image_brandCameraUpload                 @"cryptocloudCameraUpload"
-#define image_brandOffline                      @"cryptocloudOffline"
-#define image_brandLocal                        @"cryptocloudLocal"
-#define image_brandNavigationController         @"cryptocloud_navigationcontroller"
-#define image_brandNavigationController1        @"cryptocloud_navigationcontroller1"
-#define image_brandNavigationController2        @"cryptocloud_navigationcontroller2"
-#define image_brandNavigationController3        @"cryptocloud_navigationcontroller3"
-#define image_brandNavigationControllerOffline  @"cryptocloud_navigationcontroller_offline"
-#define image_brandSfondoiPad                   @"cryptocloud_sfondo_iPad"
-
-#endif
-
-#ifdef NC
-
-#define image_brandCameraUpload                 @"nextcloudCameraUpload"
-#define image_brandOffline                      @"nextcloudOffline"
-#define image_brandLocal                        @"nextcloudLocal"
-#define image_brandNavigationController         @"nextcloud_navigationcontroller"
-#define image_brandNavigationController1        @"nextcloud_navigationcontroller1"
-#define image_brandNavigationController2        @"nextcloud_navigationcontroller2"
-#define image_brandNavigationController3        @"nextcloud_navigationcontroller3"
-#define image_brandNavigationControllerOffline  @"nextcloud_navigationcontroller_offline"
-#define image_brandSfondoiPad                   @"nextcloud_sfondo_iPad"
-
-#endif
-
-#ifdef CC
-#define image_checked                           @"checked"
-#endif
-
-#ifdef NC
-#define image_checked                           @"checkedNextcloud"
-#endif
-
-#ifdef CC
-#define image_decryptedSelectedFiles            @"decryptedSelectedFiles"
-#define image_deleteSelectedFiles               @"deleteSelectedFiles"
-#define image_downloadSelectedFiles             @"downloadSelectedFiles"
-#endif
-
-#ifdef NC
-#define image_decryptedSelectedFiles            @"decryptedSelectedFilesNextcloud"
-#define image_deleteSelectedFiles               @"deleteSelectedFilesNextcloud"
-#define image_downloadSelectedFiles             @"downloadSelectedFilesNextcloud"
-#endif
-
 #define image_directory                         @"directory"
 #define image_directorycriptata                 @"directorycriptata"
 #define image_document                          @"document"
 #define image_documentcrypto                    @"documentcrypto"
-
-#ifdef CC
-#define image_drag                              @"drag"
-#define image_encryptedSelectedFiles            @"encryptedSelectedFiles"
-#endif
-
-#ifdef NC
-#define image_drag                              @"dragNextcloud"
-#define image_encryptedSelectedFiles            @"encryptedSelectedFilesNextcloud"
-#endif
-
 #define image_offline                           @"offline"
 #define image_offlinecrypto                     @"offlinecrypto"
 #define image_file                              @"file"
@@ -129,60 +91,14 @@
 #define image_filePreviewError                  @"filePreviewError"
 #define image_folder                            @"folder"
 #define image_foldercrypto                      @"foldercrypto"
-
-#ifdef CC
-#define image_folderphotocamera                 @"folderphotocamera"
-#endif
-
-#ifdef NC
-#define image_folderphotocamera                 @"folderphotocameraNextcloud"
-#endif
-
 #define image_home                              @"home"
-
 #define image_lock                              @"lock"
-
-#ifdef CC
-#define image_MenuDirectoryOnTop                @"menuDirectoryOnTop"
-
-#define image_MenuGroupByAlphabetic             @"MenuGroupByAlphabetic"
-#define image_MenuGroupByDate                   @"MenuGroupByDate"
-#define image_MenuGroupByTypeFile               @"MenuGroupByTypeFile"
-
-#define image_MenuOrdeyByDate                   @"MenuOrdeyByDate"
-#define image_MenuOrderByFileName               @"MenuOrderByFileName"
-
-#define image_MenuOrdinamentoDiscendente        @"MenuOrdinamentoDiscendente"
-#define image_MenuOrdinamentoAscendente         @"MenuOrdinamentoAscendente"
-
-#define image_moveSelectedFiles                 @"moveSelectedFiles"
-#endif
-
-#ifdef NC
-#define image_MenuDirectoryOnTop                @"menuDirectoryOnTopNextcloud"
-
-#define image_MenuGroupByAlphabetic             @"MenuGroupByAlphabeticNextcloud"
-#define image_MenuGroupByDate                   @"MenuGroupByDateNextcloud"
-#define image_MenuGroupByTypeFile               @"MenuGroupByTypeFileNextcloud"
-
-#define image_MenuOrdeyByDate                   @"MenuOrdeyByDateNextcloud"
-#define image_MenuOrderByFileName               @"MenuOrderByFileNameNextcloud"
-
-#define image_MenuOrdinamentoDiscendente        @"MenuOrdinamentoDiscendenteNextcloud"
-#define image_MenuOrdinamentoAscendente         @"MenuOrdinamentoAscendenteNextcloud"
-
-#define image_moveSelectedFiles                 @"moveSelectedFilesNextcloud"
-#endif
-
 #define image_more                              @"more"
 #define image_movie                             @"movie"
 #define image_moviecrypto                       @"moviecrypto"
-
 #define image_navBarOffline                     @"navBarOffline"
 #define image_navBarLocal                       @"navBarLocal"
-
 #define image_openSelectedFiles                 @"openSelectedFiles"
-
 #define image_passcode                          @"passcode"
 #define image_pdf                               @"pdf"
 #define image_pdfcrypto                         @"pdfcrypto"
@@ -190,36 +106,10 @@
 #define image_photocrypto                       @"photocrypto"
 #define image_photosDownload                    @"photosDownload"
 #define image_plist                             @"plist"
-
-#ifdef CC
-#define image_quickActionPhotos                 @"quickActionPhotos"
-#define image_quickActionUploadClear            @"quickActionUploadClear"
-#define image_quickActionUploadEncrypted        @"quickActionUploadEncrypted"
-#define image_quickActionUploadOffline          @"quickActionUploadOfflineNextcloud"
-#endif
-
-#ifdef NC
-#define image_quickActionPhotos                 @"quickActionPhotosNextcloud"
-#define image_quickActionUploadClear            @"quickActionUploadClearNextcloud"
-#define image_quickActionUploadEncrypted        @"quickActionUploadEncryptedNextcloud"
-#define image_quickActionUploadOffline          @"quickActionUploadOfflineNextcloud"
-#endif
-
 #define image_reloadtask                        @"reloadtask"
 #define image_reloadtaskcrypto                  @"reloadtaskcrypto"
 #define image_removetask                        @"removetask"
 #define image_removetaskcrypto                  @"removetaskcrypto"
-
-#ifdef CC
-#define image_seleziona                         @"seleziona"
-#define image_saveSelectedFiles                 @"saveSelectedFiles"
-#endif
-
-#ifdef NC
-#define image_seleziona                         @"selezionaNextcloud"
-#define image_saveSelectedFiles                 @"saveSelectedFilesNextcloud"
-#endif
-
 #define image_settings                          @"settings"
 #define image_settingsAccountDelete             @"settingsAccountDelete"
 #define image_settingsAccountDropbox            @"settingsAccountDropbox"
@@ -238,19 +128,6 @@
 #define image_settingsPasscodeNO                @"settingsPasscodeNO"
 #define image_settingsPasscodeYES               @"settingsPasscodeYES"
 #define image_settingsSync                      @"settingsSync"
-
-#ifdef CC
-#define image_settingsMail                      @"settingsMail"
-#define image_settingsManagePhotos              @"settingsManagePhotos"
-#define image_settingsManagePhotosChange        @"settingsManagePhotosChange"
-#endif
-
-#ifdef NC
-#define image_settingsMail                      @"settingsMailNextcloud"
-#define image_settingsManagePhotos              @"settingsManagePhotosNextcloud"
-#define image_settingsManagePhotosChange        @"settingsManagePhotosChangeNextcloud"
-#endif
-
 #define image_shareConnect                      @"shareConnect"
 #define image_shareExtEncrypt                   @"shareExtEncrypt"
 #define image_shareLink                         @"shareLink"
@@ -265,28 +142,10 @@
 #define image_statusuploadcrypto                @"statusuploadcrypto"
 #define image_stoptask                          @"stoptask"
 #define image_stoptaskcrypto                    @"stoptaskcrypto"
-
-#ifdef CC
-#define image_tabBarFile                        @"tabBarCryptoCloud"
-#endif
-
-#ifdef NC
-#define image_tabBarFile                        @"tabBarNextcloud"
-#endif
-
 #define image_tabBarOffline                     @"tabBarOffline"
 #define image_tabBarLocal                       @"tabBarLocal"
 #define image_tabBarPhotos                      @"tabBarPhotos"
 #define image_tabBarSettings                    @"tabBarSettings"
-
-#ifdef CC
-#define image_thumbs                            @"thumbs"
-#endif
-
-#ifdef NC
-#define image_thumbs                            @"thumbsNextcloud"
-#endif
-
 #define image_txt                               @"txt"
 #define image_txtcrypto                         @"txtcrypto"
 #define image_typeCloudDropbox                  @"typeCloudDropbox"
@@ -295,15 +154,11 @@
 #define image_typeFileCompress                  @"typeFileCompress"
 #define image_filetype_htlm                     @"filetype_html"
 #define image_filetype_htlm_crypto              @"filetype_html_crypto"
-
 #define image_uploadchiaro                      @"uploadchiaro"
 #define image_uploadcryptato                    @"uploadcryptato"
 #define image_uploaddisable                     @"uploaddisable"
-
 #define image_utorrent                          @"utorrent"
 #define image_utorrentcrypto                    @"utorrentcrypto"
-
 #define image_WiFiSmall                         @"WiFiSmall"
-
 #define image_xls                               @"xls"
 #define image_xlscrypto                         @"xlscrypto"
