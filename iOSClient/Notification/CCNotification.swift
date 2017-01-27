e4faa81938ca7f26a3cebeaeff924670b21d9e9b
Marino Faggiana <ios@nextcloud.com>
2017-01-27 10:45:55 +0100

    Notification view

---
+++ iOSClient/Notification/CCNotification.swift
@@ -0,0 +1,21 @@
+//
+//  CCNotification.swift
+//  Nextcloud
+//
+//  Created by Marino Faggiana on 27/01/17.
+//  Copyright © 2017 TWS. All rights reserved.
+//
+
+import UIKit
+
+class CCNotification: UITableViewController, UISearchResultsUpdating {
+
+
+    override func viewDidLoad() {
+    }
+    
+    
+    
+    func updateSearchResults(for searchController: UISearchController) {
+    }
+}
