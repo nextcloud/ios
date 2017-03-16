daae2eb9d9dd6259e64f848520b7d601b717d5da
Marino Faggiana <ios@nextcloud.com>
2017-03-16 23:00:42 +0100

    clear code

--- iOSClient/Brand/CCNextcloudConstant.h
+++ iOSClient/Brand/CCNextcloudConstant.h
@@ -69,8 +69,6 @@
 // COLOR
 // -----------------------------------------------------------------------------------------------------------
 
-// NEXTCLOUD COLOR
-
 #define COLOR_BRAND                     [UIColor colorWithRed:0.0/255.0 green:130.0/255.0 blue:201.0/255.0 alpha:1.0]                   // BLU NC : #0082c9
 
 #define COLOR_SELECT_BACKGROUND         [UIColor colorWithRed:0.0/255.0 green:130.0/255.0 blue:201.0/255.0 alpha:0.1]                   // BLU NC : #0082c9
