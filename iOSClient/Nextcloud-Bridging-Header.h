fbe920142aefbd37bb7049aa3b8ec9cb82368786
Marino Faggiana <ios@nextcloud.com>
2017-05-03 14:46:46 +0200

    New Version 2.17.2

---
+++ iOSClient/Nextcloud-Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  Use this file to import your target's public headers that you would like to expose to Swift.
+//
+
+#import "AHKActionSheet.h"
+#import "AppDelegate.h"
+#import "OCNetworking.h"
+#import "TWMessageBarManager.h"
+#import "CCManageAccount.h"
+#import "CCGraphics.h"
+
+#import <OCCommunicationLib/OCNotifications.h>
+#import <OCCommunicationLib/OCNotificationsAction.h>
+#import <OCCommunicationLib/OCFrameworkConstants.h>
