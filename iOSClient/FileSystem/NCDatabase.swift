0befb755d6406dd53146428519716c0c096de022
Marino Faggiana <ios@nextcloud.com>
2017-05-06 11:35:48 +0200

    fix

---
+++ iOSClient/FileSystem/NCDatabase.swift
@@ -0,0 +1,24 @@
+//
+//  NCDatabase.swift
+//  Nextcloud
+//
+//  Created by Marino Faggiana on 06/05/17.
+//  Copyright © 2017 TWS. All rights reserved.
+//
+
+import RealmSwift
+
+// Dog model
+class DBActivity: Object {
+    dynamic var account = ""
+    dynamic var action = "" //: Person? // Properties can be optional
+    dynamic var date = Date()
+    dynamic var file = ""
+    dynamic var fileID = ""
+    dynamic var idActivity : Double = 0
+    dynamic var link = ""
+    dynamic var note = ""
+    dynamic var selector = ""
+    dynamic var type = ""
+    dynamic var verbose = false
+}
---
+++ iOSClient/FileSystem/NCManageDatabase.swift
@@ -0,0 +1,27 @@
+//
+//  NCManageDatabase.swift
+//  Nextcloud
+//
+//  Created by Marino Faggiana on 06/05/17.
+//  Copyright © 2017 TWS. All rights reserved.
+//
+
+import RealmSwift
+
+class NCManageDatabase: NSObject {
+        
+    static let sharedInstance: NCManageDatabase = {
+        let instance = NCManageDatabase()
+        return instance
+    }()
+    
+    override init() {
+        
+        let dirGroup = FileManager.default.containerURL(forSecurityApplicationGroupIdentifier: k_capabilitiesGroups)
+        var config = Realm.Configuration()
+        
+        config.fileURL = dirGroup?.appendingPathComponent("\(appDatabaseNextcloud)/\(k_databaseDefault)")
+        
+        Realm.Configuration.defaultConfiguration = config
+    }
+}
