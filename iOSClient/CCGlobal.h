1e50c9e7bf7789749ba41c0dc9a92fcff2c3e70e
Marino Faggiana <ios@nextcloud.com>
2017-07-04 15:50:52 +0200

    clear code

--- iOSClient/CCGlobal.h
+++ iOSClient/CCGlobal.h
@@ -245,7 +245,6 @@ extern NSString *const urlBaseUploadDB;
 
 #define k_metadataStatusNormal                          0
 #define k_metadataStatusHide                            1
-#define k_metadataStatusTemp                            2
 
 #define k_tabBarApplicationIndexFile                    0
 #define k_tabBarApplicationIndexOffline                 1
