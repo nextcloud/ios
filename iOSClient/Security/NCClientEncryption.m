b6af109f93b61d4d58cae9d5a5b3f0f3af6ae714
Mario Danic <mario@lovelyhq.com>
2017-10-01 19:42:17 +0200

    New
    Signed-off-by: Mario Danic <mario@lovelyhq.com>

--- iOSClient/Security/NCClientEncryption.m
+++ iOSClient/Security/NCClientEncryption.m
@@ -39,21 +39,6 @@
 #import <openssl/err.h>
 #import <openssl/bn.h>
 
-CCCryptorStatus CCCryptorGCM(
-                             CCOperation     op,             /* kCCEncrypt, kCCDecrypt */
-                             CCAlgorithm     alg,
-                             const void      *key,           /* raw key material */
-                             size_t          keyLength,
-                             const void      *iv,
-                             size_t          ivLen,
-                             const void      *aData,
-                             size_t          aDataLen,
-                             const void      *dataIn,
-                             size_t          dataInLength,
-                             void            *dataOut,
-                             const void      *tag,
-                             size_t          *tagLength);
-
 #define NSMakeError(description) [NSError errorWithDomain:@"com.nextcloud.nextcloudiOS" code:-1 userInfo:@{NSLocalizedDescriptionKey: description}];
 @implementation NCClientEncryption
 
