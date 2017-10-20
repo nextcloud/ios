afbf53a13908bf7be8319c19239b3c42414dcaa6
Marino Faggiana <ios@nextcloud.com>
2017-10-20 21:44:50 +0200

    add func getMD5

--- iOSClient/Security/NCEndToEndEncryption.m
+++ iOSClient/Security/NCEndToEndEncryption.m
@@ -575,4 +575,23 @@ cleanup:
     return ivData;
 }
 
+- (NSString *)getMD5:(NSString *)input
+{
+    // Create pointer to the string as UTF8
+    const char *ptr = [input UTF8String];
+    
+    // Create byte array of unsigned chars
+    unsigned char md5Buffer[CC_MD5_DIGEST_LENGTH];
+    
+    // Create 16 byte MD5 hash value, store in buffer
+    CC_MD5(ptr, (unsigned int)strlen(ptr), md5Buffer);
+    
+    // Convert MD5 value in the buffer to NSString of hex values
+    NSMutableString *output = [NSMutableString stringWithCapacity:CC_MD5_DIGEST_LENGTH * 2];
+    for(int i = 0; i < CC_MD5_DIGEST_LENGTH; i++)
+        [output appendFormat:@"%02x",md5Buffer[i]];
+    
+    return output;
+}
+
 @end
