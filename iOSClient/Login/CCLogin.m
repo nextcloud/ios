ecd8ff4a5ab80b5fda6a3a2aa48771fcd2ee88e6
Marino Faggiana <ios@nextcloud.com>
2017-10-27 16:27:08 +0200

    fix disable_linkLoginProvider

--- iOSClient/Login/CCLogin.m
+++ iOSClient/Login/CCLogin.m
@@ -47,21 +47,22 @@
     
     if ([NCBrandOptions sharedInstance].disable_linkLoginProvider) {
         self.bottomLabel.hidden = YES;
+    } else {
+    
+        if (self.view.frame.size.width == ([[UIScreen mainScreen] bounds].size.width*([[UIScreen mainScreen] bounds].size.width<[[UIScreen mainScreen] bounds].size.height))+([[UIScreen mainScreen] bounds].size.height*([[UIScreen mainScreen] bounds].size.width>[[UIScreen mainScreen] bounds].size.height))) {
+        
+            // Portrait
+            self.bottomLabel.hidden = NO;
+        
+        } else {
+        
+            // Landscape
+            self.bottomLabel.hidden = YES;
+        }
     }
-
+    
     UITapGestureRecognizer *tapGesture = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(tabBottomLabel)];
     [self.bottomLabel addGestureRecognizer:tapGesture];
-    
-    if (self.view.frame.size.width == ([[UIScreen mainScreen] bounds].size.width*([[UIScreen mainScreen] bounds].size.width<[[UIScreen mainScreen] bounds].size.height))+([[UIScreen mainScreen] bounds].size.height*([[UIScreen mainScreen] bounds].size.width>[[UIScreen mainScreen] bounds].size.height))) {
-        
-        // Portrait
-        self.bottomLabel.hidden = NO;
-        
-    } else {
-        
-        // Landscape
-        self.bottomLabel.hidden = YES;
-    }
     
     self.annulla.tintColor = [NCBrandColor sharedInstance].customer;
     
@@ -148,15 +149,18 @@
 {
     [coordinator animateAlongsideTransition:nil completion:^(id<UIViewControllerTransitionCoordinatorContext> context) {
         
-        if (self.view.frame.size.width == ([[UIScreen mainScreen] bounds].size.width*([[UIScreen mainScreen] bounds].size.width<[[UIScreen mainScreen] bounds].size.height))+([[UIScreen mainScreen] bounds].size.height*([[UIScreen mainScreen] bounds].size.width>[[UIScreen mainScreen] bounds].size.height))) {
+        if (![NCBrandOptions sharedInstance].disable_linkLoginProvider) {
+        
+            if (self.view.frame.size.width == ([[UIScreen mainScreen] bounds].size.width*([[UIScreen mainScreen] bounds].size.width<[[UIScreen mainScreen] bounds].size.height))+([[UIScreen mainScreen] bounds].size.height*([[UIScreen mainScreen] bounds].size.width>[[UIScreen mainScreen] bounds].size.height))) {
             
-            // Portrait
-            self.bottomLabel.hidden = NO;
+                // Portrait
+                self.bottomLabel.hidden = NO;
             
-        } else {
+            } else {
             
-            // Landscape
-            self.bottomLabel.hidden = YES;
+                // Landscape
+                self.bottomLabel.hidden = YES;
+            }
         }
     }];
     
