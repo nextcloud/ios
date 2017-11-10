6428e4c1010950d77c51b7e8fce692f3e3b23d53
Marino Faggiana <ios@nextcloud.com>
2017-11-10 11:43:38 +0100

    clear data

--- iOSClient/Security/NCEndToEndEncryption.m
+++ iOSClient/Security/NCEndToEndEncryption.m
@@ -538,43 +538,6 @@ cleanup:
     return false;
 }
 
-/*
-- (void)decryptMetadata:(NSString *)metadata activeUrl:(NSString *)activeUrl
-{
-    NSMutableData *plainData;
-    
-    NSData *cipherData = [[NSFileManager defaultManager] contentsAtPath:[NSString stringWithFormat:@"%@/%@", activeUrl, metadata.fileID]];
-    NSData *keyData = [[NSData alloc] initWithBase64EncodedString:@"WANM0gRv+DhaexIsI0T3Lg==" options:0];
-    NSData *ivData = [[NSData alloc] initWithBase64EncodedString:@"gKm3n+mJzeY26q4OfuZEqg==" options:0];
-    NSData *tagData = [[NSData alloc] initWithBase64EncodedString:@"PboI9tqHHX3QeAA22PIu4w==" options:0];
-    
-    BOOL result = [self decryptData:cipherData plainData:&plainData keyData:keyData keyLen:AES_KEY_128_LENGTH ivData:ivData tagData:tagData];
-    
-    if (plainData != nil && result) {
-        [plainData writeToFile:[NSString stringWithFormat:@"%@/%@", activeUrl, @"decrypted"] atomically:YES];
-    }
-}
-
-- (NSString *)decryptMetadata:(NSString *)cipher key:(NSString *)key iv:(NSString *)iv tag:(NSString *)tag
-{
-    NSMutableData *plainData;
-    
-    NSData *cipherData = [cipher dataUsingEncoding:NSUTF8StringEncoding];
-    NSData *keyData = [key dataUsingEncoding:NSUTF8StringEncoding];
-    NSData *ivData = [iv dataUsingEncoding:NSUTF8StringEncoding];
-    NSData *tagData = [tag dataUsingEncoding:NSUTF8StringEncoding];
-
-    
-    BOOL result = [self decryptData:cipherData plainData:&plainData keyData:keyData keyLen:AES_KEY_128_LENGTH ivData:ivData tagData:tagData];
-    
-    if (plainData != nil && result)
-        return [[NSString alloc] initWithData:plainData encoding:NSUTF8StringEncoding];
-    else
-        return nil;
-}
-*/
-
-
 // -----------------------------------------------------------------------------------------------------------------------------------------------------------------------
 // -----------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
