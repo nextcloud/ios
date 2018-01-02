5a6407b97d43d63da6b8c0842f1c40c7cc6a9c70
Marino Faggiana <m.faggiana@twsweb.it>
2018-01-02 12:58:02 +0100

    change routine converted Byte Formatter

--- iOSClient/Utility/CCUtility.m
+++ iOSClient/Utility/CCUtility.m
@@ -560,6 +560,13 @@
 
 + (NSString *)transformedSize:(double)value
 {
+    NSString *string = [NSByteCountFormatter stringFromByteCount:value countStyle:NSByteCountFormatterCountStyleFile];
+    return string;
+    /*
+    NSLog(@"%@", string);
+    
+    
+    
     int multiplyFactor = 0;
     
     NSArray *tokens = [NSArray arrayWithObjects:@"bytes",@"KB",@"MB",@"GB",@"TB",@"PB",@"EB",@"ZB",@"YB",nil];
@@ -567,7 +574,8 @@
         value /= 1024;
         multiplyFactor++;
     }
-    return [NSString stringWithFormat:@"%4.2f %@",value, [tokens objectAtIndex:multiplyFactor]];
+    return [NSString stringWithFormat:@"%4.2f %@ - %@",value, [tokens objectAtIndex:multiplyFactor], string];
+    */
 }
 
 // Remove do not forbidden characters for Nextcloud Server
