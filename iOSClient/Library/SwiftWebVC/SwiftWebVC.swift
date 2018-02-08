8c764da79391edb6653738c9b3601260d4d9a542
Marino Faggiana <m.faggiana@twsweb.it>
2018-02-08 10:44:42 +0100

    Fix crash on URLAuthenticationChallenge #982

--- iOSClient/Library/SwiftWebVC/SwiftWebVC.swift
+++ iOSClient/Library/SwiftWebVC/SwiftWebVC.swift
@@ -358,7 +358,13 @@ extension SwiftWebVC: WKNavigationDelegate {
     
     public func webView(_ webView: WKWebView, didReceive challenge: URLAuthenticationChallenge, completionHandler: @escaping (URLSession.AuthChallengeDisposition, URLCredential?) -> Void) {
         
-        completionHandler(Foundation.URLSession.AuthChallengeDisposition.useCredential, URLCredential(trust: challenge.protectionSpace.serverTrust!))
+        if challenge.previousFailureCount > 0 {
+            completionHandler(Foundation.URLSession.AuthChallengeDisposition.cancelAuthenticationChallenge, nil)
+        } else if let serverTrust = challenge.protectionSpace.serverTrust {
+            completionHandler(Foundation.URLSession.AuthChallengeDisposition.useCredential, URLCredential(trust: serverTrust))
+        } else {
+            print("unknown state. error: \(String(describing: challenge.error))")
+        }
     }
 
 }
