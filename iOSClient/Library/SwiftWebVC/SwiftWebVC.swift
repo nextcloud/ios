e88429831024700769ad532d54894ea876a16cab
Marino Faggiana <m.faggiana@twsweb.it>
2018-02-15 10:52:29 +0100

    add check on evaluateJavaScript("document.title"

--- iOSClient/Library/SwiftWebVC/SwiftWebVC.swift
+++ iOSClient/Library/SwiftWebVC/SwiftWebVC.swift
@@ -323,7 +323,7 @@ extension SwiftWebVC: WKNavigationDelegate {
         
         webView.evaluateJavaScript("document.title", completionHandler: {(response, error) in
             if error == nil {
-                if (response != nil) {
+                if (response != nil && response is String) {
                     self.navBarTitle.text = response as! String?
                 }
                 self.navBarTitle.sizeToFit()
