a9b5c670421ea2f080a697011705805e1e1e39b9
Marino Faggiana <m.faggiana@twsweb.it>
2018-03-04 13:03:00 +0100

    fix

--- iOSClient/Networking/OCNetworking.m
+++ iOSClient/Networking/OCNetworking.m
@@ -576,9 +576,9 @@
                 serverUrl = [serverUrl substringToIndex:[serverUrl length] - 1];
             serverUrl = [CCUtility stringAppendServerUrl:[_activeUrl stringByAppendingString:webDAV] addFileName:serverUrl];
             
-            if (itemDto.isDirectory && ![serverUrl isEqualToString:[CCUtility getHomeServerUrlActiveUrl:_activeUrl]]) {
+            if (itemDto.isDirectory) {
                 isFolderEncrypted = itemDto.isEncrypted;
-                directoryID = [[NCManageDatabase sharedInstance] addDirectoryWithEncrypted:isFolderEncrypted favorite:itemDto.isFavorite fileID:itemDto.ocId permissions:itemDto.permissions serverUrl:serverUrl].directoryID;
+                directoryID = [[NCManageDatabase sharedInstance] addDirectoryWithEncrypted:isFolderEncrypted favorite:itemDto.isFavorite fileID:itemDto.ocId permissions:itemDto.permissions serverUrl:[NSString stringWithFormat:@"%@/%@", serverUrl, fileName]].directoryID;
             } else {
                 isFolderEncrypted = [CCUtility isFolderEncrypted:serverUrl account:_metadataNet.account];
                 directoryID = [[NCManageDatabase sharedInstance] getDirectoryID:serverUrl];
