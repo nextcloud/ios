9020b7d8194cc85da1f009d800bdd5f3e2cfcba5
Marino Faggiana <m.faggiana@twsweb.it>
2018-05-27 12:11:40 +0200

    clear code

--- PickerFileProvider/FileProvider.swift
+++ PickerFileProvider/FileProvider.swift
@@ -40,8 +40,6 @@ var fileProviderStorageURL: URL?
 
 // List
 var listUpdateItems = [NSFileProviderItem]()
-var listDeleteItems = [NSFileProviderItem]()
-
 var listFavoriteIdentifierRank = [String:NSNumber]()
 var fileNamePathImport = [String]()
 
@@ -1096,23 +1094,6 @@ class FileProvider: NSFileProviderExtension, CCNetworkingDelegate {
     // --------------------------------------------------------------------------------------------
     //  MARK: - User Function
     // --------------------------------------------------------------------------------------------
-    
-    // Convinent method to signal the enumeration for containers.
-    //
-    func signalEnumerator(for containerItemIdentifiers: [NSFileProviderItemIdentifier]) {
-        
-        /* ONLY iOS 11*/
-        guard #available(iOS 11, *) else { return }
-    
-        for containerItemIdentifier in containerItemIdentifiers {
-            
-            NSFileProviderManager.default.signalEnumerator(for: containerItemIdentifier) { error in
-                if let error = error {
-                    print("SignalEnumerator for \(containerItemIdentifier) returned error: \(error)")
-                }
-            }
-        }
-    }
     
     func refreshEnumerator(identifier: NSFileProviderItemIdentifier, serverUrl: String) {
         
