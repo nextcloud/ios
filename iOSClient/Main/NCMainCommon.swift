50b86ad79342359a68e4412ba2fd785e6cc02bec
Marino Faggiana <m.faggiana@twsweb.it>
2018-07-20 17:45:16 +0200

    clear code

--- iOSClient/Main/NCMainCommon.swift
+++ iOSClient/Main/NCMainCommon.swift
@@ -568,6 +568,86 @@ class NCMainCommon: NSObject {
             })
         }
     }
-    
 }
+    
+//MARK: -
+    
+class CCMainTabBarController : UITabBarController, UITabBarControllerDelegate {
+        
+    override func viewDidLoad() {
+            
+        super.viewDidLoad()
+        delegate = self
+    }
+        
+    //Delegate methods
+    func tabBarController(_ tabBarController: UITabBarController, shouldSelect viewController: UIViewController) -> Bool {
+            
+        let tabViewControllers = tabBarController.viewControllers!
+        guard let toIndex = tabViewControllers.index(of: viewController) else {
+                
+            if let vc = viewController as? UINavigationController {
+                vc.popToRootViewController(animated: true);
+            }
+                
+            return false
+        }
+            
+        animateToTab(toIndex: toIndex)
+            
+        return true
+    }
+        
+    func animateToTab(toIndex: Int) {
+            
+        let tabViewControllers = viewControllers!
+        let fromView = selectedViewController!.view!
+        let toView = tabViewControllers[toIndex].view!
+        let fromIndex = tabViewControllers.index(of: selectedViewController!)
+            
+        guard fromIndex != toIndex else {return}
+            
+        // Add the toView to the tab bar view
+        fromView.superview?.addSubview(toView)
+        fromView.superview?.backgroundColor = UIColor.white
+            
+        // Position toView off screen (to the left/right of fromView)
+        let screenWidth = UIScreen.main.bounds.size.width;
+        let scrollRight = toIndex > fromIndex!;
+        let offset = (scrollRight ? screenWidth : -screenWidth)
+        toView.center = CGPoint(x: (fromView.center.x) + offset, y: (toView.center.y))
+            
+        // Disable interaction during animation
+        view.isUserInteractionEnabled = false
+            
+        UIView.animate(withDuration: 0.3, delay: 0.0, usingSpringWithDamping: 1, initialSpringVelocity: 0, options: UIViewAnimationOptions.curveEaseOut, animations: {
+                
+            // Slide the views by -offset
+            fromView.center = CGPoint(x: fromView.center.x - offset, y: fromView.center.y);
+            toView.center   = CGPoint(x: toView.center.x - offset, y: toView.center.y);
+                
+        }, completion: { finished in
+                
+            // Remove the old view from the tabbar view.
+            fromView.removeFromSuperview()
+            self.selectedIndex = toIndex
+            self.view.isUserInteractionEnabled = true
+        })
+    }
+}
+
+//
+// https://stackoverflow.com/questions/44822558/ios-11-uitabbar-uitabbaritem-positioning-issue/46348796#46348796
+//
+extension UITabBar {
+    // Workaround for iOS 11's new UITabBar behavior where on iPad, the UITabBar inside
+    // the Master view controller shows the UITabBarItem icon next to the text
+    override open var traitCollection: UITraitCollection {
+        if UIDevice.current.userInterfaceIdiom == .pad {
+            return UITraitCollection(horizontalSizeClass: .compact)
+        }
+        return super.traitCollection
+    }
+}
+
 
