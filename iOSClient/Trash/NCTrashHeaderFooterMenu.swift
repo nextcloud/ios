df7e492ca0787daafd0b42972edf125ecf5a9607
Marino Faggiana <m.faggiana@twsweb.it>
2018-10-17 20:56:55 +0200

    order UI

--- iOSClient/Trash/NCTrashHeaderFooterMenu.swift
+++ iOSClient/Trash/NCTrashHeaderFooterMenu.swift
@@ -23,6 +23,7 @@ class NCTrashHeaderMenu: UICollectionReusableView {
         buttonSwitch.setImage(CCGraphics.changeThemingColorImage(UIImage.init(named: "switchList"), multiplier: 2, color: NCBrandColor.sharedInstance.icon), for: .normal)
         
         buttonOrder.setTitle("", for: .normal)
+        buttonOrder.setTitleColor(NCBrandColor.sharedInstance.icon, for: .normal)
         
         buttonMore.setImage(CCGraphics.changeThemingColorImage(UIImage.init(named: "moreBig"), multiplier: 2, color: NCBrandColor.sharedInstance.icon), for: .normal)
         
