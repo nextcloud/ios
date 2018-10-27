7f9f7dbe017dc207b5ed6e13b73e87e3ccb6fbf0
Marino Faggiana <m.faggiana@twsweb.it>
2018-10-27 16:47:06 +0200

    clear code

--- iOSClient/Offline/NCOffline.swift
+++ iOSClient/Offline/NCOffline.swift
@@ -34,8 +34,8 @@ class NCOffline: UIViewController ,UICollectionViewDataSource, UICollectionViewD
     var isEditMode = false
     var selectFileID = [String]()
     
-    var listLayout: ListLayoutOffline!
-    var gridLayout: GridLayoutOffline!
+    var listLayout: listLayoutOffline!
+    var gridLayout: gridLayoutOffline!
     
     private let headerMenuHeight: CGFloat = 50
     private let sectionHeaderHeight: CGFloat = 20
@@ -59,8 +59,8 @@ class NCOffline: UIViewController ,UICollectionViewDataSource, UICollectionViewD
         
         collectionView.alwaysBounceVertical = true
 
-        listLayout = ListLayoutOffline()
-        gridLayout = GridLayoutOffline()
+        listLayout = listLayoutOffline()
+        gridLayout = gridLayoutOffline()
         
         if CCUtility.getLayoutOffline() == "list" {
             collectionView.collectionViewLayout = listLayout
@@ -692,86 +692,5 @@ class NCOffline: UIViewController ,UICollectionViewDataSource, UICollectionViewD
         headerView.label.textColor = NCBrandColor.sharedInstance.icon
         
         return headerView
-    }
-}
-
-class ListLayoutOffline: UICollectionViewFlowLayout {
-    
-    let itemHeight: CGFloat = 60
-    
-    override init() {
-        super.init()
-        
-        minimumInteritemSpacing = 0
-        minimumLineSpacing = 1
-        
-        self.scrollDirection = .vertical
-        self.sectionInset = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
-    }
-    
-    required init?(coder aDecoder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-    
-    override var itemSize: CGSize {
-        get {
-            if let collectionView = collectionView {
-                let itemWidth: CGFloat = collectionView.frame.width
-                return CGSize(width: itemWidth, height: self.itemHeight)
-            }
-            
-            // Default fallback
-            return CGSize(width: 100, height: 100)
-        }
-        set {
-            super.itemSize = newValue
-        }
-    }
-    
-    override func targetContentOffset(forProposedContentOffset proposedContentOffset: CGPoint) -> CGPoint {
-        return proposedContentOffset
-    }
-}
-
-class GridLayoutOffline: UICollectionViewFlowLayout {
-    
-    let heightLabelPlusButton: CGFloat = 45
-    let preferenceWidth: CGFloat = 110
-    let marginLeftRight: CGFloat = 5
-    
-    override init() {
-        super.init()
-        
-        minimumInteritemSpacing = 1
-        minimumLineSpacing = 1
-
-        self.scrollDirection = .vertical
-        self.sectionInset = UIEdgeInsets(top: 10, left: marginLeftRight, bottom: 0, right:  marginLeftRight)
-    }
-    
-    required init?(coder aDecoder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-    
-    override var itemSize: CGSize {
-        get {
-            if let collectionView = collectionView {
-                
-                let numItems: Int = Int(collectionView.frame.width / preferenceWidth)                
-                let itemWidth: CGFloat = (collectionView.frame.width - (marginLeftRight * 2) - CGFloat(numItems)) / CGFloat(numItems)
-                let itemHeight: CGFloat = itemWidth + heightLabelPlusButton
-                return CGSize(width: itemWidth, height: itemHeight)
-            }
-            
-            // Default fallback
-            return CGSize(width: 100, height: 100)
-        }
-        set {
-            super.itemSize = newValue
-        }
-    }
-    
-    override func targetContentOffset(forProposedContentOffset proposedContentOffset: CGPoint) -> CGPoint {
-        return proposedContentOffset
     }
 }
