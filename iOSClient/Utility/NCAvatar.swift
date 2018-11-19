d90fdadfbf6e663d229b1c178afdf0fe6fbea21d
Marino Faggiana <m.faggiana@twsweb.it>
2018-11-19 14:12:28 +0100

    fix license

--- iOSClient/Utility/NCAvatar.swift
+++ iOSClient/Utility/NCAvatar.swift
@@ -3,8 +3,24 @@
 //  Nextcloud
 //
 //  Created by Marino Faggiana on 16/11/2018.
-//  Copyright © 2018 TWS. All rights reserved.
+//  Copyright (c) 2018 Marino Faggiana. All rights reserved.
 //
+//  Author Marino Faggiana <m.faggiana@twsweb.it>
+//
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+
 
 import Foundation
 
