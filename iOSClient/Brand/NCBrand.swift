100b8cc02ead604eb418bd9ed8289382d8bd88e3
Marino Faggiana <m.faggiana@twsweb.it>
2018-11-26 15:01:29 +0100

    clear code

--- iOSClient/Brand/NCBrand.swift
+++ iOSClient/Brand/NCBrand.swift
@@ -73,7 +73,7 @@ class NCBrandColor: NSObject {
     @objc public let mailMe:                            String = "ios@nextcloud.com"
     @objc public let textCopyrightNextcloudiOS:         String = "Nextcloud for iOS %@ © 2018"
     @objc public let textCopyrightNextcloudServer:      String = "Nextcloud Server %@"
-    @objc public let loginBaseUrl:                      String = "https://cloud.twsweb.it"
+    @objc public let loginBaseUrl:                      String = "https://cloud.nextcloud.com"
     @objc public let pushNotificationServerProxy:       String = "https://push-notifications.nextcloud.com"
     @objc public let linkLoginHost:                     String = "https://nextcloud.com/install"
     @objc public let linkloginPreferredProviders:       String = "https://nextcloud.com/signup";
