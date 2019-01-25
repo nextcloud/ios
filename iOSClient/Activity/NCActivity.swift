e8f66b2c20fbbb6fc26afac3177cb69e4d55dbc9
Marino Faggiana <marino@marinofaggiana.com>
2019-01-25 22:15:12 +0100

    activity view

--- iOSClient/Activity/NCActivity.swift
+++ iOSClient/Activity/NCActivity.swift
@@ -282,6 +282,10 @@ class activityTableViewCell: UITableViewCell, UICollectionViewDelegate, UICollec
         return 20
     }
     
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        return UIEdgeInsets(top: 0, left: 0, bottom: 10, right: 0)
+    }
+    
     func numberOfSections(in collectionView: UICollectionView) -> Int {
         return 1
     }
