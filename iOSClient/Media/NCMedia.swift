8a578812badbfbb77835d78765cf3367f65a5bc9
marinofaggiana <marino@marinofaggiana.com>
2019-02-28 17:34:05 +0100

    add DEBUG

--- iOSClient/Media/NCMedia.swift
+++ iOSClient/Media/NCMedia.swift
@@ -510,9 +510,11 @@ extension NCMedia {
     func search(lteDate: Date, gteDate: Date, addPast: Bool, setDistantPast: Bool) {
         
         // ----- DEBUG -----
+#if DEBUG
         let dateFormatter = DateFormatter()
         dateFormatter.dateFormat = "dd-MM-yyyy HH:mm"
         print("[LOG] Search: addPast \(addPast), distantPass: \(setDistantPast), Lte: " + dateFormatter.string(from: lteDate) + " - Gte: " + dateFormatter.string(from: gteDate))
+#endif
         // -----------------
         
         if appDelegate.activeAccount.count == 0 {
