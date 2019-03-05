337d590880f0f92becbd3c991af67c7981ec393f
marinofaggiana <marino@marinofaggiana.com>
2019-03-05 16:17:08 +0100

    fix

--- iOSClient/PeekPop/CCPeekPop.m
+++ iOSClient/PeekPop/CCPeekPop.m
@@ -55,16 +55,18 @@
             
             self.imagePreview.image = [UIImage imageWithContentsOfFile:[CCUtility getDirectoryProviderStorageIconFileID:self.metadata.fileID fileNameView:self.metadata.fileNameView]];
         
+            self.imagePreview.contentMode = UIViewContentModeScaleToFill;
+            self.preferredContentSize = CGSizeMake(self.imagePreview.image.size.width, self.imagePreview.image.size.height);
+            
         } else {
             
             NSURL *url = [[NSBundle mainBundle] URLForResource:@"loading" withExtension:@"gif"];
             self.imagePreview.image = [UIImage animatedImageWithAnimatedGIFURL:url];
             
+            self.imagePreview.contentMode = UIViewContentModeCenter;
+            
             [self downloadThumbnail];
         }
-        
-        self.imagePreview.contentMode = UIViewContentModeScaleToFill;
-        self.preferredContentSize = CGSizeMake(self.imagePreview.image.size.width, self.imagePreview.image.size.height);
         
     } else {
         
@@ -121,6 +123,8 @@
         if (errorCode == 0 && [account isEqualToString:appDelegate.activeAccount]) {
             
             self.imagePreview.image = image;
+            
+            self.imagePreview.contentMode = UIViewContentModeScaleToFill;
             self.preferredContentSize = CGSizeMake(self.imagePreview.image.size.width, self.imagePreview.image.size.height);
             
         } else {
