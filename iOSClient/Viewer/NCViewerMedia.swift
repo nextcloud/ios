f72dde58cccca1d34e359425bb5b0187cb570a17
marinofaggiana <marino@marinofaggiana.com>
2019-03-29 12:58:13 +0100

    clear code

--- iOSClient/Viewer/NCViewerMedia.swift
+++ iOSClient/Viewer/NCViewerMedia.swift
@@ -97,7 +97,8 @@ class NCViewerMedia: NSObject {
             
             // Save cache
             if !CCUtility.fileProviderStorageExists(self.metadata.fileID, fileNameView:self.metadata.fileNameView) {
-                guard let url = KTVHTTPCache.cacheCompleteFileURLIfExisted(with: self.videoURL) else {
+                
+                guard let url = KTVHTTPCache.cacheCompleteFileURL(with: self.videoURL) else {
                     return
                 }
                 
@@ -122,29 +123,27 @@ class NCViewerMedia: NSObject {
     
     @objc func setupHTTPCache() {
         
-        var error: NSError?
-
         KTVHTTPCache.cacheSetMaxCacheLength(Int64(k_maxHTTPCache))
         
         if ProcessInfo.processInfo.environment["SIMULATOR_DEVICE_NAME"] != nil {
             KTVHTTPCache.logSetConsoleLogEnable(true)
         }
         
-        KTVHTTPCache.proxyStart(&error)
-        if error == nil {
-            print("Proxy Start Success")
-        } else {
-            print("Proxy Start error : \(error!)")
+        do {
+            try KTVHTTPCache.proxyStart()
+        } catch let error {
+            print("Proxy Start error : \(error)")
         }
-    
-        KTVHTTPCache.tokenSetURLFilter { (url) -> URL? in
+        
+        KTVHTTPCache.encodeSetURLConverter { (url) -> URL? in
             print("URL Filter reviced URL : " + String(describing: url))
             return url
         }
         
-        KTVHTTPCache.downloadSetUnsupportContentTypeFilter { (url, contentType) -> Bool in
+        KTVHTTPCache.downloadSetUnacceptableContentTypeDisposer { (url, contentType) -> Bool in
             print("Unsupport Content-Type Filter reviced URL : " + String(describing: url) + " " + String(describing: contentType))
             return false
         }
+        
     }
 }
