79824b7ee7837388bf7a68dbbebb17b86ee1ef6b
marinofaggiana <marino@marinofaggiana.com>
2019-04-29 10:46:13 +0200

    new string

--- iOSClient/Settings/CCSettings.m
+++ iOSClient/Settings/CCSettings.m
@@ -253,7 +253,7 @@
         
         if ([[rowDescriptor.value valueData] boolValue] == YES) {
             
-            UIAlertController *alertController = [UIAlertController alertControllerWithTitle:@"" message:NSLocalizedString(@"_continue_", nil) preferredStyle:UIAlertControllerStyleActionSheet];
+            UIAlertController *alertController = [UIAlertController alertControllerWithTitle:@"" message:NSLocalizedString(@"_continue_request_", nil) preferredStyle:UIAlertControllerStyleActionSheet];
             
             [alertController addAction: [UIAlertAction actionWithTitle:NSLocalizedString(@"_ok_", nil) style:UIAlertActionStyleDestructive handler:^(UIAlertAction *action) {
                 [CCUtility setFavoriteOffline:true];
