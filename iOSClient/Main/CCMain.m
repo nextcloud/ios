c2d125299d920c278ecb9aba30d93d7f6bbf897f
marinofaggiana <marino@marinofaggiana.com>
2019-05-28 17:31:44 +0200

    replace  readShareWithAccount

--- iOSClient/Main/CCMain.m
+++ iOSClient/Main/CCMain.m
@@ -1863,13 +1863,8 @@
 
 - (void)readShareWithAccount:(NSString *)account openWindow:(BOOL)openWindow metadata:(tableMetadata *)metadata
 {
-    NSString *path = [CCUtility returnFileNamePathFromFileName:metadata.fileName serverUrl:self.serverUrl activeUrl:appDelegate.activeUrl];
-    
-    [[OCNetworking sharedManager] readShareWithAccount:account path:path completion:^(NSString *account, NSArray *items, NSString *message, NSInteger errorCode) {
-        
-    }];
-    
-    [[OCNetworking sharedManager] readShareWithAccount:account completion:^(NSString *account, NSArray *items, NSString *message, NSInteger errorCode) {
+    //[[OCNetworking sharedManager] readShareWithAccount:account completion:^(NSString *account, NSArray *items, NSString *message, NSInteger errorCode) {
+    [[OCNetworking sharedManager] readShareWithAccount:account path:@"/" completion:^(NSString *account, NSArray *items, NSString *message, NSInteger errorCode) {
         
         [_hud hideHud];
 
