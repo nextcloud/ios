b5377ebb927bba68914ff702d149bb48e619aa83
marinofaggiana <marino@marinofaggiana.com>
2019-07-27 11:09:37 +0200

    new share

--- iOSClient/Share/NCSharePaging.swift
+++ iOSClient/Share/NCSharePaging.swift
@@ -64,8 +64,10 @@ class NCSharePaging: UIViewController {
             pagingViewController.view.trailingAnchor.constraint(equalTo: view.trailingAnchor),
             ])
         
-        // Set our data source and delegate.
+        // Set our data source and start page
         pagingViewController.dataSource = self
+        pagingViewController.select(index: 2)
+
     }
     
     @objc func exitTapped() {
