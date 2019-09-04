9c450620a3e40ef6b0d0795a0c300c9b586a8f78
marinofaggiana <marino@marinofaggiana.com>
2019-09-04 17:07:42 +0200

    dev dark mode

--- iOSClient/Brand/NCBrand.swift
+++ iOSClient/Brand/NCBrand.swift
@@ -42,6 +42,7 @@ class NCBrandColor: NSObject {
     @objc public var connectionNo:          UIColor = UIColor(red: 204.0/255.0, green: 204.0/255.0, blue: 204.0/255.0, alpha: 1.0)
     @objc public var encrypted:             UIColor = .red
     @objc public var backgroundView:        UIColor = .white
+    @objc public var backgroundMenu:        UIColor = .white
     @objc public var textView:              UIColor = .black
     @objc public var separator:             UIColor = UIColor(red: 235.0/255.0, green: 235.0/255.0, blue: 235.0/255.0, alpha: 1.0)
     @objc public var tabBar:                UIColor = .white
@@ -64,11 +65,13 @@ class NCBrandColor: NSObject {
         if darkMode {
             tabBar = .black
             backgroundView = .black
+            backgroundMenu = .darkGray
             textView = .white
             separator = UIColor(red: 60.0/255.0, green: 60.0/255.0, blue: 60.0/255.0, alpha: 1.0)
         } else {
             tabBar = .white
             backgroundView = .white
+            backgroundMenu = .white
             textView = .black
             separator = UIColor(red: 235.0/255.0, green: 235.0/255.0, blue: 235.0/255.0, alpha: 1.0)
         }
