fc3c2077342d0a9caa18d7f57969048c6964e2b7
marinofaggiana <marino@marinofaggiana.com>
2019-09-12 10:15:18 +0200

    imi compatibility

--- Libraries external/Imagemeter/IMImagemeterViewer.swift
+++ Libraries external/Imagemeter/IMImagemeterViewer.swift
@@ -2,8 +2,23 @@
 //  IMImagemeterViewer.swift
 //  Nextcloud
 //
-//  Created by Marino Faggiana on 12/09/2019.
-//  Copyright © 2019 TWS. All rights reserved.
+//  Created by Marino Faggiana on 12/11/2019.
+//  Copyright (c) 2017 Marino Faggiana. All rights reserved.
+//
+//  Author Marino Faggiana <marino.faggiana@nextcloud.com>
+//
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 //
 
 import UIKit
@@ -11,6 +26,5 @@ import Foundation
 import GLKit
 
 class IMImagemeterViewer: GLKViewController, UITabBarDelegate {
-    
     var metadata: tableMetadata?
 }
