389e5e75555bd0edc37464b7b3681e4de3ab6439
marinofaggiana <marino@marinofaggiana.com>
2020-01-09 10:49:52 +0100

    fix

--- iOSClient/Brand/NCBrand.swift
+++ iOSClient/Brand/NCBrand.swift
@@ -71,7 +71,7 @@ import UIKit
     @objc public var userAgent:                         String = "Nextcloud-iOS"                                    // Don't touch me !!
     
     // Options
-    @objc public var use_login_web_personalized:        Bool = false
+    @objc public var use_login_web_personalized:        Bool = false                                                // Don't touch me !!
     @objc public var use_default_auto_upload:           Bool = false
     @objc public var use_themingColor:                  Bool = true
     @objc public var use_themingBackground:             Bool = true
