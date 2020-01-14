9efe8addc4913ca30e5401fc7c0737780a3b8a80
marinofaggiana <marino@marinofaggiana.com>
2020-01-14 17:17:51 +0100

    #1037 - Build 6

--- iOSClient/RichWorkspace/NCRichWorkspace.swift
+++ iOSClient/RichWorkspace/NCRichWorkspace.swift
@@ -11,6 +11,7 @@ import Foundation
 @objc class NCViewRichWorkspace: UIView {
     
     @IBOutlet weak var webView: WKWebView!
+    var richWorkspace: String = ""
 
     required init?(coder: NSCoder) {
         super.init(coder: coder)
@@ -21,27 +22,35 @@ import Foundation
     
     @objc func changeTheming() {
         self.backgroundColor = NCBrandColor.sharedInstance.backgroundView;
+        setRichWorkspaceText(richWorkspace)
     }
     
     @objc func setRichWorkspaceText(_ richWorkspace: String) {
         
+        var color =  "#000000"
+        if CCUtility.getDarkMode() {
+            color =  "#FFFFFF"
+        }
+    
+        self.richWorkspace = richWorkspace
         var richWorkspaceHtml = ""
         let richWorkspaceArray = richWorkspace.components(separatedBy: "\n")
+        
         for string in richWorkspaceArray {
             if string.hasPrefix("# ") {
-                richWorkspaceHtml = richWorkspaceHtml + "<h1><span style=\"color: #000000;\">" + string.replacingOccurrences(of: "# ", with: "") + "</span></h1>"
+                richWorkspaceHtml = richWorkspaceHtml + "<h1><span style=\"color: \(color);\">" + string.replacingOccurrences(of: "# ", with: "") + "</span></h1>"
             } else if string.hasPrefix("## ") {
-                richWorkspaceHtml = richWorkspaceHtml + "<h2><span style=\"color: #000000;\">" + string.replacingOccurrences(of: "## ", with: "") + "</span></h2>"
+                richWorkspaceHtml = richWorkspaceHtml + "<h2><span style=\"color: \(color);\">" + string.replacingOccurrences(of: "## ", with: "") + "</span></h2>"
             } else if string.hasPrefix("### ") {
-                richWorkspaceHtml = richWorkspaceHtml + "<h3><span style=\"color: #000000;\">" + string.replacingOccurrences(of: "### ", with: "") + "</span></h3>"
+                richWorkspaceHtml = richWorkspaceHtml + "<h3><span style=\"color: \(color);\">" + string.replacingOccurrences(of: "### ", with: "") + "</span></h3>"
             } else if string.hasPrefix("#### ") {
-                richWorkspaceHtml = richWorkspaceHtml + "<h4><span style=\"color: #000000;\">" + string.replacingOccurrences(of: "#### ", with: "") + "</span></h4>"
+                richWorkspaceHtml = richWorkspaceHtml + "<h4><span style=\"color: \(color);\">" + string.replacingOccurrences(of: "#### ", with: "") + "</span></h4>"
             } else if string.hasPrefix("##### ") {
-                richWorkspaceHtml = richWorkspaceHtml + "<h5><span style=\"color: #000000;\">" + string.replacingOccurrences(of: "##### ", with: "") + "</span></h5>"
+                richWorkspaceHtml = richWorkspaceHtml + "<h5><span style=\"color: \(color);\">" + string.replacingOccurrences(of: "##### ", with: "") + "</span></h5>"
             } else if string.hasPrefix("###### ") {
-                richWorkspaceHtml = richWorkspaceHtml + "<h6><span style=\"color: #000000;\">" + string.replacingOccurrences(of: "###### ", with: "") + "</span></h6>"
+                richWorkspaceHtml = richWorkspaceHtml + "<h6><span style=\"color: \(color);\">" + string.replacingOccurrences(of: "###### ", with: "") + "</span></h6>"
             } else {
-                richWorkspaceHtml = richWorkspaceHtml + "<span style=\"color: #000000;\">" + string + "</span>"
+                richWorkspaceHtml = richWorkspaceHtml + "<span style=\"color: \(color);\">" + string + "</span>"
             }
             richWorkspaceHtml = richWorkspaceHtml + "<br>"
         }
@@ -50,5 +59,34 @@ import Foundation
         
         webView.loadHTMLString(richWorkspaceHtml, baseURL: Bundle.main.bundleURL)
         webView.isUserInteractionEnabled = false
+        webView.isOpaque = false
+    }
+}
+
+extension UIColor {
+    public convenience init?(hex: String) {
+        let r, g, b, a: CGFloat
+
+        if hex.hasPrefix("#") {
+            let start = hex.index(hex.startIndex, offsetBy: 1)
+            let hexColor = String(hex[start...])
+
+            if hexColor.count == 8 {
+                let scanner = Scanner(string: hexColor)
+                var hexNumber: UInt64 = 0
+
+                if scanner.scanHexInt64(&hexNumber) {
+                    r = CGFloat((hexNumber & 0xff000000) >> 24) / 255
+                    g = CGFloat((hexNumber & 0x00ff0000) >> 16) / 255
+                    b = CGFloat((hexNumber & 0x0000ff00) >> 8) / 255
+                    a = CGFloat(hexNumber & 0x000000ff) / 255
+
+                    self.init(red: r, green: g, blue: b, alpha: a)
+                    return
+                }
+            }
+        }
+
+        return nil
     }
 }
