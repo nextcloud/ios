0254c914990ee8cfa16f3f571c45030367895881
marinofaggiana <marino@marinofaggiana.com>
2020-01-24 15:56:17 +0100

    Update (c)

--- iOSClient/Brand/NCBrand.swift
+++ iOSClient/Brand/NCBrand.swift
@@ -49,7 +49,7 @@ import UIKit
     
     @objc public var brand:                             String = "Nextcloud"
     @objc public var mailMe:                            String = "ios@nextcloud.com"
-    @objc public var textCopyrightNextcloudiOS:         String = "Nextcloud for iOS %@ © 2019"
+    @objc public var textCopyrightNextcloudiOS:         String = "Nextcloud for iOS %@ © 2020"
     @objc public var textCopyrightNextcloudServer:      String = "Nextcloud Server %@"
     @objc public var loginBaseUrl:                      String = "https://cloud.nextcloud.com"
     @objc public var pushNotificationServerProxy:       String = "https://push-notifications.nextcloud.com"
