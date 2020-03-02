e326ee0d5349f8faa2de4b3972fd106d54b75be6
marinofaggiana <marino@marinofaggiana.com>
2020-03-02 14:41:16 +0100

    dev

--- iOSClient/Main/NCDetailViewController.swift
+++ iOSClient/Main/NCDetailViewController.swift
@@ -136,8 +136,9 @@ class NCDetailViewController: UIViewController {
     
     @objc func moveFile(_ notification: NSNotification) {
         if let userInfo = notification.userInfo as NSDictionary? {
-            if let metadata = userInfo["metadata"] as? tableMetadata, let metadataNew = userInfo["metadataNew"] as? tableMetadata, let errorCode = userInfo["errorCode"] as? Int {
-                if errorCode != 0 { return }
+            if let metadata = userInfo["metadata"] as? tableMetadata, let errorCode = userInfo["errorCode"] as? Int {
+                if errorCode != 0 || metadata.serverUrl != self.metadata?.serverUrl { return }
+                self.deleteFile(notification)
             }
         }
     }
