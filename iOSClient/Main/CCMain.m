923160f747daaf6da9820b0cf8bb5186cd614040
marinofaggiana <marino@marinofaggiana.com>
2020-03-16 12:22:52 +0100

    dev

--- iOSClient/Main/CCMain.m
+++ iOSClient/Main/CCMain.m
@@ -1130,7 +1130,7 @@
             (void)[[NCManageDatabase sharedInstance] addMetadata:metadataForUpload];
             
             // Add Medtadata MOV LIVE PHOTO for upload
-            if (asset.mediaSubtypes == PHAssetMediaSubtypePhotoLive && CCUtility.getMOVLivePhoto) {
+            if (asset.mediaSubtypes == PHAssetMediaSubtypePhotoLive && CCUtility.getMOVLivePhoto && urls.count == assets.count) {
                 
                 NSUInteger index = [assets indexOfObject:asset];
                 NSURL *url = [urls objectAtIndex:index];
