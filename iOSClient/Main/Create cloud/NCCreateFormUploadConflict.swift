e5103aae5ed41e7a741525dcfee1f39e5f9334b2
marinofaggiana <marino@marinofaggiana.com>
2020-03-29 17:05:04 +0200

    dev

--- iOSClient/Main/Create cloud/NCCreateFormUploadConflict.swift
+++ iOSClient/Main/Create cloud/NCCreateFormUploadConflict.swift
@@ -82,7 +82,7 @@ import Foundation
 extension NCCreateFormUploadConflict: UITableViewDelegate {
     
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return 125
+        return 135
     }
 }
 
