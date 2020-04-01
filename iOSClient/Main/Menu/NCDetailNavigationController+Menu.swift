8f06a3ef837121f3f5954e22107b88eec8528026
marinofaggiana <marino@marinofaggiana.com>
2020-04-01 11:52:15 +0200

    dev

--- iOSClient/Main/Menu/NCDetailNavigationController+Menu.swift
+++ iOSClient/Main/Menu/NCDetailNavigationController+Menu.swift
@@ -160,6 +160,8 @@ extension NCDetailNavigationController {
     }
 
     @objc func toggleMoreMenu(viewController: UIViewController, metadata: tableMetadata) {
+        // Refresh metadata
+        guard let metadata = NCManageDatabase.sharedInstance.getMetadata(predicate: NSPredicate(format: "ocId == %@", metadata.ocId)) else { return }
         if appDelegate.activeDetail.subViewActive() != nil {
             let mainMenuViewController = UIStoryboard.init(name: "NCMenu", bundle: nil).instantiateViewController(withIdentifier: "NCMainMenuTableViewController") as! NCMainMenuTableViewController
             mainMenuViewController.actions = self.initMoreMenu(viewController: viewController, metadata: metadata)
