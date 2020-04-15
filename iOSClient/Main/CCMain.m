1063658556f8ee856196661fae9a521e2ece354c
marinofaggiana <marino@marinofaggiana.com>
2020-04-15 15:33:19 +0200

    change errata parameter

--- iOSClient/Main/CCMain.m
+++ iOSClient/Main/CCMain.m
@@ -1308,12 +1308,12 @@
             
             // E2EE Is encrypted folder get metadata
             if (_metadataFolder.e2eEncrypted) {
-                NSString *metadataFolderocId = metadataFolder.ocId;
+                NSString *metadataFolderFileId = metadataFolder.fileId;
                 // Read Metadata
                 if ([CCUtility isEndToEndEnabled:account]) {
                     dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_LOW, 0), ^{
                         NSString *metadata;
-                        NSError *error = [[NCNetworkingEndToEnd sharedManager] getEndToEndMetadata:&metadata ocId:metadataFolderocId user:appDelegate.activeUser userID:appDelegate.activeUserID password:[CCUtility getPassword:appDelegate.activeAccount] url:appDelegate.activeUrl];
+                        NSError *error = [[NCNetworkingEndToEnd sharedManager] getEndToEndMetadata:&metadata fileId:metadataFolderFileId user:appDelegate.activeUser userID:appDelegate.activeUserID password:[CCUtility getPassword:appDelegate.activeAccount] url:appDelegate.activeUrl];
                         dispatch_async(dispatch_get_main_queue(), ^{
                             if (error) {
                                 if (error.code != kOCErrorServerPathNotFound)
