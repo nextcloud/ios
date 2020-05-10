860b3e218ebacee8af779967adfff8634603f9cc
marinofaggiana <marino@marinofaggiana.com>
2020-05-10 11:51:34 +0200

    fix

--- iOSClient/Login/CCLogin.m
+++ iOSClient/Login/CCLogin.m
@@ -169,7 +169,7 @@
 - (BOOL)textFieldShouldReturn:(UITextField *)textField
 {
     [textField resignFirstResponder];
-    return YES;
+    return NO;
 }
 
 #pragma --------------------------------------------------------------------------------------------
