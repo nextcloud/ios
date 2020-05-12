c5a8e652892289f42b0de1692e1f43d810e99971
Philippe Weidmann <philippe.weidmann@infomaniak.com>
2020-05-12 16:02:21 +0200

    New tabbar with shadow and indent
    Signed-off-by: Philippe Weidmann <philippe.weidmann@infomaniak.com>

--- iOSClient/Main/NCMainTabBar.swift
+++ iOSClient/Main/NCMainTabBar.swift
@@ -34,9 +34,8 @@ import Foundation
     private func addShape() {
         let shapeLayer = CAShapeLayer()
         shapeLayer.path = createPath()
-        shapeLayer.strokeColor = UIColor.lightGray.cgColor
-        shapeLayer.fillColor = UIColor.white.cgColor
-        shapeLayer.lineWidth = 1.0
+        shapeLayer.fillColor = NCBrandColor.sharedInstance.tabBar.cgColor
+
 
         if let oldShapeLayer = self.shapeLayer {
             self.layer.replaceSublayer(oldShapeLayer, with: shapeLayer)
@@ -47,25 +46,30 @@ import Foundation
         self.shapeLayer = shapeLayer
     }
 
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        self.layer.shadowPath = createPath()
+        self.layer.shadowRadius = 5
+        self.layer.shadowOffset = .zero
+        self.layer.shadowOpacity = 0.25
+    }
+
     override func draw(_ rect: CGRect) {
         self.addShape()
     }
 
     func createPath() -> CGPath {
-
-        let height: CGFloat = 37.0
+        let height: CGFloat = 16
+        let depth: CGFloat = height * 2 + 2
         let path = UIBezierPath()
         let centerWidth = self.frame.width / 2
 
         path.move(to: CGPoint(x: 0, y: 0)) // start top left
-        path.addLine(to: CGPoint(x: (centerWidth - height * 2), y: 0)) // the beginning of the trough
+        path.addLine(to: CGPoint(x: (centerWidth - height * 2) - 5, y: 0)) // the beginning of the trough
         // first curve down
-        path.addCurve(to: CGPoint(x: centerWidth, y: height),
-            controlPoint1: CGPoint(x: (centerWidth - 30), y: 0), controlPoint2: CGPoint(x: centerWidth - 35, y: height))
+        path.addQuadCurve(to: CGPoint(x: centerWidth, y: depth), controlPoint: CGPoint(x: centerWidth - height * 2, y: depth))
         // second curve up
-        path.addCurve(to: CGPoint(x: (centerWidth + height * 2), y: 0),
-            controlPoint1: CGPoint(x: centerWidth + 35, y: height), controlPoint2: CGPoint(x: (centerWidth + 30), y: 0))
-
+        path.addQuadCurve(to: CGPoint(x: centerWidth + height * 2 + 5, y: 0), controlPoint: CGPoint(x: centerWidth + height * 2, y: depth))
         // complete the rect
         path.addLine(to: CGPoint(x: self.frame.width, y: 0))
         path.addLine(to: CGPoint(x: self.frame.width, y: self.frame.height))
@@ -88,7 +92,6 @@ import Foundation
     }
 
     func createPathCircle() -> CGPath {
-
         let radius: CGFloat = 37.0
         let path = UIBezierPath()
         let centerWidth = self.frame.width / 2
