9cc992005cb80114e9a3294dcb85d8277ab727d8
Philippe Weidmann <philippe.weidmann@infomaniak.com>
2020-05-14 14:09:57 +0200

    Fix dark mode for navbar
    Signed-off-by: Philippe Weidmann <philippe.weidmann@infomaniak.com>

--- iOSClient/Main/CCMain+Swift.swift
+++ iOSClient/Main/CCMain+Swift.swift
@@ -15,11 +15,15 @@ extension CCMain {
             if #available(iOS 13.0, *) {
                 let navBarAppearance = UINavigationBarAppearance()
                 navBarAppearance.configureWithOpaqueBackground()
-                navBarAppearance.backgroundColor = .systemBackground
+                navBarAppearance.largeTitleTextAttributes = [NSAttributedString.Key.foregroundColor : NCBrandColor.sharedInstance.textView]
+                navBarAppearance.titleTextAttributes = [NSAttributedString.Key.foregroundColor : NCBrandColor.sharedInstance.textView]
+                navBarAppearance.backgroundColor = NCBrandColor.sharedInstance.backgroundView
                 self.navigationController?.navigationBar.standardAppearance = navBarAppearance
                 self.navigationController?.navigationBar.scrollEdgeAppearance = navBarAppearance
             } else {
                 self.navigationController?.navigationBar.barStyle = .default
+                self.navigationController?.navigationBar.titleTextAttributes = [NSAttributedString.Key.foregroundColor : NCBrandColor.sharedInstance.textView]
+                self.navigationController?.navigationBar.largeTitleTextAttributes = [NSAttributedString.Key.foregroundColor : NCBrandColor.sharedInstance.textView]
                 self.navigationController?.navigationBar.barTintColor = NCBrandColor.sharedInstance.backgroundView
                 self.navigationController?.navigationBar.tintColor = NCBrandColor.sharedInstance.brand
                 self.navigationController?.navigationBar.shadowImage = nil
@@ -31,13 +35,17 @@ extension CCMain {
             if #available(iOS 13.0, *) {
                 let navBarAppearance = UINavigationBarAppearance()
                 navBarAppearance.configureWithOpaqueBackground()
-                navBarAppearance.backgroundColor = .systemBackground
+                navBarAppearance.largeTitleTextAttributes = [NSAttributedString.Key.foregroundColor : NCBrandColor.sharedInstance.textView]
+                navBarAppearance.titleTextAttributes = [NSAttributedString.Key.foregroundColor : NCBrandColor.sharedInstance.textView]
+                navBarAppearance.backgroundColor = NCBrandColor.sharedInstance.backgroundView
                 navBarAppearance.shadowColor = .clear
                 navBarAppearance.shadowImage = UIImage()
                 self.navigationController?.navigationBar.standardAppearance = navBarAppearance
                 self.navigationController?.navigationBar.scrollEdgeAppearance = navBarAppearance
             } else {
                 self.navigationController?.navigationBar.barStyle = .default
+                self.navigationController?.navigationBar.titleTextAttributes = [NSAttributedString.Key.foregroundColor : NCBrandColor.sharedInstance.textView]
+                self.navigationController?.navigationBar.largeTitleTextAttributes = [NSAttributedString.Key.foregroundColor : NCBrandColor.sharedInstance.textView]
                 self.navigationController?.navigationBar.barTintColor = NCBrandColor.sharedInstance.backgroundView
                 self.navigationController?.navigationBar.tintColor = NCBrandColor.sharedInstance.brand
                 self.navigationController?.navigationBar.shadowImage = UIImage()
