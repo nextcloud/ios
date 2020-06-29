9ad2c8e9f80853d7871a1dd60b8d16b492487953
marinofaggiana <marino@marinofaggiana.com>
2020-06-29 16:29:19 +0200

    coding

--- iOSClient/Main/Menu/CCMain+Menu.swift
+++ iOSClient/Main/Menu/CCMain+Menu.swift
@@ -322,7 +322,7 @@ extension CCMain {
                         action: { menuAction in
                             let serverUrl = CCUtility.stringAppendServerUrl(metadata.serverUrl, addFileName: metadata.fileName)!
                             NCManageDatabase.sharedInstance.setDirectory(serverUrl: serverUrl, offline: !isOffline, account: appDelegate.activeAccount)
-                            if(isOffline) {
+                            if (!isOffline) {
                                 CCSynchronize.shared()?.readFolder(serverUrl, selector: selectorReadFolderWithDownload, account: appDelegate.activeAccount)
                             }
                             NotificationCenter.default.postOnMainThread(name: k_notificationCenter_reloadDataSource, userInfo: ["ocId":metadata.ocId, "serverUrl":metadata.serverUrl])
