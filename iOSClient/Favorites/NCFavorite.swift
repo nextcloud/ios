84fb7f79acfd0d42d4ce8a9659cd8babeba6da6c
marinofaggiana <marino@marinofaggiana.com>
2020-09-09 15:38:09 +0200

    coding

--- iOSClient/Favorites/NCFavorite.swift
+++ iOSClient/Favorites/NCFavorite.swift
@@ -600,7 +600,7 @@ extension NCFavorite {
             NCNetworking.shared.readFolder(serverUrl: serverUrl, account: appDelegate.account) { (account, metadataFolder, metadatas, metadatasUpdate, metadatasLocalUpdate, errorCode, errorDescription) in
                 if errorCode == 0 {
                     for metadata in metadatas ?? [] {
-                        if !metadata.directory {
+                        if !metadata.directory && CCUtility.getFavoriteOffline() {                            
                             let localFile = NCManageDatabase.sharedInstance.getTableLocalFile(predicate: NSPredicate(format: "ocId == %@", metadata.ocId))
                             if localFile == nil || localFile?.etag != metadata.etag {
                                 NCOperationQueue.shared.download(metadata: metadata, selector: selectorDownloadFile, setFavorite: false)
