d096cc0aad83d6f5b11c7536121dd96c2f22fc25
marinofaggiana <marino@marinofaggiana.com>
2020-09-16 22:21:40 +0200

    coding

--- iOSClient/Main/Menu/NCCollectionViewCommon+Menu.swift
+++ iOSClient/Main/Menu/NCCollectionViewCommon+Menu.swift
@@ -158,21 +158,20 @@ extension NCCollectionViewCommon {
                     
                     alertController.addTextField { (textField) in
                         textField.text = metadata.fileNameView
-                        textField.delegate = self as? UITextFieldDelegate
-                        textField.addTarget(self, action: #selector(self.minCharTextFieldDidChange(sender:)), for: UIControl.Event.editingChanged)
                     }
 
                     let cancelAction = UIAlertAction(title: NSLocalizedString("_cancel_", comment: ""), style: .cancel, handler: nil)
 
                     let okAction = UIAlertAction(title: NSLocalizedString("_ok_", comment: ""), style: .default, handler: { action in
-                        let fileNameNew = alertController.textFields![0].text
-                        NCNetworking.shared.renameMetadata(metadata, fileNameNew: fileNameNew!, urlBase: appDelegate.urlBase, viewController: self) { (errorCode, errorDescription) in
-                            if errorCode != 0 {
-                                NCContentPresenter.shared.messageNotification("_error_", description: errorDescription, delay: TimeInterval(k_dismissAfterSecond), type: NCContentPresenter.messageType.error, errorCode: errorCode)
+                        if let fileNameNew = alertController.textFields?.first?.text {
+                            NCNetworking.shared.renameMetadata(metadata, fileNameNew: fileNameNew, urlBase: appDelegate.urlBase, viewController: self) { (errorCode, errorDescription) in
+                                if errorCode != 0 {
+                                    NCContentPresenter.shared.messageNotification("_error_", description: errorDescription, delay: TimeInterval(k_dismissAfterSecond), type: NCContentPresenter.messageType.error, errorCode: errorCode)
+                                }
                             }
                         }
                     })
-                    okAction.isEnabled = false
+
                     alertController.addAction(cancelAction)
                     alertController.addAction(okAction)
 
