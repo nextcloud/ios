0387e0c6dd33583c0523fd766a4f8b886e3e92fd
marinofaggiana <marino@marinofaggiana.com>
2020-09-28 10:11:57 +0200

    coding

--- iOSClient/Networking/NCService.swift
+++ iOSClient/Networking/NCService.swift
@@ -160,7 +160,7 @@ class NCService: NSObject {
             
                 // File Sharing
                 let isFilesSharingEnabled = NCManageDatabase.sharedInstance.getCapabilitiesServerBool(account: account, elements: NCElementsJSON.shared.capabilitiesFileSharingApiEnabled, exists: false)
-                if (isFilesSharingEnabled && self.appDelegate.activeFiles != nil) {
+                if isFilesSharingEnabled {
                     NCCommunication.shared.readShares { (account, shares, errorCode, ErrorDescription) in
                         if errorCode == 0 {
                             NCManageDatabase.sharedInstance.deleteTableShare(account: account)
