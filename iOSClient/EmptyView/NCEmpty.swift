44ee69808ac57f6405aa89fec4825280658be5d2
marinofaggiana <marino@marinofaggiana.com>
2020-10-19 11:03:40 +0200

    coding
    Signed-off-by: Fredrik Rentsch <fredrik@rentsch.no>

---
+++ iOSClient/EmptyView/NCEmpty.swift
@@ -0,0 +1,22 @@
+//
+//  NCEmpty.swift
+//  Nextcloud
+//
+//  Created by Marino Faggiana on 19/10/2020.
+//  Copyright © 2020 Marino Faggiana. All rights reserved.
+//
+
+import Foundation
+
+class NCEmpty: NSObject {
+    
+    
+}
+
+class NCEmptyView: UIView {
+    
+    @IBOutlet weak var emptyImage: UIImageView!
+    @IBOutlet weak var emptyTtle: UILabel!
+    @IBOutlet weak var emptyDescription: UILabel!
+}
+
