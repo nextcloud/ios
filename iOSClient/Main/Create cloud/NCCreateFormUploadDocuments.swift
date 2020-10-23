83af5a616e61b52734236dde747532a80d0d7532
marinofaggiana <marino@marinofaggiana.com>
2020-10-23 12:33:27 +0200

    new view image

--- iOSClient/Main/Create cloud/NCCreateFormUploadDocuments.swift
+++ iOSClient/Main/Create cloud/NCCreateFormUploadDocuments.swift
@@ -333,7 +333,7 @@ import NCCommunication
                         self.dismiss(animated: true, completion: {
                             let metadata = NCManageDatabase.sharedInstance.createMetadata(account: self.appDelegate.account, fileName: fileName, ocId: CCUtility.createRandomString(12), serverUrl: self.serverUrl, urlBase: self.appDelegate.urlBase, url: url ?? "", contentType: result.contentType, livePhoto: false)
                             
-                            NCViewer.shared.view(viewController: self.appDelegate.activeViewController, metadata: metadata)
+                            NCViewer.shared.view(viewController: self.appDelegate.activeViewController, metadata: metadata, metadatas: [metadata])
                         })
                     }
                     
@@ -355,7 +355,7 @@ import NCCommunication
                     
                         let metadata = NCManageDatabase.sharedInstance.createMetadata(account: self.appDelegate.account, fileName: (fileName as NSString).deletingPathExtension + "." + self.fileNameExtension, ocId: CCUtility.createRandomString(12), serverUrl: self.serverUrl, urlBase: self.appDelegate.urlBase, url: url!, contentType: "", livePhoto: false)
                     
-                        NCViewer.shared.view(viewController: self.appDelegate.activeViewController, metadata: metadata) 
+                        NCViewer.shared.view(viewController: self.appDelegate.activeViewController, metadata: metadata, metadatas: [metadata])
                    })
                    
                     
