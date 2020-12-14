03c03728a41b28e7c16a1e8fa16ae8d5a4b7ea01
marinofaggiana <marino@marinofaggiana.com>
2020-12-14 17:49:54 +0100

    clear code

--- iOSClient/Main/Menu/AppDelegate+Menu.swift
+++ iOSClient/Main/Menu/AppDelegate+Menu.swift
@@ -112,7 +112,7 @@ extension AppDelegate: NCAudioRecorderViewControllerDelegate {
         
         actions.append(
             NCMenuAction(
-                title: NSLocalizedString("_create_voice_memo_", comment: ""), icon: UIImage(named: "microphone")!.image(color: NCBrandColor.shared.icon, size: 20), action: { menuAction in
+                title: NSLocalizedString("_create_voice_memo_", comment: ""), icon: UIImage(named: "microphone")!.image(color: NCBrandColor.shared.icon, size: 50), action: { menuAction in
                     
                     let fileName = CCUtility.createFileNameDate(NSLocalizedString("_voice_memo_filename_", comment: ""), extension: "m4a")!
                     let viewController = UIStoryboard(name: "NCAudioRecorderViewController", bundle: nil).instantiateInitialViewController() as! NCAudioRecorderViewController
