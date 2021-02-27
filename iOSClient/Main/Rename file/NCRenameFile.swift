95edb27005635ff8f174c579f1a71dc09f0bb7cf
marinofaggiana <marino@marinofaggiana.com>
2021-02-27 16:07:58 +0100

    new rename view

--- iOSClient/Main/Rename file/NCRenameFile.swift
+++ iOSClient/Main/Rename file/NCRenameFile.swift
@@ -5,6 +5,21 @@
 //  Created by Marino Faggiana on 26/02/21.
 //  Copyright © 2021 Marino Faggiana. All rights reserved.
 //
+//  Author Marino Faggiana <marino.faggiana@nextcloud.com>
+//
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
 
 import Foundation
 import NCCommunication
@@ -169,9 +184,12 @@ class NCRenameFile: UIViewController, UITextFieldDelegate {
     func renameMetadata(_ metadata: tableMetadata, fileNameNew: String) {
         
         NCNetworking.shared.renameMetadata(metadata, fileNameNew: fileNameNew, urlBase: metadata.urlBase, viewController: self) { (errorCode, errorDescription) in
+            
             if errorCode == 0 {
+                
                 self.dismiss(animated: true)
             } else {
+                
                 NCContentPresenter.shared.messageNotification("_error_", description: errorDescription, delay: NCGlobal.shared.dismissAfterSecond, type: NCContentPresenter.messageType.error, errorCode: errorCode)
             }
         }
