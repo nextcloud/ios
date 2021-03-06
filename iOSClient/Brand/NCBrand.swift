cd0630fc53bc4517e665dc05922a23c0cf7b721c
marinofaggiana <marino@marinofaggiana.com>
2021-03-06 10:48:51 +0100

    fix color

--- iOSClient/Brand/NCBrand.swift
+++ iOSClient/Brand/NCBrand.swift
@@ -147,7 +147,7 @@ class NCBrandColor: NSObject {
         } else {
             tabBar = .white
             backgroundView = .white
-            backgroundForm = UIColor(red: 247.0/255.0, green: 247.0/255.0, blue: 247.0/255.0, alpha: 1.0)
+            backgroundForm = UIColor(red: 242.0/255.0, green: 242.0/255.0, blue: 247.0/255.0, alpha: 1.0)
             textView = .black
             separator = UIColor(red: 235.0/255.0, green: 235.0/255.0, blue: 235.0/255.0, alpha: 1.0)
             select = self.brandElement.withAlphaComponent(0.1)
