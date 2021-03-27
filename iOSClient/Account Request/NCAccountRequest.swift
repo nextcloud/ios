c688cc50b5e972c5a10004079e86496d0040701b
marinofaggiana <marino@marinofaggiana.com>
2021-03-27 20:41:11 +0100

    fix color

--- iOSClient/Account Request/NCAccountRequest.swift
+++ iOSClient/Account Request/NCAccountRequest.swift
@@ -197,7 +197,7 @@ extension NCAccountRequest: UITableViewDataSource {
         
         if indexPath.row == accounts.count {
            
-            avatarImage?.image = NCUtility.shared.loadImage(named: "plus").image(color: .black, size: 25)
+            avatarImage?.image = NCUtility.shared.loadImage(named: "plus").image(color: NCBrandColor.shared.textView, size: 25)
             avatarImage?.contentMode = .center
             userLabel?.text = NSLocalizedString("_add_account_", comment: "")
             
