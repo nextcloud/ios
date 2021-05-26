27676ae981f4401fe6e78ef3bfdbdc763627b797
marinofaggiana <marino@marinofaggiana.com>
2021-05-26 08:03:17 +0200

    clear code

--- iOSClient/Extensions/UIImage+animatedGIF.m
+++ iOSClient/Extensions/UIImage+animatedGIF.m
@@ -1,5 +1,28 @@
+//
+//  UIImage+animatedGIF
+//  Nextcloud
+//
+//  Created by Marino Faggiana on 28/05/17.
+//  Copyright (c) 2017 Marino Faggiana. All rights reserved.
+//
+//  Author Marino Faggiana <marino.faggiana@nextcloud.com>
+//  Found in Internet
+//
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+
 #import "UIImage+animatedGIF.h"
-#import <ImageIO/ImageIO.h>
 
 #if __has_feature(objc_arc)
 #define toCF (__bridge CFTypeRef)
