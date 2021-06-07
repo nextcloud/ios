149a7e8f348fbf6d0201c60bc2972ad964a4f3b1
marinofaggiana <marino@marinofaggiana.com>
2021-06-07 09:47:48 +0200

    coding

--- iOSClient/Recent/NCRecent.swift
+++ iOSClient/Recent/NCRecent.swift
@@ -53,7 +53,7 @@ class NCRecent: NCCollectionViewCommon  {
         DispatchQueue.global().async {
             
             self.metadatasSource = NCManageDatabase.shared.getAdvancedMetadatas(predicate: NSPredicate(format: "account == %@", self.appDelegate.account), page: 1, limit: 100, sorted: "date", ascending: false)
-            self.dataSource = NCDataSource.init(metadatasSource: self.metadatasSource, favoriteOnTop: false)
+            self.dataSource = NCDataSource.init(metadatasSource: self.metadatasSource, directoryOnTop: false, favoriteOnTop: false)
             
             DispatchQueue.main.async {
                 self.refreshControl.endRefreshing()
