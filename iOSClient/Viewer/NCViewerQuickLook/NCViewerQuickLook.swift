3c457bbd9d4ea8a2809a4a5d8aef604ef3f73f3a
marinofaggiana <marino@marinofaggiana.com>
2021-06-09 09:31:02 +0200

    coding

--- iOSClient/Viewer/NCViewerQuickLook/NCViewerQuickLook.swift
+++ iOSClient/Viewer/NCViewerQuickLook/NCViewerQuickLook.swift
@@ -67,7 +67,7 @@ import QuickLook
         super.viewDidLoad()
         
         if editingMode && metadata?.typeFile == NCGlobal.shared.metadataTypeFileImage {
-            Timer.scheduledTimer(withTimeInterval: 0.5, repeats: true, block: { (t) in
+            Timer.scheduledTimer(withTimeInterval: 0.1, repeats: true, block: { (t) in
                 if self.navigationItem.rightBarButtonItems?.count ?? 0 > 1 || !(self.navigationController?.isToolbarHidden ?? false) {
                     if #available(iOS 14.0, *) {
                         if self.navigationItem.rightBarButtonItems?.count ?? 0 > 1 {
