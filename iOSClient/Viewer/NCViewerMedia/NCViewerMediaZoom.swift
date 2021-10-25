09f02c2f44bc78f6b2ebcf3aa6034311cc90e5cc
marinofaggiana <marino@marinofaggiana.com>
2021-10-25 16:10:32 +0200

    Improved download preview image
    Signed-off-by: marinofaggiana <marino@marinofaggiana.com>

--- iOSClient/Viewer/NCViewerMedia/NCViewerMediaZoom.swift
+++ iOSClient/Viewer/NCViewerMedia/NCViewerMediaZoom.swift
@@ -89,21 +89,18 @@ class NCViewerMediaZoom: UIViewController {
                 image = UIImage.init(named: "noPreviewVideo")!.image(color: .gray, size: view.frame.width)
             }
             imageVideoContainer.image = image
-            imageVideoContainer.sourceImage = image
             
         } else if metadata.classFile == NCCommunicationCommon.typeClassFile.audio.rawValue {
             if image == nil {
                 image = UIImage.init(named: "noPreviewAudio")!.image(color: .gray, size: view.frame.width)
             }
             imageVideoContainer.image = image
-            imageVideoContainer.sourceImage = image
 
         } else {
             if image == nil {
                 image = UIImage.init(named: "noPreview")!.image(color: .gray, size: view.frame.width)
             }
             imageVideoContainer.image = image
-            imageVideoContainer.sourceImage = image
         }
         
         if NCManageDatabase.shared.getMetadataLivePhoto(metadata: metadata) != nil {
@@ -216,7 +213,6 @@ class NCViewerMediaZoom: UIViewController {
                 self.image = image
 
                 imageVideoContainer.image = image
-                imageVideoContainer.sourceImage = image
             }
             
         } else {
@@ -224,7 +220,6 @@ class NCViewerMediaZoom: UIViewController {
             self.image = image
 
             imageVideoContainer.image = image
-            imageVideoContainer.sourceImage = image
         }
     }
         
@@ -459,7 +454,6 @@ extension NCViewerMediaZoom: NCViewerMediaDetailViewDelegate  {
 class imageVideoContainerView: UIImageView {
     var playerLayer: CALayer?
     var metadata: tableMetadata?
-    var sourceImage: UIImage?
     override func layoutSublayers(of layer: CALayer) {
         super.layoutSublayers(of: layer)
         playerLayer?.frame = self.bounds
