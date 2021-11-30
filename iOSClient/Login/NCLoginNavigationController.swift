58452fc5b3ab15d6041a51b72799782cd994d6dc
marinofaggiana <marino@marinofaggiana.com>
2021-11-30 16:12:36 +0100

    PrivacyProtectionWindow
    Signed-off-by: marinofaggiana <marino@marinofaggiana.com>
    Signed-off-by: Harshal Sengar <harshal.sengar@t-systems.com>

---
+++ iOSClient/Login/NCLoginNavigationController.swift
@@ -0,0 +1,15 @@
+//
+//  NCLoginNavigationController.swift
+//  Nextcloud
+//
+//  Created by Marino Faggiana on 30/11/21.
+//  Copyright © 2021 Marino Faggiana. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import NCCommunication
+
+class NCLoginNavigationController: UINavigationController {
+    
+}
