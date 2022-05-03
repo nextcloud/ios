943fbdb1e68f056d6099772f0d82f4b578030c68
marinofaggiana <ios@nextcloud.com>
2022-05-03 10:09:34 +0200

    Revert color
    Signed-off-by: marinofaggiana <ios@nextcloud.com>

--- iOSClient/Viewer/NCViewerPDF/NCViewerPDF.swift
+++ iOSClient/Viewer/NCViewerPDF/NCViewerPDF.swift
@@ -132,7 +132,9 @@ class NCViewerPDF: UIViewController, NCViewerPDFSearchDelegate, UIGestureRecogni
 
         pageView.translatesAutoresizingMaskIntoConstraints = false
         pageView.layer.cornerRadius = 10
-        pageView.backgroundColor = UIColor.gray.withAlphaComponent(0.3)
+        pageView.backgroundColor = NCBrandColor.shared.systemBackground.withAlphaComponent(
+            UIAccessibility.isReduceTransparencyEnabled ? 1 : 0.5
+        )
         view.addSubview(pageView)
 
         NSLayoutConstraint.activate([
@@ -145,7 +147,7 @@ class NCViewerPDF: UIViewController, NCViewerPDFSearchDelegate, UIGestureRecogni
 
         pageViewLabel.translatesAutoresizingMaskIntoConstraints = false
         pageViewLabel.textAlignment = .center
-        pageViewLabel.textColor = .gray
+        pageViewLabel.textColor = NCBrandColor.shared.label
         pageView.addSubview(pageViewLabel)
 
         NSLayoutConstraint.activate([
