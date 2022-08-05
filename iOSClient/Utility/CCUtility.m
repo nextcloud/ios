56769e22068c14595483e77248ac7506e5c483f6
marinofaggiana <ios@nextcloud.com>
2022-08-05 13:28:22 +0200

    fix code
    Signed-off-by: marinofaggiana <ios@nextcloud.com>

--- iOSClient/Utility/CCUtility.m
+++ iOSClient/Utility/CCUtility.m
@@ -755,9 +755,7 @@
     
     NSError *error = nil;
     BOOL success = [URL setResourceValue:[NSNumber numberWithBool: YES] forKey: NSURLIsExcludedFromBackupKey error: &error];
-    if(success) {
-        NSLog(@"Excluding %@ from backup", [URL lastPathComponent]);
-    } else {
+    if(!success){
         NSLog(@"Error excluding %@ from backup %@", [URL lastPathComponent], error);
     }
     
