d0e343508515182e69b725518c49652201a23026
Marino Faggiana <marino@marinofaggiana.com>
2022-09-09 18:43:15 +0200

    Create DB dashboard 1
    Signed-off-by: Marino Faggiana <marino@marinofaggiana.com>

--- iOSClient/NCGlobal.swift
+++ iOSClient/NCGlobal.swift
@@ -113,7 +113,7 @@ class NCGlobal: NSObject {
     // Database Realm
     //
     let databaseDefault                             = "nextcloud.realm"
-    let databaseSchemaVersion: UInt64               = 237
+    let databaseSchemaVersion: UInt64               = 243
 
     // Intro selector
     //
