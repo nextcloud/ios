d508c9f5f9c6b7622ead4dd83080b6adda9f34c2
Marino Faggiana <marino@marinofaggiana.com>
2022-11-15 17:07:41 +0100

    coding
    Signed-off-by: Marino Faggiana <marino@marinofaggiana.com>

--- iOSClient/Networking/E2EE/NCNetworkingE2EECreateFolder.swift
+++ iOSClient/Networking/E2EE/NCNetworkingE2EECreateFolder.swift
@@ -79,6 +79,16 @@ class NCNetworkingE2EECreateFolder: NSObject {
         let object = tableE2eEncryption()
         var method = "POST"
 
+        // Get last metadata
+        let getE2EEMetadataResults = await  NextcloudKit.shared.getE2EEMetadata(fileId: directory.fileId, e2eToken: e2eToken)
+        if getE2EEMetadataResults.error == .success, let e2eMetadata = getE2EEMetadataResults.e2eMetadata {
+            if !NCEndToEndMetadata.shared.decoderMetadata(e2eMetadata, privateKey: CCUtility.getEndToEndPrivateKey(account), serverUrl: serverUrl, account: account, urlBase: urlBase, userId: userId) {
+                return NKError(errorCode: NCGlobal.shared.errorInternalError, errorDescription: NSLocalizedString("_e2e_error_encode_metadata_", comment: ""))
+            }
+            method = "PUT"
+        }
+
+        // Add new metadata
         NCEndToEndEncryption.sharedManager()?.encryptkey(&key, initializationVector: &initializationVector)
         object.account = account
         object.authenticationTag = nil
@@ -98,15 +108,6 @@ class NCNetworkingE2EECreateFolder: NSObject {
         object.serverUrl = serverUrl
         object.version = 1
         NCManageDatabase.shared.addE2eEncryption(object)
-
-        // Get last metadata
-        let getE2EEMetadataResults = await  NextcloudKit.shared.getE2EEMetadata(fileId: directory.fileId, e2eToken: e2eToken)
-        if getE2EEMetadataResults.error == .success, let e2eMetadata = getE2EEMetadataResults.e2eMetadata {
-            if !NCEndToEndMetadata.shared.decoderMetadata(e2eMetadata, privateKey: CCUtility.getEndToEndPrivateKey(account), serverUrl: serverUrl, account: account, urlBase: urlBase, userId: userId) {
-                return NKError(errorCode: NCGlobal.shared.errorInternalError, errorDescription: NSLocalizedString("_e2e_error_encode_metadata_", comment: ""))
-            }
-            method = "PUT"
-        }
 
         // Rebuild metadata
         guard let tableE2eEncryption = NCManageDatabase.shared.getE2eEncryptions(predicate: NSPredicate(format: "account == %@ AND serverUrl == %@", account, serverUrl)), let e2eMetadataNew = NCEndToEndMetadata.shared.encoderMetadata(tableE2eEncryption, privateKey: CCUtility.getEndToEndPrivateKey(account), serverUrl: serverUrl) else {
