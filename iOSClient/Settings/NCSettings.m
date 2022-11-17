2293fafd881a6f9e96aaf83bd6fb1119289dda4c
Marino Faggiana <marino@marinofaggiana.com>
2022-11-17 14:05:49 +0100

    coding
    Signed-off-by: Marino Faggiana <marino@marinofaggiana.com>

--- iOSClient/Settings/NCSettings.m
+++ iOSClient/Settings/NCSettings.m
@@ -117,8 +117,8 @@
         [row.cellConfig setObject:[UIFont systemFontOfSize:15.0] forKey:@"textLabel.font"];
         [row.cellConfig setObject:UIColor.labelColor forKey:@"textLabel.textColor"];
         [row.cellConfig setObject:[[UIImage imageNamed:@"lock"] imageWithColor:NCBrandColor.shared.gray size:25] forKey:@"imageView.image"];
-        row.action.viewControllerClass = [NCManageEndToEndEncryption class];
-
+        row.action.formSelector = @selector(manageE2EE:);
+        //row.action.viewControllerClass = [NCManageEndToEndEncryption class];
         [section addFormRow:row];
     }
 
@@ -295,6 +295,14 @@
 }
 
 #pragma mark -
+
+- (void)manageE2EE:(XLFormRowDescriptor *)sender
+{
+    [self deselectFormRow:sender];
+
+    UIViewController *vc = [[NCManageE2EEInterface alloc] makeShipDetailsUI];
+    [self.navigationController pushViewController:vc animated:YES];
+}
 
 - (void)privacy:(XLFormRowDescriptor *)sender
 {
