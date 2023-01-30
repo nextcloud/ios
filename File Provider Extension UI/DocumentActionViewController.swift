f7c55e421d6e7b61fcc15e2d317aac18414987a4
Marino Faggiana <marino@marinofaggiana.com>
2023-01-30 17:08:29 +0100

    coding
    Signed-off-by: Marino Faggiana <marino@marinofaggiana.com>

--- File Provider Extension UI/DocumentActionViewController.swift
+++ File Provider Extension UI/DocumentActionViewController.swift
@@ -10,15 +10,15 @@ import UIKit
 import FileProviderUI
 
 class DocumentActionViewController: FPUIActionExtensionViewController {
-    
+
     @IBOutlet weak var identifierLabel: UILabel!
     @IBOutlet weak var actionTypeLabel: UILabel!
-    
+
     override func prepare(forAction actionIdentifier: String, itemIdentifiers: [NSFileProviderItemIdentifier]) {
         identifierLabel?.text = actionIdentifier
         actionTypeLabel?.text = "Custom action"
     }
-    
+
     override func prepare(forError error: Error) {
         identifierLabel?.text = error.localizedDescription
         actionTypeLabel?.text = "Authenticate"
@@ -28,10 +28,8 @@ class DocumentActionViewController: FPUIActionExtensionViewController {
         // Perform the action and call the completion block. If an unrecoverable error occurs you must still call the completion block with an error. Use the error code FPUIExtensionErrorCode.failed to signal the failure.
         extensionContext.completeRequest()
     }
-    
+
     @IBAction func cancelButtonTapped(_ sender: Any) {
         extensionContext.cancelRequest(withError: NSError(domain: FPUIErrorDomain, code: Int(FPUIExtensionErrorCode.userCancelled.rawValue), userInfo: nil))
     }
-    
 }
-
