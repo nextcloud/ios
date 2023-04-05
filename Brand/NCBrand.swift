34014ce450207bb894bada9afd4344c82698de19
Marino Faggiana <marino@marinofaggiana.com>
2023-04-05 14:41:45 +0200

    update comment
    Signed-off-by: Marino Faggiana <marino@marinofaggiana.com>

--- Brand/NCBrand.swift
+++ Brand/NCBrand.swift
@@ -79,6 +79,7 @@ import UIKit
     @objc public var disable_log:                       Bool = false
     @objc public var disable_mobileconfig:              Bool = false
 
+    // Internal option behaviour
     @objc public var cleanUpDay:                        Int = 0                                                     // Set default "Delete all file older than " possible days value are: 0, 1, 7, 30, 90, 180, 365
 
     override init() {
