2a6aa3a2b99767fe2f9e455c2fbc6510fbdf7239
Marino Faggiana <marino@marinofaggiana.com>
2023-04-24 16:28:00 +0200

    Update NCPlayer.swift

--- iOSClient/Viewer/NCViewerMedia/NCPlayer/NCPlayer.swift
+++ iOSClient/Viewer/NCViewerMedia/NCPlayer/NCPlayer.swift
@@ -294,18 +294,9 @@ extension NCPlayer: VLCMediaThumbnailerDelegate {
 
         do {
             image = UIImage(cgImage: thumbnail)
-            // Update Playing Info Center
-            // let mediaItemPropertyTitle = MPNowPlayingInfoCenter.default().nowPlayingInfo?[MPMediaItemPropertyTitle] as? String
-            // if let image = image, mediaItemPropertyTitle == metadata.fileNameView {
-            //    MPNowPlayingInfoCenter.default().nowPlayingInfo?[MPMediaItemPropertyArtwork] = MPMediaItemArtwork(boundsSize: image.size) { _ in
-            //        return image
-            //    }
-            // }
-            // Preview
             if let data = image?.jpegData(compressionQuality: 0.5) {
                 try data.write(to: URL(fileURLWithPath: fileNamePreviewLocalPath), options: .atomic)
             }
-            // Icon
             if let data = image?.jpegData(compressionQuality: 0.5) {
                 try data.write(to: URL(fileURLWithPath: fileNameIconLocalPath), options: .atomic)
             }
