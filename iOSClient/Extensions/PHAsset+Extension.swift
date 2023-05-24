69d8f73844eb12e798b098008c5b74e6085c3f6a
Milen Pivchev <milen.pivchev@gmail.com>
2023-05-24 11:12:47 +0200

    Replace more cases
    Signed-off-by: Milen Pivchev <milen.pivchev@gmail.com>

---
+++ iOSClient/Extensions/PHAsset+Extension.swift
@@ -0,0 +1,20 @@
+//
+//  PHAsset+Extension.swift
+//  Nextcloud
+//
+//  Created by Milen on 24.05.23.
+//  Copyright © 2023 Marino Faggiana. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+extension PHAsset {
+    var originalFilename: NSString {
+        if let resource = PHAssetResource.assetResources(for: self).first {
+            return resource.originalFilename as NSString
+        } else {
+            return ""
+        }
+    }
+}
