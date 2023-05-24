615160f2ac6f03c4d4d4ae836ec21b6607947a9f
Milen Pivchev <milen.pivchev@gmail.com>
2023-05-24 14:46:31 +0200

    Fix lint
    Signed-off-by: Milen Pivchev <milen.pivchev@gmail.com>

--- iOSClient/Extensions/PHAsset+Extension.swift
+++ iOSClient/Extensions/PHAsset+Extension.swift
@@ -14,7 +14,21 @@ extension PHAsset {
         if let resource = PHAssetResource.assetResources(for: self).first {
             return resource.originalFilename as NSString
         } else {
-            return self.value(forKey: "filename") as! NSString
+            return self.value(forKey: "filename") as? NSString
+            ?? ("IMG_" + CCUtility.getIncrementalNumber() + getExtension()) as NSString 
+        }
+    }
+
+    private func getExtension() -> String {
+        switch mediaType {
+        case .video:
+            return ".mp4"
+        case .image:
+            return ".jpg"
+        case .audio:
+            return ".mp3"
+        default:
+            return ".unknownType"
         }
     }
 }
