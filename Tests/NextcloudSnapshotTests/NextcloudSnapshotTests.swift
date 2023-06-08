918e1c5d8a14979fda5ee96af8e33d8fce7e44c0
Milen Pivchev <milen.pivchev@gmail.com>
2023-06-08 10:32:26 +0200

    Refactor
    Signed-off-by: Milen Pivchev <milen.pivchev@gmail.com>

--- Tests/NextcloudSnapshotTests/NextcloudSnapshotTests.swift
+++ Tests/NextcloudSnapshotTests/NextcloudSnapshotTests.swift
@@ -14,24 +14,11 @@ import SwiftUI
 @testable import Nextcloud
 
 final class NextcloudSnapshotTests: XCTestCase {
-
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    func test_HUDView() {
+        HUDView_Previews.snapshots.assertSnapshots(as: .imageHEIC)
     }
 
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        let contentView = NCCapabilitiesView(capabilitiesStatus: NCCapabilitiesViewOO())
-//        assertSnapshot(matching: contentView.toVC(), as: .recursiveDescription)
-
+    func test_CapalitiesView() {
         NCCapabilitiesView_Previews.snapshots.assertSnapshots(as: .imageHEIC)
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
     }
 }
