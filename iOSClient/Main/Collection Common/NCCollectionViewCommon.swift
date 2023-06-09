7609e5df6feaa4dfa5535f31cf8d940f47421957
Milen Pivchev <milen.pivchev@gmail.com>
2023-06-09 11:52:26 +0200

    Fix refresh control
    Signed-off-by: Milen Pivchev <milen.pivchev@gmail.com>

--- iOSClient/Main/Collection Common/NCCollectionViewCommon.swift
+++ iOSClient/Main/Collection Common/NCCollectionViewCommon.swift
@@ -123,7 +123,7 @@ class NCCollectionViewCommon: UIViewController, UIGestureRecognizerDelegate, UIS
         collectionView.register(UINib(nibName: "NCSectionFooter", bundle: nil), forSupplementaryViewOfKind: UICollectionView.elementKindSectionFooter, withReuseIdentifier: "sectionFooter")
 
         // Refresh Control
-        collectionView.addSubview(refreshControl)
+        collectionView.refreshControl = refreshControl
         refreshControl.action(for: .valueChanged) { _ in
             self.dataSource.clearDirectory()
             self.reloadDataSourceNetwork(forced: true)
