7db91c6f8974d94f616d641b078a632355d4e9db
Marino Faggiana <marino@marinofaggiana.com>
2023-06-19 16:45:01 +0200

    Update NCBrand.swift

--- Brand/NCBrand.swift
+++ Brand/NCBrand.swift
@@ -35,7 +35,7 @@ import UIKit
     @objc public var textCopyrightNextcloudiOS: String = "Nextcloud Liquid for iOS %@ © 2023"
     @objc public var textCopyrightNextcloudServer: String = "Nextcloud Server %@"
     @objc public var loginBaseUrl: String = "https://cloud.nextcloud.com"
-    @objc public var pushNotificationServerProxy: String = "https://push-notifications.nextcloud.com"
+    @objc public var pushNotificationServerProxy: String = "c0004.customerpush.nextcloud.com"
     @objc public var linkLoginHost: String = "https://nextcloud.com/install"
     @objc public var linkloginPreferredProviders: String = "https://nextcloud.com/signup-ios"
     @objc public var webLoginAutenticationProtocol: String = "nc://"                                                // example "abc://"
