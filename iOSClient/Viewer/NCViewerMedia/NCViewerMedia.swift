0e291cc841248add0896b59462f9002252ef474e
Milen Pivchev <milen.pivchev@gmail.com>
2023-08-21 16:19:46 +0200

    WIP
    Signed-off-by: Milen Pivchev <milen.pivchev@gmail.com>

--- iOSClient/Viewer/NCViewerMedia/NCViewerMedia.swift
+++ iOSClient/Viewer/NCViewerMedia/NCViewerMedia.swift
@@ -280,14 +280,6 @@ class NCViewerMedia: UIViewController {
                     NCNetworking.shared.download(metadata: metadata, selector: "") { _, _ in }
                 }
             }
-
-//            NCNetworking.shared.download(metadata: metadata, selector: "") { _, _ in
-//                let image = getImageMetadata(metadata)
-//                if self.metadata.ocId == metadata.ocId {
-//                    self.image = image
-//                    self.imageVideoContainer.image = image
-//                }
-//            }
         }
 
         // Get image
