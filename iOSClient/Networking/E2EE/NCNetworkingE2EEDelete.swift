f84b5b8621269b9deafe2f8b92ded3e0366d7eec
Marino Faggiana <marino@marinofaggiana.com>
2023-08-25 16:10:14 +0200

    fix delete
    Signed-off-by: Marino Faggiana <marino@marinofaggiana.com>

--- iOSClient/Networking/E2EE/NCNetworkingE2EEDelete.swift
+++ iOSClient/Networking/E2EE/NCNetworkingE2EEDelete.swift
@@ -32,39 +32,47 @@ class NCNetworkingE2EEDelete: NSObject {
             return NKError(errorCode: NCGlobal.shared.errorUnexpectedResponseFromDB, errorDescription: "_e2e_error_")
         }
 
+        // LOCK
+        //
         let resultsLock = await networkingE2EE.lock(account: metadata.account, serverUrl: metadata.serverUrl)
         guard resultsLock.error == .success, let e2eToken = resultsLock.e2eToken, let fileId = resultsLock.fileId else { return resultsLock.error }
 
+        // DELETE FILE
+        //
         let deleteMetadataPlainError = await NCNetworking.shared.deleteMetadataPlain(metadata, customHeader: ["e2e-token": e2eToken])
         guard deleteMetadataPlainError == .success else {
             await NCNetworkingE2EE().unlock(account: metadata.account, serverUrl: metadata.serverUrl)
             return deleteMetadataPlainError
         }
 
+        // GET METADATA + DECODE
         let resultsGetE2EEMetadata = await NextcloudKit.shared.getE2EEMetadata(fileId: fileId, e2eToken: e2eToken)
         guard resultsGetE2EEMetadata.error == .success, let e2eMetadata = resultsGetE2EEMetadata.e2eMetadata else {
             await NCNetworkingE2EE().unlock(account: metadata.account, serverUrl: metadata.serverUrl)
             return NKError(errorCode: NCGlobal.shared.errorE2EEKeyEncodeMetadata, errorDescription: NSLocalizedString("_e2e_error_", comment: ""))
         }
-
         let resultsDecodeMetadataError = NCEndToEndMetadata().decodeMetadata(e2eMetadata, signature: resultsGetE2EEMetadata.signature, serverUrl: metadata.serverUrl, account: metadata.account, urlBase: metadata.urlBase, userId: metadata.userId, ownerId: metadata.ownerId)
         guard resultsDecodeMetadataError == .success else {
             await NCNetworkingE2EE().unlock(account: metadata.account, serverUrl: metadata.serverUrl)
             return resultsDecodeMetadataError
         }
 
+        // DELETE FILE ON E2eEncryption
+        NCManageDatabase.shared.deleteE2eEncryption(predicate: NSPredicate(format: "account == %@ AND serverUrl == %@ AND fileNameIdentifier == %@", metadata.account, metadata.serverUrl, metadata.fileName))
+
+        // ENCODE METADATA
         let resultsEncodeMetadata = NCEndToEndMetadata().encodeMetadata(account: metadata.account, serverUrl: metadata.serverUrl, userId: metadata.userId)
         guard resultsEncodeMetadata.error == .success, let e2eMetadata = resultsEncodeMetadata.metadata else { return resultsEncodeMetadata.error }
 
+        // SEND METADATA
         let resultsPutE2EEMetadata = await NextcloudKit.shared.putE2EEMetadata(fileId: fileId, e2eToken: e2eToken, e2eMetadata: e2eMetadata, signature: resultsEncodeMetadata.signature, method: "PUT")
 
-        if resultsPutE2EEMetadata.error == .success {
-            if NCGlobal.shared.capabilityE2EEApiVersion == NCGlobal.shared.e2eeVersionV20 {
-                NCManageDatabase.shared.updateCounterE2eMetadataV2(account: metadata.account, ocIdServerUrl: directory.ocId, counter: resultsEncodeMetadata.counter)
-            }
-            NCManageDatabase.shared.deleteE2eEncryption(predicate: NSPredicate(format: "account == %@ AND serverUrl == %@ AND fileNameIdentifier == %@", metadata.account, metadata.serverUrl, metadata.fileName))
+        // UPDATE COUNTER
+        if resultsPutE2EEMetadata.error == .success, NCGlobal.shared.capabilityE2EEApiVersion == NCGlobal.shared.e2eeVersionV20 {
+            NCManageDatabase.shared.updateCounterE2eMetadataV2(account: metadata.account, ocIdServerUrl: directory.ocId, counter: resultsEncodeMetadata.counter)
         }
 
+        // UNLOCK
         await NCNetworkingE2EE().unlock(account: metadata.account, serverUrl: metadata.serverUrl)
 
         return resultsPutE2EEMetadata.error
