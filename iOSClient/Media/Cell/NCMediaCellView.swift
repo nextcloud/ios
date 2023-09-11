97819e7cb0e74074656e34921f5e48868fa6f7aa
Milen Pivchev <milen.pivchev@gmail.com>
2023-09-11 18:33:31 +0200

    Try to get first cell in list
    Signed-off-by: Milen Pivchev <milen.pivchev@gmail.com>

--- iOSClient/Media/Cell/NCMediaCellView.swift
+++ iOSClient/Media/Cell/NCMediaCellView.swift
@@ -11,10 +11,34 @@ import SwiftUI
 struct NCMediaCellView: View {
     let thumbnail: ScaledThumbnail
     let shrinkRatio: CGFloat
+    let outerProxy: GeometryProxy
+    @Binding var title: String
 
     var body: some View {
-        Image(uiImage: thumbnail.image)
-            .resizable()
-            .frame(width: CGFloat(thumbnail.scaledSize.width * shrinkRatio), height: CGFloat(thumbnail.scaledSize.height * shrinkRatio))
+        GeometryReader { geometry in
+            Image(uiImage: thumbnail.image)
+                .resizable()
+            //                .scaledToFit()
+            //                .frame(width: CGFloat(thumbnail.scaledSize.width * shrinkRatio), height: CGFloat(thumbnail.scaledSize.height * shrinkRatio))
+                .onChange(of: geometry.frame(in: .named(NCMediaNew.scrollViewCoordinateSpace))) { rect in
+                    if isInView(innerRect: rect, isIn: outerProxy) {
+                        print(thumbnail.metadata.fileName)
+                        title = thumbnail.metadata.fileName
+                    }
+                }
+        }
+        .frame(width: CGFloat(thumbnail.scaledSize.width * shrinkRatio), height: CGFloat(thumbnail.scaledSize.height * shrinkRatio))
+    }
+
+    private func isInView(innerRect:CGRect, isIn outerProxy:GeometryProxy) -> Bool {
+        let innerOrigin = innerRect.origin.x
+        let imageWidth = innerRect.width
+        let scrollOrigin = outerProxy.frame(in: .global).origin.x
+        let scrollWidth = outerProxy.size.width
+        if innerOrigin + imageWidth < scrollOrigin + scrollWidth && innerOrigin + imageWidth > scrollOrigin ||
+            innerOrigin + imageWidth > scrollOrigin && innerOrigin < scrollOrigin + scrollWidth {
+            return true
+        }
+        return false
     }
 }
