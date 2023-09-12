922166d769453e3f1faacffa84589f407f5124be
Milen Pivchev <milen.pivchev@gmail.com>
2023-09-12 16:49:14 +0200

    WIP
    Signed-off-by: Milen Pivchev <milen.pivchev@gmail.com>

--- iOSClient/Media/NCMediaNew.swift
+++ iOSClient/Media/NCMediaNew.swift
@@ -19,35 +19,34 @@ class NCMediaUIHostingController: UIHostingController<NCMediaNew> {
 }
 
 struct NCMediaNew: View {
-    @StateObject private var viewModel = NCMediaViewModel()
+    @StateObject private var vm = NCMediaViewModel()
     @State var columns = 2
     @State var title = ""
-
-    @State private var visibleMetadatas: [String] = []
-
-    public static let scrollViewCoordinateSpace = "scrollView"
 
     var body: some View {
         GeometryReader { outerProxy in
             ZStack(alignment: .top) {
                 VisibilityTrackingScrollView(action: handleVisibilityChanged) {
                     LazyVStack(alignment: .leading, spacing: 2) {
-                        ForEach(viewModel.metadatas.chunked(into: columns), id: \.self) { rowMetadatas in
+                        ForEach(vm.metadatas.chunked(into: columns), id: \.self) { rowMetadatas in
                             NCMediaRow(metadatas: rowMetadatas, geometryProxy: outerProxy)
+                        }
+
+                        if vm.needsLoadingMoreItems {
+                            ProgressView()
+                                .frame(maxWidth: .infinity)
+                                .onAppear { vm.loadMoreItems() }
                         }
                     }
                 }
 
-                HStack(content: {
-                    Text(title)
-                        .font(.system(size: 20, weight: .bold))
-                        .foregroundStyle(.white)
-                })
-                .frame(maxWidth: .infinity)
-                .background(LinearGradient(gradient: Gradient(colors: [.black.opacity(0.8), .black.opacity(0.0)]), startPoint: .top, endPoint: .bottom).edgesIgnoringSafeArea(.top))
-            }
-            .onChange(of: visibleMetadatas) { metadata in
-                title = metadata[0]
+//                HStack(content: {
+//                    Text(title)
+//                        .font(.system(size: 20, weight: .bold))
+//                        .foregroundStyle(.white)
+//                })
+//                .frame(maxWidth: .infinity)
+//                .background(LinearGradient(gradient: Gradient(colors: [.black.opacity(0.8), .black.opacity(0.0)]), startPoint: .top, endPoint: .bottom).edgesIgnoringSafeArea(.top))
             }
             .onRotate { orientation in
                 if orientation.isLandscapeHardCheck {
