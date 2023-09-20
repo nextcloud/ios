5073ea4bfb851ed89b13a7a115e1f6606bf9c1e6
Marino Faggiana <marino@marinofaggiana.com>
2023-09-20 16:49:10 +0200

    test

---
+++ iOSClient/Media/NCMediaUI.swift
@@ -0,0 +1,46 @@
+//
+//  NCMediaUI.swift
+//  Nextcloud
+//
+//  Created by Marino Faggiana on 20/09/23.
+//  Copyright © 2023 Marino Faggiana. All rights reserved.
+//
+
+import Foundation
+import SwiftUI
+
+class NCMediaUI: UIViewController, ObservableObject {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        addView()
+    }
+
+    func addView() {
+        let testView = SwiftUIView()
+        let controller = UIHostingController(rootView: testView.environmentObject(self))
+        addChild(controller)
+        controller.view.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(controller.view)
+        controller.didMove(toParent: self)
+
+        NSLayoutConstraint.activate([
+            controller.view.widthAnchor.constraint(equalTo: view.widthAnchor),
+            controller.view.heightAnchor.constraint(equalTo: view.heightAnchor),
+            controller.view.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            controller.view.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+        ])
+    }
+}
+
+struct SwiftUIView: View {
+    @EnvironmentObject var parent: NCMediaUI
+    var body: some View {
+        Button {
+            // push new view controller
+            // parent.navigationController?.pushViewController(<#T##viewController: UIViewController##UIViewController#>, animated: <#T##Bool#>)
+        } label: {
+            Text("go")
+        }
+    }
+}
