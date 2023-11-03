9af55eed3798ad68f096a9f13fe6f4a109dbd4bf
Milen Pivchev <milen.pivchev@gmail.com>
2023-11-03 13:51:13 +0100

    WIP
    Signed-off-by: Milen Pivchev <milen.pivchev@gmail.com>

--- iOSClient/Media/NCMediaRowViewModel.swift
+++ iOSClient/Media/NCMediaRowViewModel.swift
@@ -33,11 +33,12 @@ struct ScaledThumbnail: Hashable {
 
     private let cache = NCImageCache.shared
 
-//    private let queuer = (UIApplication.shared.delegate as? AppDelegate)?.downloadThumbnailQueue
+//    private let appDelegate = UIApplication.shared.delegate as? AppDelegate
+    //    private let queuer = (UIApplication.shared.delegate as? AppDelegate)?.downloadThumbnailQueue
     private var queuer: Queuer?
 
-//    internal lazy var cache = manager.cache
-//    internal lazy var thumbnailsQueue = manager.queuer
+    //    internal lazy var cache = manager.cache
+    //    internal lazy var thumbnailsQueue = manager.queuer
 
     var operations: [ConcurrentOperation] = []
 
@@ -63,18 +64,18 @@ struct ScaledThumbnail: Hashable {
 
                 DispatchQueue.main.async {
                     thumbnails.append(thumbnail)
-                    self.calculateShrinkRatio(thumbnails: &thumbnails, rowWidth: rowWidth, spacing: spacing)
+                    self.calculateShrinkRatio(metadatas: self.metadatas, rowData: &self.rowData, thumbnails: &thumbnails, rowWidth: rowWidth, spacing: spacing)
                 }
             } else if FileManager.default.fileExists(atPath: thumbnailPath) {
                 // Load thumbnail from file
                 if let image = UIImage(contentsOfFile: thumbnailPath) {
                     let thumbnail = ScaledThumbnail(image: image, metadata: metadata)
                     cache.setMediaImage(ocId: metadata.ocId, image: .actual(image))
-//                    cache.setValue(thumbnail, forKey: metadata.ocId)
+                    //                    cache.setValue(thumbnail, forKey: metadata.ocId)
 
                     DispatchQueue.main.async {
                         thumbnails.append(thumbnail)
-                        self.calculateShrinkRatio(thumbnails: &thumbnails, rowWidth: rowWidth, spacing: spacing)
+                        self.calculateShrinkRatio(metadatas: self.metadatas, rowData: &self.rowData, thumbnails: &thumbnails, rowWidth: rowWidth, spacing: spacing)
                     }
                 }
             } else {
@@ -88,62 +89,167 @@ struct ScaledThumbnail: Hashable {
                 }
                 let options = NKRequestOptions(queue: NextcloudKit.shared.nkCommonInstance.backgroundQueue)
 
-                let concurrentOperation = ConcurrentOperation { _ in
-                    NextcloudKit.shared.downloadPreview(
-                        fileNamePathOrFileId: fileNamePath,
-                        fileNamePreviewLocalPath: fileNamePreviewLocalPath,
-                        widthPreview: Int(UIScreen.main.bounds.width) / 2,
-                        heightPreview: Int(UIScreen.main.bounds.height) / 2,
-                        fileNameIconLocalPath: fileNameIconLocalPath,
-                        sizeIcon: NCGlobal.shared.sizeIcon,
-                        etag: etagResource,
-                        options: options) { _, _, imageIcon, _, etag, error in
-                            NCManageDatabase.shared.setMetadataEtagResource(ocId: metadata.ocId, etagResource: etag)
+                //                let concurrentOperation = ConcurrentOperation { operation in
+                //                    operation.execute()
+                //
+                //                    NextcloudKit.shared.downloadPreview(
+                //                        fileNamePathOrFileId: fileNamePath,
+                //                        fileNamePreviewLocalPath: fileNamePreviewLocalPath,
+                //                        widthPreview: Int(UIScreen.main.bounds.width) / 2,
+                //                        heightPreview: Int(UIScreen.main.bounds.height) / 2,
+                //                        fileNameIconLocalPath: fileNameIconLocalPath,
+                //                        sizeIcon: NCGlobal.shared.sizeIcon,
+                //                        etag: etagResource,
+                //                        options: options) { _, _, imageIcon, _, etag, error in
+                //                            NCManageDatabase.shared.setMetadataEtagResource(ocId: metadata.ocId, etagResource: etag)
+                //
+                //                            let thumbnail: ScaledThumbnail
+                //
+                //                            if error == .success, let image = imageIcon {
+                //                                thumbnail = ScaledThumbnail(image: image, metadata: metadata)
+                //                                self.cache.setMediaImage(ocId: metadata.ocId, image: image)
+                //                            } else {
+                //                                thumbnail = ScaledThumbnail(image: UIImage(systemName: metadata.isVideo ? "video.fill" : "photo.fill")!.withRenderingMode(.alwaysTemplate), isPlaceholderImage: true, metadata: metadata)
+                //                            }
+                //
+                ////                            self.cache.setValue(thumbnail, forKey: metadata.ocId)
+                //
+                //                            operation.finish()
+                //
+                //                            DispatchQueue.main.async {
+                //                                thumbnails.append(thumbnail)
+                //                                self.calculateShrinkRatio(thumbnails: &thumbnails, rowWidth: rowWidth, spacing: spacing)
+                //                            }
+                //
+                //                        }
+                //                }
 
-                            let thumbnail: ScaledThumbnail
-
-                            if error == .success, let image = imageIcon {
-                                thumbnail = ScaledThumbnail(image: image, metadata: metadata)
-                                self.cache.setMediaImage(ocId: metadata.ocId, image: .actual(image))
-                            } else {
-                                let image = UIImage(systemName: metadata.isVideo ? "video.fill" : "photo.fill")!.withRenderingMode(.alwaysTemplate)
-                                thumbnail = ScaledThumbnail(image: image, isPlaceholderImage: true, metadata: metadata)
-                                self.cache.setMediaImage(ocId: metadata.ocId, image: .placeholder)
-                            }
-
-//                            self.cache.setValue(thumbnail, forKey: metadata.ocId)
-
-                            DispatchQueue.main.async {
-                                thumbnails.append(thumbnail)
-                                self.calculateShrinkRatio(thumbnails: &thumbnails, rowWidth: rowWidth, spacing: spacing)
-                            }
+                if let queuer, queuer.operations.filter({ ($0 as? NCMediaDownloadThumbnaill)?.metadata.ocId == metadata.ocId }).isEmpty {
+                    let concurrentOperation = NCMediaDownloadThumbnaill(metadata: metadata, cache: cache, rowWidth: rowWidth, spacing: spacing, maxWidth: UIScreen.main.bounds.width, maxHeight: UIScreen.main.bounds.height) { thumbnail in
+                        DispatchQueue.main.async {
+                            thumbnails.append(thumbnail)
+                            self.calculateShrinkRatio(metadatas: self.metadatas, rowData: &self.rowData, thumbnails: &thumbnails, rowWidth: rowWidth, spacing: spacing)
                         }
+                    }
+
+                    operations.append(concurrentOperation)
+                    queuer.addOperation(concurrentOperation)
+                    print(queuer.operationCount)
                 }
 
-                operations.append(concurrentOperation)
-                queuer?.addOperation(concurrentOperation)
-                print(queuer?.operationCount)
-                print(queuer?.maxConcurrentOperationCount.hashValue)
+                //                let concurrentOperation = NCMediaDownloadThumbnaill(metadata: metadata, cache: cache, thumbnails: &thumbnails, rowWidth: rowWidth, spacing: spacing, maxWidth: UIScreen.main.bounds.width, maxHeight: UIScreen.main.bounds.height)
+
+                //                operations.append(concurrentOperation)
+                //                appDelegate?.downloadThumbnailQueue.addOperation(concurrentOperation)
+                //                print(appDelegate!.downloadThumbnailQueue.operationCount)
+
+                //                concurrentOperation.start()
             }
         }
     }
 
-    private func calculateShrinkRatio(thumbnails: inout [ScaledThumbnail], rowWidth: CGFloat, spacing: CGFloat) {
-            if thumbnails.count == self.metadatas.count {
-                thumbnails.enumerated().forEach { index, thumbnail in
-                    thumbnails[index].scaledSize = self.getScaledThumbnailSize(of: thumbnail, thumbnailsInRow: thumbnails)
-                }
+    class NCMediaDownloadThumbnaill: ConcurrentOperation {
+        let metadata: tableMetadata
+        let cache: NCImageCache
+        let rowWidth: CGFloat
+        let spacing: CGFloat
+        let maxWidth: CGFloat
+        let maxHeight: CGFloat
 
-                let shrinkRatio = self.getShrinkRatio(thumbnailsInRow: thumbnails, fullWidth: rowWidth, spacing: spacing)
+        let onThumbnailDownloaded: (ScaledThumbnail) -> Void
 
-                self.rowData.scaledThumbnails = thumbnails
-                self.rowData.shrinkRatio = shrinkRatio
-            }
+        init(metadata: tableMetadata, cache: NCImageCache, rowWidth: CGFloat, spacing: CGFloat, maxWidth: CGFloat, maxHeight: CGFloat, onThumbnailDownloaded: @escaping (ScaledThumbnail) -> Void) {
+            self.metadata = metadata
+            self.cache = cache
+            self.rowWidth = rowWidth
+            self.spacing = spacing
+            self.maxWidth = maxWidth
+            self.maxHeight = maxHeight
+            self.onThumbnailDownloaded = onThumbnailDownloaded
         }
 
-    func cancelDownloadingThumbnails() {
-//        operations.forEach {( $0.cancel() )}
-//        operations.removeAll()
+        //        var cell: NCCellProtocol?
+        //        var collectionView: UICollectionView?
+        //        var fileNamePath: String
+        //        var fileNamePreviewLocalPath: String
+        //        var fileNameIconLocalPath: String
+        //        let utilityFileSystem = NCUtilityFileSystem()
+
+        //        init(metadata: tableMetadata, cell: NCCellProtocol?, collectionView: UICollectionView?) {
+        //            self.metadata = tableMetadata.init(value: metadata)
+        ////            self.cell = cell
+        ////            self.collectionView = collectionView
+        ////            self.fileNamePath = utilityFileSystem.getFileNamePath(metadata.fileName, serverUrl: metadata.serverUrl, urlBase: metadata.urlBase, userId: metadata.userId)
+        ////            self.fileNamePreviewLocalPath = utilityFileSystem.getDirectoryProviderStoragePreviewOcId(metadata.ocId, etag: metadata.etag)
+        ////            self.fileNameIconLocalPath = utilityFileSystem.getDirectoryProviderStorageIconOcId(metadata.ocId, etag: metadata.etag)
+        //        }
+
+        override func start() {
+            guard !isCancelled else { return self.finish() }
+
+            let fileNamePath = NCUtilityFileSystem().getFileNamePath(metadata.fileName, serverUrl: metadata.serverUrl, urlBase: metadata.urlBase, userId: metadata.userId)
+            let fileNamePreviewLocalPath = NCUtilityFileSystem().getDirectoryProviderStoragePreviewOcId(metadata.ocId, etag: metadata.etag)
+            let fileNameIconLocalPath = NCUtilityFileSystem().getDirectoryProviderStorageIconOcId(metadata.ocId, etag: metadata.etag)
+
+            var etagResource: String?
+            if FileManager.default.fileExists(atPath: fileNameIconLocalPath) && FileManager.default.fileExists(atPath: fileNamePreviewLocalPath) {
+                etagResource = metadata.etagResource
+            }
+            let options = NKRequestOptions(queue: NextcloudKit.shared.nkCommonInstance.backgroundQueue)
+
+            //            let concurrentOperation = ConcurrentOperation { operation in
+            NextcloudKit.shared.downloadPreview(
+                fileNamePathOrFileId: fileNamePath,
+                fileNamePreviewLocalPath: fileNamePreviewLocalPath,
+                widthPreview: Int(self.maxWidth) / 2,
+                heightPreview: Int(self.maxHeight) / 2,
+                fileNameIconLocalPath: fileNameIconLocalPath,
+                sizeIcon: NCGlobal.shared.sizeIcon,
+                etag: etagResource,
+                options: options) { _, _, imageIcon, _, etag, error in
+                    NCManageDatabase.shared.setMetadataEtagResource(ocId: self.metadata.ocId, etagResource: etag)
+
+                    let thumbnail: ScaledThumbnail
+
+                    if error == .success, let image = imageIcon {
+                        thumbnail = ScaledThumbnail(image: image, metadata: self.metadata)
+                        self.cache.setMediaImage(ocId: self.metadata.ocId, image: .actual(image))
+                    } else {
+                        let image = UIImage(systemName: self.metadata.isVideo ? "video.fill" : "photo.fill")!.withRenderingMode(.alwaysTemplate)
+                        thumbnail = ScaledThumbnail(image: image, isPlaceholderImage: true, metadata: self.metadata)
+                        self.cache.setMediaImage(ocId: self.metadata.ocId, image: .placeholder)
+                    }
+
+                    self.onThumbnailDownloaded(thumbnail)
+
+                    //                            self.cache.setValue(thumbnail, forKey: metadata.ocId)
+
+                    //                        DispatchQueue.main.async {
+                    //                            self.thumbnails.append(thumbnail)
+                    //                            NCMediaRowViewModel.calculateShrinkRatio(metadatas: self.metadatas, rowData: &self.rowData, thumbnails: &self.thumbnails, rowWidth: self.rowWidth, spacing: self.spacing)
+                    //                        }
+
+                    self.finish()
+                    //                    }
+                }
+
+
+        }
+
+
+    }
+
+    private func calculateShrinkRatio(metadatas: [tableMetadata], rowData: inout RowData, thumbnails: inout [ScaledThumbnail], rowWidth: CGFloat, spacing: CGFloat) {
+        if thumbnails.count == metadatas.count {
+            thumbnails.enumerated().forEach { index, thumbnail in
+                thumbnails[index].scaledSize = getScaledThumbnailSize(of: thumbnail, thumbnailsInRow: thumbnails)
+            }
+
+            let shrinkRatio = getShrinkRatio(thumbnailsInRow: thumbnails, fullWidth: rowWidth, spacing: spacing)
+
+            rowData.scaledThumbnails = thumbnails
+            rowData.shrinkRatio = shrinkRatio
+        }
     }
 
     private func getScaledThumbnailSize(of thumbnail: ScaledThumbnail, thumbnailsInRow thumbnails: [ScaledThumbnail]) -> CGSize {
@@ -171,4 +277,35 @@ struct ScaledThumbnail: Hashable {
 
         return shrinkRatio
     }
+
+    func cancelDownloadingThumbnails() {
+        //        operations.forEach {( $0.cancel() )}
+        //        operations.removeAll()
+    }
+
+    //    private func getScaledThumbnailSize(of thumbnail: ScaledThumbnail, thumbnailsInRow thumbnails: [ScaledThumbnail]) -> CGSize {
+    //        let maxHeight = thumbnails.compactMap { CGFloat($0.image.size.height) }.max() ?? 0
+    //
+    //        let height = thumbnail.image.size.height
+    //        let width = thumbnail.image.size.width
+    //
+    //        let scaleFactor = maxHeight / height
+    //        let newHeight = height * scaleFactor
+    //        let newWidth = width * scaleFactor
+    //
+    //        return .init(width: newWidth, height: newHeight)
+    //    }
+    //
+    //    private func getShrinkRatio(thumbnailsInRow thumbnails: [ScaledThumbnail], fullWidth: CGFloat, spacing: CGFloat) -> CGFloat {
+    //        var newSummedWidth: CGFloat = 0
+    //
+    //        for thumbnail in thumbnails {
+    //            newSummedWidth += CGFloat(thumbnail.scaledSize.width)
+    //        }
+    //
+    //        let spacingWidth = spacing * CGFloat(thumbnails.count - 1)
+    //        let shrinkRatio: CGFloat = (fullWidth - spacingWidth) / newSummedWidth
+    //
+    //        return shrinkRatio
+    //    }
 }
