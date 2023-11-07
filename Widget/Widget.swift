51a5127227555bf44faa6e9b8636e37540686fea
Milen Pivchev <milen.pivchev@gmail.com>
2023-11-07 19:00:04 +0100

    Test
    Signed-off-by: Milen Pivchev <milen.pivchev@gmail.com>

--- Widget/Widget.swift
+++ Widget/Widget.swift
@@ -99,12 +99,12 @@ struct LockscreenWidget: Widget {
 
 extension View {
     func widgetBackground(_ backgroundView: some View) -> some View {
-        if #available(iOSApplicationExtension 17.0, *) {
-            return containerBackground(for: .widget) {
-                backgroundView
-            }
-        } else {
+//        if #available(iOSApplicationExtension 17.0, *) {
+//            return containerBackground(for: .widget) {
+//                backgroundView
+//            }
+//        } else {
             return background(backgroundView)
-        }
+//        }
     }
 }
