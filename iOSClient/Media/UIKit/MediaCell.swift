84ae08bd998afd091e4b5bff139bf67679edd007
Milen Pivchev <milen.pivchev@gmail.com>
2024-01-22 18:41:25 +0100

    WIP
    Signed-off-by: Milen Pivchev <milen.pivchev@gmail.com>

--- iOSClient/Media/UIKit/MediaCell.swift
+++ iOSClient/Media/UIKit/MediaCell.swift
@@ -8,8 +8,33 @@
 
 import Foundation
 
+//struct RowData {
+//    var scaledThumbnails: [ScaledThumbnail] = []
+//    var shrinkRatio: CGFloat = 0
+//}
+//
+//struct ScaledThumbnail: Hashable {
+//    let image: UIImage
+//    var isPlaceholderImage = false
+//    var scaledSize: CGSize = .zero
+//    let metadata: tableMetadata
+//
+//    func hash(into hasher: inout Hasher) {
+//        hasher.combine(metadata.ocId)
+//    }
+//}
+
 class MediaCell: UICollectionViewCell {
     static let identifier = "MediaCell"
+    var medatadata: tableMetadata?
+    var image = UIImage()
+
+    @Published private(set) var rowData = RowData()
+
+    private var metadatas: [tableMetadata] = []
+    private let cache = NCImageCache.shared
+
+    private let queuer = NCNetworking.shared.downloadThumbnailQueue
 
     // Add subviews and set up the cell
     private let titleLabel: UILabel = {
@@ -19,18 +44,84 @@ class MediaCell: UICollectionViewCell {
         return label
     }()
 
+    private let imageView = UIImageView()
+
     override init(frame: CGRect) {
         super.init(frame: frame)
-        contentView.addSubview(titleLabel)
-        contentView.backgroundColor = .systemBlue
-        titleLabel.frame = contentView.bounds
+//        contentView.addSubview(titleLabel)
+//        contentView.backgroundColor = .systemBlue
+//        titleLabel.frame = contentView.bounds
+        imageView.contentMode = .scaleToFill
+
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+
+        contentView.addSubview(imageView)
+
+        imageView.centerXAnchor.constraint(equalTo: contentView.centerXAnchor).isActive = true
+        imageView.centerYAnchor.constraint(equalTo: contentView.centerYAnchor).isActive = true
+        imageView.heightAnchor.constraint(equalTo: contentView.heightAnchor).isActive = true
+        imageView.widthAnchor.constraint(equalTo: contentView.widthAnchor).isActive = true
     }
 
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
 
-    func configure(with item: tableMetadata) {
-        titleLabel.text = item.name
+    func configure(metadata: tableMetadata, thumbnail: ScaledThumbnail) {
+        self.medatadata = metadata
+        self.imageView.image = thumbnail.image
+//        titleLabel.text = metadata.name
+//    }
+        let thumbnailPath = NCUtilityFileSystem().getDirectoryProviderStorageIconOcId(metadata.ocId, etag: metadata.etag)
+        if let cachedImage = cache.getMediaImage(ocId: metadata.ocId, etag: metadata.etag) {
+            let thumbnail: ScaledThumbnail
+
+            if case let .actual(image) = cachedImage {
+                thumbnail = ScaledThumbnail(image: image, metadata: metadata)
+            } else {
+                let image = UIImage(systemName: metadata.isVideo ? "video.fill" : "photo.fill")!.withRenderingMode(.alwaysTemplate)
+                thumbnail = ScaledThumbnail(image: image, isPlaceholderImage: true, metadata: metadata)
+            }
+
+            DispatchQueue.main.async {
+//                thumbnails.append(thumbnail)
+                self.calculateShrinkRatio(metadatas: self.metadatas, rowData: &self.rowData, thumbnails: &thumbnails, rowWidth: rowWidth, spacing: spacing)
+            }
+        } else if FileManager.default.fileExists(atPath: thumbnailPath) {
+            // Load thumbnail from file
+            if let image = UIImage(contentsOfFile: thumbnailPath) {
+                let thumbnail = ScaledThumbnail(image: image, metadata: metadata)
+                cache.setMediaImage(ocId: metadata.ocId, etag: metadata.etag, image: .actual(image))
+
+                DispatchQueue.main.async {
+                    thumbnails.append(thumbnail)
+                    self.calculateShrinkRatio(metadatas: self.metadatas, rowData: &self.rowData, thumbnails: &thumbnails, rowWidth: rowWidth, spacing: spacing)
+                }
+            }
+        } else {
+            if queuer.operations.filter({ ($0 as? NCMediaDownloadThumbnaill)?.metadata.ocId == metadata.ocId }).isEmpty {
+                let concurrentOperation = NCMediaDownloadThumbnaill(metadata: metadata, cache: cache, rowWidth: rowWidth, spacing: spacing, maxWidth: UIScreen.main.bounds.width, maxHeight: UIScreen.main.bounds.height) { thumbnail in
+                    DispatchQueue.main.async {
+                        thumbnails.append(thumbnail)
+                        self.calculateShrinkRatio(metadatas: self.metadatas, rowData: &self.rowData, thumbnails: &thumbnails, rowWidth: rowWidth, spacing: spacing)
+                    }
+                }
+
+                queuer.addOperation(concurrentOperation)
+            }
+        }
+    }
+
+    private func calculateShrinkRatio(metadata: tableMetadata, rowData: inout RowData, thumbnails: inout ScaledThumbnail, rowWidth: CGFloat, spacing: CGFloat) {
+//        if thumbnails.count == metadatas.count {
+//            thumbnails.enumerated().forEach { index, thumbnail in
+                thumbnail.scaledSize = getScaledThumbnailSize(of: thumbnail, thumbnailsInRow: thumbnails)
+//            }
+
+            let shrinkRatio = getShrinkRatio(thumbnailsInRow: thumbnails, fullWidth: rowWidth, spacing: spacing)
+
+            rowData.scaledThumbnails = thumbnails
+            rowData.shrinkRatio = shrinkRatio
+        }
     }
 }
--- iOSClient/Media/UIKit/MediaCollectionView.swift
+++ iOSClient/Media/UIKit/MediaCollectionView.swift
@@ -9,9 +9,15 @@
 import Foundation
 import UIKit
 import SwiftUI
+import Queuer
+import NextcloudKit
 
 struct MediaCollectionView: UIViewControllerRepresentable {
     @Binding var items: [tableMetadata]
+
+    private var itemsPerRow: [[tableMetadata]] {
+        return items.chunked(into: 3)
+    }
 
     func makeUIViewController(context: Context) -> UICollectionViewController {
         let layout = UICollectionViewFlowLayout()
@@ -25,6 +31,15 @@ struct MediaCollectionView: UIViewControllerRepresentable {
 
         collectionView.dataSource = context.coordinator
         collectionView.delegate = context.coordinator
+
+        if let flowLayout = collectionView.collectionViewLayout as? UICollectionViewFlowLayout {
+//            flowLayout.minimumInteritemSpacing = 0
+//            flowLayout.minimumLineSpacing = 2
+//            flowLayout.sectionInset = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
+
+        }
+
+//        collectionView.sepera
 
         return viewController
     }
@@ -40,20 +55,255 @@ struct MediaCollectionView: UIViewControllerRepresentable {
 
     // MARK: - Coordinator
     class Coordinator: NSObject, UICollectionViewDataSource, UICollectionViewDelegate {
-        var parent: MediaCollectionView
+        private(set) var rowData = RowData()
+
+        private var metadatas: [tableMetadata] = []
+        private let cache = NCImageCache.shared
+
+        private let queuer = NCNetworking.shared.downloadThumbnailQueue
+
+        private let rowWidth = UIScreen.main.bounds.width
+        private let spacing: CGFloat = 2
+
+        let parent: MediaCollectionView
+//        let cellsPerRow: [[tableMetadata]]
 
         init(_ parent: MediaCollectionView) {
             self.parent = parent
+//            cellsPerRow = parent.items.chunked(into: 3)
+//            print("TEST")
+//            print(cellsPerRow)
+//            print(cellsPerRow.count)
         }
 
         func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-            parent.items.count
+            parent.itemsPerRow[section].count
         }
 
-        func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-            guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: MediaCell.identifier, for: indexPath) as? MediaCell else { return UICollectionViewCell() }
-            cell.configure(with: parent.items[indexPath.row])
-            return cell
+        func numberOfSections(in collectionView: UICollectionView) -> Int {
+            return parent.itemsPerRow.count
         }
+
+//        func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+//            return groups[section].items.count
+//        }
+//
+//        func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+//            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Cell", for: indexPath)
+//            // Configure the cell
+//            return cell
+//        }
+//
+//        struct RowData {
+//            var scaledThumbnails: [ScaledThumbnail] = []
+//            var shrinkRatio: CGFloat = 0
+//        }
+//
+//        struct ScaledThumbnail: Hashable {
+//            let image: UIImage
+//            var isPlaceholderImage = false
+//            var scaledSize: CGSize = .zero
+//            let metadata: tableMetadata
+//
+//            func hash(into hasher: inout Hasher) {
+//                hasher.combine(metadata.ocId)
+//            }
+//        }
+
+        func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+            let metadatas = parent.itemsPerRow[indexPath.section]
+            print(metadatas.forEach({ $0.fileName
+            }))
+            var thumbnails: [ScaledThumbnail] = []
+
+//            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: MediaCell.identifier, for: indexPath) as? MediaCell
+//
+//            return cell!
+
+            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: MediaCell.identifier, for: indexPath) as? MediaCell
+
+
+            for metadata in metadatas {
+                let thumbnailPath = NCUtilityFileSystem().getDirectoryProviderStorageIconOcId(metadata.ocId, etag: metadata.etag)
+                if let cachedImage = cache.getMediaImage(ocId: metadata.ocId, etag: metadata.etag) {
+                    let thumbnail: ScaledThumbnail
+
+                    if case let .actual(image) = cachedImage {
+                        thumbnail = ScaledThumbnail(image: image, metadata: metadata)
+                    } else {
+                        let image = UIImage(systemName: metadata.isVideo ? "video.fill" : "photo.fill")!.withRenderingMode(.alwaysTemplate)
+                        thumbnail = ScaledThumbnail(image: image, isPlaceholderImage: true, metadata: metadata)
+                    }
+
+//                    DispatchQueue.main.async {
+                        thumbnails.append(thumbnail)
+                        self.calculateShrinkRatio(metadatas: self.metadatas, rowData: &self.rowData, thumbnails: &thumbnails, rowWidth: self.rowWidth, spacing: self.spacing)
+                        cell?.configure(metadata: metadata, thumbnail: thumbnail)
+//                        collectionView.collectionViewLayout.invalidateLayout()
+
+//                    }
+                } else if FileManager.default.fileExists(atPath: thumbnailPath) {
+                    // Load thumbnail from file
+                    if let image = UIImage(contentsOfFile: thumbnailPath) {
+                        let thumbnail = ScaledThumbnail(image: image, metadata: metadata)
+                        cache.setMediaImage(ocId: metadata.ocId, etag: metadata.etag, image: .actual(image))
+
+//                        DispatchQueue.main.async {
+                            thumbnails.append(thumbnail)
+                            self.calculateShrinkRatio(metadatas: self.metadatas, rowData: &self.rowData, thumbnails: &thumbnails, rowWidth: self.rowWidth, spacing: self.spacing)
+                            cell?.configure(metadata: metadata, thumbnail: thumbnail)
+//                            collectionView.collectionViewLayout.invalidateLayout()
+//                        }
+                    }
+                } else {
+                    if queuer.operations.filter({ ($0 as? NCMediaDownloadThumbnaill)?.metadata.ocId == metadata.ocId }).isEmpty {
+                        let concurrentOperation = NCMediaDownloadThumbnaill(metadata: metadata, cache: cache, rowWidth: rowWidth, spacing: spacing, maxWidth: UIScreen.main.bounds.width, maxHeight: UIScreen.main.bounds.height) { thumbnail in
+//                            DispatchQueue.main.async {
+                                thumbnails.append(thumbnail)
+                                self.calculateShrinkRatio(metadatas: self.metadatas, rowData: &self.rowData, thumbnails: &thumbnails, rowWidth: self.rowWidth, spacing: self.spacing)
+                                cell?.configure(metadata: metadata, thumbnail: thumbnail)
+//                                collectionView.collectionViewLayout.invalidateLayout()
+//                            }
+                        }
+
+                        queuer.addOperation(concurrentOperation)
+                    }
+                }
+
+                print("TEST")
+                print(thumbnails.count)
+                if thumbnails.count == 3 {
+                    let cell = collectionView.dequeueReusableCell(withReuseIdentifier: MediaCell.identifier, for: indexPath) as? MediaCell
+                    let metadata = metadatas[indexPath.row]
+                    cell?.backgroundColor = .black
+                    return cell!
+                }
+
+                cell?.backgroundColor = UIColor(hue: CGFloat(drand48()), saturation: 1, brightness: 1, alpha: 1)
+                return cell!
+            }
+//
+
+
+//            guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: MediaCell.identifier, for: indexPath) as? MediaCell else { return UICollectionViewCell() }
+//            cell.configure(metadata: parent.itemsPerRow[indexPath.section][indexPath.row])
+//            cell.backgroundColor = UIColor(hue: CGFloat(drand48()), saturation: 1, brightness: 1, alpha: 1)
+            return UICollectionViewCell()
+        }
+
+        private func calculateShrinkRatio(metadatas: [tableMetadata], rowData: inout RowData, thumbnails: inout [ScaledThumbnail], rowWidth: CGFloat, spacing: CGFloat) {
+            if thumbnails.count == metadatas.count {
+                thumbnails.enumerated().forEach { index, thumbnail in
+                    thumbnails[index].scaledSize = getScaledThumbnailSize(of: thumbnail, thumbnailsInRow: thumbnails)
+                }
+
+                let shrinkRatio = getShrinkRatio(thumbnailsInRow: thumbnails, fullWidth: rowWidth, spacing: spacing)
+
+                rowData.scaledThumbnails = thumbnails
+                rowData.shrinkRatio = shrinkRatio
+            }
+        }
+
+        private func getScaledThumbnailSize(of thumbnail: ScaledThumbnail, thumbnailsInRow thumbnails: [ScaledThumbnail]) -> CGSize {
+            let maxHeight = thumbnails.compactMap { CGFloat($0.image.size.height) }.max() ?? 0
+
+            let height = thumbnail.image.size.height
+            let width = thumbnail.image.size.width
+
+            let scaleFactor = maxHeight / height
+            let newHeight = height * scaleFactor
+            let newWidth = width * scaleFactor
+
+            return .init(width: newWidth, height: newHeight)
+        }
+
+        private func getShrinkRatio(thumbnailsInRow thumbnails: [ScaledThumbnail], fullWidth: CGFloat, spacing: CGFloat) -> CGFloat {
+            var newSummedWidth: CGFloat = 0
+
+            for thumbnail in thumbnails {
+                newSummedWidth += CGFloat(thumbnail.scaledSize.width)
+            }
+
+            let spacingWidth = spacing * CGFloat(thumbnails.count - 1)
+            let shrinkRatio: CGFloat = (fullWidth - spacingWidth) / newSummedWidth
+
+            return shrinkRatio
+        }
+
+        func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+            let noOfCellsInRow = 3   // number of column you want
+            guard let flowLayout = collectionViewLayout as? UICollectionViewFlowLayout else { return .zero }
+//            flowLayout.minimumInteritemSpacing = 0
+//            flowLayout.minimumLineSpacing = 2
+//            flowLayout.sectionInset = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
+            let totalSpace = flowLayout.sectionInset.left
+                + flowLayout.sectionInset.right
+                + (flowLayout.minimumInteritemSpacing * CGFloat(noOfCellsInRow - 1))
+
+            let size = Int((collectionView.bounds.width - totalSpace) / CGFloat(noOfCellsInRow))
+            return CGSize(width: size, height: size)
+        }
+    }
+}
+
+class NCMediaDownloadThumbnaill: ConcurrentOperation {
+    let metadata: tableMetadata
+    let cache: NCImageCache
+    let rowWidth: CGFloat
+    let spacing: CGFloat
+    let maxWidth: CGFloat
+    let maxHeight: CGFloat
+
+    let onThumbnailDownloaded: (ScaledThumbnail) -> Void
+
+    init(metadata: tableMetadata, cache: NCImageCache, rowWidth: CGFloat, spacing: CGFloat, maxWidth: CGFloat, maxHeight: CGFloat, onThumbnailDownloaded: @escaping (ScaledThumbnail) -> Void) {
+        self.metadata = metadata
+        self.cache = cache
+        self.rowWidth = rowWidth
+        self.spacing = spacing
+        self.maxWidth = maxWidth
+        self.maxHeight = maxHeight
+        self.onThumbnailDownloaded = onThumbnailDownloaded
+    }
+
+    override func start() {
+        guard !isCancelled else { return self.finish() }
+
+        let fileNamePath = NCUtilityFileSystem().getFileNamePath(metadata.fileName, serverUrl: metadata.serverUrl, urlBase: metadata.urlBase, userId: metadata.userId)
+        let fileNamePreviewLocalPath = NCUtilityFileSystem().getDirectoryProviderStoragePreviewOcId(metadata.ocId, etag: metadata.etag)
+        let fileNameIconLocalPath = NCUtilityFileSystem().getDirectoryProviderStorageIconOcId(metadata.ocId, etag: metadata.etag)
+
+        var etagResource: String?
+        if FileManager.default.fileExists(atPath: fileNameIconLocalPath) && FileManager.default.fileExists(atPath: fileNamePreviewLocalPath) {
+            etagResource = metadata.etagResource
+        }
+        let options = NKRequestOptions(queue: NextcloudKit.shared.nkCommonInstance.backgroundQueue)
+
+        NextcloudKit.shared.downloadPreview(
+            fileNamePathOrFileId: fileNamePath,
+            fileNamePreviewLocalPath: fileNamePreviewLocalPath,
+            widthPreview: Int(self.maxWidth) / 2,
+            heightPreview: Int(self.maxHeight) / 2,
+            fileNameIconLocalPath: fileNameIconLocalPath,
+            sizeIcon: NCGlobal.shared.sizeIcon,
+            etag: etagResource,
+            options: options) { _, _, imageIcon, _, etag, error in
+                NCManageDatabase.shared.setMetadataEtagResource(ocId: self.metadata.ocId, etagResource: etag)
+
+                let thumbnail: ScaledThumbnail
+
+                if error == .success, let image = imageIcon {
+                    thumbnail = ScaledThumbnail(image: image, metadata: self.metadata)
+                    self.cache.setMediaImage(ocId: self.metadata.ocId, etag: self.metadata.etag, image: .actual(image))
+                } else {
+                    let image = UIImage(systemName: self.metadata.isVideo ? "video.fill" : "photo.fill")!.withRenderingMode(.alwaysTemplate)
+                    thumbnail = ScaledThumbnail(image: image, isPlaceholderImage: true, metadata: self.metadata)
+                    self.cache.setMediaImage(ocId: self.metadata.ocId, etag: self.metadata.etag, image: .placeholder)
+                }
+
+                self.onThumbnailDownloaded(thumbnail)
+
+                self.finish()
+            }
     }
 }
