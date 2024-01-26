01b49d2eb8c8cfeccc37e3b0a864272b8c5ae35c
Milen Pivchev <milen.pivchev@gmail.com>
2024-01-26 19:01:06 +0100

    Add new menu
    Signed-off-by: Milen Pivchev <milen.pivchev@gmail.com>

--- iOSClient/Main/Collection Common/NCSelectableNavigationView.swift
+++ iOSClient/Main/Collection Common/NCSelectableNavigationView.swift
@@ -62,17 +62,49 @@ extension NCSelectableNavigationView {
 
     func setNavigationRightItems() {
         if isEditMode {
-            let more = UIBarButtonItem(image: UIImage(systemName: "ellipsis"), style: .plain, action: tapSelectMenu)
+            let more = UIBarButtonItem(image: .init(systemName: "ellipsis"), style: .plain, action: tapSelectMenu)
             navigationItem.rightBarButtonItems = [more]
         } else {
-            let select = UIBarButtonItem(title: NSLocalizedString("_select_", comment: ""), style: UIBarButtonItem.Style.plain, action: tapSelect)
-            let notification = UIBarButtonItem(image: UIImage(systemName: "bell"), style: .plain, action: tapNotification)
+            //            let select = UIBarButtonItem(title: NSLocalizedString("_select_", comment: ""), style: UIBarButtonItem.Style.plain, action: tapSelect)
+            let notification = UIBarButtonItem(image: .init(systemName: "bell"), style: .plain, action: tapNotification)
+            //            if layoutKey == NCGlobal.shared.layoutViewFiles {
+            //                navigationItem.rightBarButtonItems = [select, notification]
+            //            } else {
+            //                navigationItem.rightBarButtonItems = [select]
+            //            }
+
+            let menu = UIMenu(children: createMenuActions())
+            let menuButton = UIBarButtonItem(image: .init(systemName: "ellipsis.circle"), menu: menu)
+
             if layoutKey == NCGlobal.shared.layoutViewFiles {
-                navigationItem.rightBarButtonItems = [select, notification]
+                navigationItem.rightBarButtonItems = [menuButton, notification]
             } else {
-                navigationItem.rightBarButtonItems = [select]
+                navigationItem.rightBarButtonItems = [menuButton]
             }
+
+            //            navigationItem.rightBarButtonItems = [menuButton]
         }
+    }
+
+    private func createMenuActions() -> [UIMenuElement] {
+        let select = UIAction(title: NSLocalizedString("_select_", comment: ""), image: .init(systemName: "checkmark.circle")) { _ in self.tapSelectMenu() }
+
+        let list = UIAction(title: NSLocalizedString("_list_", comment: ""), image: .init(systemName: "list.bullet")) { _ in self.tapSelectMenu() }
+        let grid = UIAction(title: NSLocalizedString("_grid_", comment: ""), image: .init(systemName: "square.grid.2x2")) { _ in self.tapSelectMenu() }
+
+        let viewStyleSubmenu = UIMenu(title: "", options: .displayInline, children: [list, grid])
+
+        let byName = UIAction(title: NSLocalizedString("_order_by_name_a_z_", comment: ""), image: nil) { _ in self.tapSelectMenu() }
+        let byNewest = UIAction(title: NSLocalizedString("_order_by_date_more_recent_", comment: ""), image: nil) { _ in self.tapSelectMenu() }
+        let byLargest = UIAction(title: NSLocalizedString("_order_by_size_smallest_", comment: ""), image: nil) { _ in self.tapSelectMenu() }
+
+        let sortSubmenu = UIMenu(title: "", options: .displayInline, children: [byName, byNewest, byLargest])
+
+        let foldersOnTop = UIAction(title: NSLocalizedString("_directory_on_top_no_", comment: ""), image: nil) { _ in self.tapSelectMenu() }
+
+        let foldersSubmenu = UIMenu(title: "", options: .displayInline, children: [foldersOnTop])
+
+        return [select, viewStyleSubmenu, sortSubmenu, foldersSubmenu]
     }
 
     func tapSelect() {
