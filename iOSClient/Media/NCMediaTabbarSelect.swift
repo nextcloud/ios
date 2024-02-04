7b960fc7272ad0d4c59dc73671a0116ed708eb15
Marino Faggiana <marino@marinofaggiana.com>
2024-02-04 11:08:26 +0100

    fix
    Signed-off-by: Marino Faggiana <marino@marinofaggiana.com>

--- iOSClient/Media/NCMediaTabbarSelect.swift
+++ iOSClient/Media/NCMediaTabbarSelect.swift
@@ -14,42 +14,10 @@ protocol NCTabBarSelectDelegate: AnyObject {
     func delete(tabBarSelect: NCMediaTabbarSelect)
 }
 
-class NCMediaUIHostingController: UIHostingController<MediaTabBarSelectView> {
-
-    var mediaTabBarController: UITabBarController?
-    var heightAnchor: NSLayoutConstraint?
-    var bottomAnchor: NSLayoutConstraint?
-
-    override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
-        super.viewWillTransition(to: size, with: coordinator)
-
-        coordinator.animate(alongsideTransition: nil) { _ in
-
-            if let tabBarController = self.mediaTabBarController,
-               let heightAnchor = self.heightAnchor,
-               let bottomAnchor = self.bottomAnchor {
-
-                let height = tabBarController.tabBar.frame.height
-                let safeAreaInsetsBottom = tabBarController.view.safeAreaInsets.bottom
-
-                self.bottomAnchor?.isActive = false
-                self.view.removeConstraint(bottomAnchor)
-                self.bottomAnchor = self.view.bottomAnchor.constraint(equalTo: tabBarController.view.bottomAnchor, constant: safeAreaInsetsBottom)
-                self.bottomAnchor?.isActive = true
-
-                self.heightAnchor?.isActive = false
-                self.view.removeConstraint(heightAnchor)
-                self.heightAnchor = self.view.heightAnchor.constraint(equalToConstant: height + safeAreaInsetsBottom)
-                self.heightAnchor?.isActive = true
-            }
-        }
-    }
-}
-
 class NCMediaTabbarSelect: ObservableObject {
 
+    var hostingController: UIViewController!
     var mediaTabBarController: UITabBarController?
-    var mediaUIHostingController: NCMediaUIHostingController!
     open weak var delegate: NCTabBarSelectDelegate?
 
     @Published var selectCount: Int = 0
@@ -57,40 +25,36 @@ class NCMediaTabbarSelect: ObservableObject {
     init(tabBarController: UITabBarController? = nil, delegate: NCTabBarSelectDelegate? = nil) {
 
         guard let tabBarController else { return }
-        let height = tabBarController.tabBar.frame.height + tabBarController.view.safeAreaInsets.bottom
         let mediaTabBarSelectView = MediaTabBarSelectView(tabBarSelect: self)
-        mediaUIHostingController = NCMediaUIHostingController(rootView: mediaTabBarSelectView)
+        hostingController = UIHostingController(rootView: mediaTabBarSelectView)
 
         self.mediaTabBarController = tabBarController
         self.delegate = delegate
 
-        tabBarController.addChild(mediaUIHostingController)
-        tabBarController.view.addSubview(mediaUIHostingController.view)
+        tabBarController.addChild(hostingController)
+        tabBarController.view.addSubview(hostingController.view)
 
-        mediaUIHostingController.mediaTabBarController = mediaTabBarController
-        mediaUIHostingController.view.translatesAutoresizingMaskIntoConstraints = false
-        mediaUIHostingController.view.rightAnchor.constraint(equalTo: tabBarController.view.rightAnchor).isActive = true
-        mediaUIHostingController.view.leftAnchor.constraint(equalTo: tabBarController.view.leftAnchor).isActive = true
+        hostingController.view.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            hostingController.view.rightAnchor.constraint(equalTo: tabBarController.tabBar.rightAnchor),
+            hostingController.view.leftAnchor.constraint(equalTo: tabBarController.tabBar.leftAnchor),
+            hostingController.view.topAnchor.constraint(equalTo: tabBarController.tabBar.topAnchor),
+            hostingController.view.bottomAnchor.constraint(equalTo: tabBarController.tabBar.bottomAnchor)
+        ])
 
-        mediaUIHostingController.bottomAnchor = mediaUIHostingController.view.bottomAnchor.constraint(equalTo: tabBarController.view.bottomAnchor, constant: tabBarController.view.safeAreaInsets.bottom)
-        mediaUIHostingController.bottomAnchor?.isActive = true
-
-        mediaUIHostingController.heightAnchor = mediaUIHostingController.view.heightAnchor.constraint(equalToConstant: height)
-        mediaUIHostingController.heightAnchor?.isActive = true
-
-        mediaUIHostingController.view.backgroundColor = .clear
-        mediaUIHostingController.view.isHidden = true
+        hostingController.view.backgroundColor = .clear
+        hostingController.view.isHidden = true
     }
 
     func show(animation: Bool) {
 
         mediaTabBarController?.tabBar.isHidden = true
-        mediaUIHostingController.view.isHidden = false
+        hostingController.view.isHidden = false
     }
 
     func hide(animation: Bool) {
 
-        mediaUIHostingController.view.isHidden = true
+        hostingController.view.isHidden = true
         mediaTabBarController?.tabBar.isHidden = false
     }
 }
