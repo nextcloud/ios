050379daf8c634cb96fd64b6457ae3b7ff92d766
Marino Faggiana <marino@marinofaggiana.com>
2024-02-08 12:44:36 +0100

    lint
    Signed-off-by: Marino Faggiana <marino@marinofaggiana.com>

--- iOSClient/Networking/NCNetworkingProcess.swift
+++ iOSClient/Networking/NCNetworkingProcess.swift
@@ -120,6 +120,7 @@ class NCNetworkingProcess: NSObject {
 
             // DOWNLOAD
             //
+
             let limitDownload = maxConcurrentOperationDownload - counterDownload
             let metadatasDownload = NCManageDatabase.shared.getAdvancedMetadatas(predicate: NSPredicate(format: "account == %@ AND session == %@ AND status == %d", self.appDelegate.account, NCNetworking.shared.sessionDownloadBackground, NCGlobal.shared.metadataStatusWaitDownload), page: 1, limit: limitDownload, sorted: "date", ascending: true)
             for metadata in metadatasDownload where counterDownload < maxConcurrentOperationDownload {
