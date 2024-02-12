59c57b09b93de804750b128a7d29118ce94f4ce0
Marino Faggiana <marino@marinofaggiana.com>
2024-02-12 14:15:15 +0100

    log
    Signed-off-by: Marino Faggiana <marino@marinofaggiana.com>

--- iOSClient/AppDelegate.swift
+++ iOSClient/AppDelegate.swift
@@ -368,7 +368,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterD
             NextcloudKit.shared.nkCommonInstance.writeLog("[INFO] \(taskText) auto upload with \(items) uploads")
 
             NCNetworkingProcess.shared.start { counterDownload, counterUpload in
-                NextcloudKit.shared.nkCommonInstance.writeLog("[INFO] \(taskText) task upload process with download: \(counterDownload) upload: \(counterUpload)")
+                NextcloudKit.shared.nkCommonInstance.writeLog("[INFO] \(taskText) networking process with download: \(counterDownload) upload: \(counterUpload)")
 
                 if taskText == "ProcessingTask",
                    items == 0, counterDownload == 0, counterUpload == 0,
