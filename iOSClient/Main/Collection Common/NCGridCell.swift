6da2d8a355d8daa9b8c11cb9d3c33f627546b25e
Milen Pivchev <milen.pivchev@gmail.com>
2024-02-29 12:33:57 +0100

    WIP
    Signed-off-by: Milen Pivchev <milen.pivchev@gmail.com>

--- iOSClient/Main/Collection Common/NCGridCell.swift
+++ iOSClient/Main/Collection Common/NCGridCell.swift
@@ -32,6 +32,7 @@ class NCGridCell: UICollectionViewCell, UIGestureRecognizerDelegate, NCCellProto
     @IBOutlet weak var imageLocal: UIImageView!
     @IBOutlet weak var labelTitle: UILabel!
     @IBOutlet weak var labelInfo: UILabel!
+    @IBOutlet weak var labelSize: UILabel!
     @IBOutlet weak var buttonMore: UIButton!
     @IBOutlet weak var imageVisualEffect: UIVisualEffectView!
     @IBOutlet weak var progressView: UIProgressView!
