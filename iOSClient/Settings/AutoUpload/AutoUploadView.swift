76f270c684afa7f83a570a1fbbc0127cb3682415
adityagi02 <adityagi02@yahoo.com>
2024-03-06 21:28:54 +0530

    Added AutoUpload UI
    Signed-off-by: adityagi02 <adityagi02@yahoo.com>

---
+++ iOSClient/Settings/AutoUpload/AutoUploadView.swift
@@ -0,0 +1,19 @@
+//
+//  AutoUploadView.swift
+//  Nextcloud
+//
+//  Created by Aditya Tyagi on 06/03/24.
+//  Copyright © 2024 Marino Faggiana. All rights reserved.
+//
+
+import SwiftUI
+
+struct AutoUploadView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AutoUploadView()
+}
