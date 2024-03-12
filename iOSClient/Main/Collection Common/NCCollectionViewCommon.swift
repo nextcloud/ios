1c78cd75103bcbd70d54369b0f8bd8ccaeb9e0b3
Marino Faggiana <marino@marinofaggiana.com>
2024-03-12 07:35:21 +0100

    coding
    Signed-off-by: Marino Faggiana <marino@marinofaggiana.com>

--- iOSClient/Main/Collection Common/NCCollectionViewCommon.swift
+++ iOSClient/Main/Collection Common/NCCollectionViewCommon.swift
@@ -1660,6 +1660,8 @@ extension NCCollectionViewCommon: EasyTipViewDelegate {
 
 extension NCCollectionViewCommon: NCSelectableNavigationView, NCCollectionViewCommonSelectTabBarDelegate {
     func setNavigationRightItems(enableMenu: Bool = false) {
+        if layoutKey == NCGlobal.shared.layoutViewTransfers { return }
+
         var selectedMetadatas: [tableMetadata] = []
         var isAnyOffline = false
         var isAnyDirectory = false
@@ -1884,7 +1886,6 @@ extension NCCollectionViewCommon: NCSelectableNavigationView, NCCollectionViewCo
             if isName { // repeated press
                 layoutForView.ascending = !layoutForView.ascending
             }
-
             layoutForView.sort = "fileName"
             self.saveLayout(layoutForView)
         }
@@ -1893,7 +1894,6 @@ extension NCCollectionViewCommon: NCSelectableNavigationView, NCCollectionViewCo
             if isDate { // repeated press
                 layoutForView.ascending = !layoutForView.ascending
             }
-
             layoutForView.sort = "date"
             self.saveLayout(layoutForView)
         }
@@ -1902,7 +1902,6 @@ extension NCCollectionViewCommon: NCSelectableNavigationView, NCCollectionViewCo
             if isSize { // repeated press
                 layoutForView.ascending = !layoutForView.ascending
             }
-
             layoutForView.sort = "size"
             self.saveLayout(layoutForView)
         }
@@ -1914,21 +1913,29 @@ extension NCCollectionViewCommon: NCSelectableNavigationView, NCCollectionViewCo
             self.saveLayout(layoutForView)
         }
 
-        let showDescriptionKeychain = NCKeychain().showDescription
+        let personalFilesOnly = NCKeychain().getPersonalFilesOnly(account: appDelegate.account)
+        let personalFilesOnlyAction = UIAction(title: NSLocalizedString("_personal_files_only_", comment: ""), image: UIImage(systemName: "folder.badge.person.crop"), state: personalFilesOnly ? .on : .off) { _ in
+            NCKeychain().setPersonalFilesOnly(account: self.appDelegate.account, value: !personalFilesOnly)
+            self.reloadDataSource()
+        }
 
+        let showDescriptionKeychain = NCKeychain().showDescription
         let showDescription = UIAction(title: NSLocalizedString("_show_description_", comment: ""), image: UIImage(systemName: "list.dash.header.rectangle"), attributes: richWorkspaceText == nil ? .disabled : [], state: showDescriptionKeychain && richWorkspaceText != nil ? .on : .off) { _ in
             NCKeychain().showDescription = !showDescriptionKeychain
             self.collectionView.reloadData()
             self.setNavigationRightItems()
         }
-
         showDescription.subtitle = richWorkspaceText == nil ? NSLocalizedString("_no_description_available_", comment: "") : ""
-
-        let additionalSubmenu = UIMenu(title: "", options: .displayInline, children: [foldersOnTop, showDescription])
 
         if layoutKey == NCGlobal.shared.layoutViewRecent {
             return [select]
         } else {
+            var additionalSubmenu = UIMenu()
+            if layoutKey == NCGlobal.shared.layoutViewFiles {
+                additionalSubmenu = UIMenu(title: "", options: .displayInline, children: [foldersOnTop, personalFilesOnlyAction, showDescription])
+            } else {
+                additionalSubmenu = UIMenu(title: "", options: .displayInline, children: [foldersOnTop, showDescription])
+            }
             return [select, viewStyleSubmenu, sortSubmenu, additionalSubmenu]
         }
     }
