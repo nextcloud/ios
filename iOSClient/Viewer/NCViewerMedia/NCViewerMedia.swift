37339a2f2b386db2cf8e7401c9d8162137085d21
Milen Pivchev <milen.pivchev@gmail.com>
2024-03-18 13:31:04 +0100

    Refactor
    Signed-off-by: Milen Pivchev <milen.pivchev@gmail.com>

--- iOSClient/Viewer/NCViewerMedia/NCViewerMedia.swift
+++ iOSClient/Viewer/NCViewerMedia/NCViewerMedia.swift
@@ -36,31 +36,6 @@ public protocol NCViewerMediaViewDelegate: AnyObject {
     func didCloseDetail()
 }
 
-@MainActor
-struct Analyzer {
-    private var _imageAnalyzer: Any?
-    private var _imageInteraction: Any?
-
-    @available(iOS 16, *)
-    var imageAnalyzer: ImageAnalyzer? {
-        get { return _imageAnalyzer as? ImageAnalyzer }
-        set { _imageAnalyzer = newValue }
-    }
-
-    @available(iOS 16, *)
-    var imageInteraction: ImageAnalysisInteraction? {
-        get { return _imageInteraction as? ImageAnalysisInteraction }
-        set { _imageInteraction = newValue }
-    }
-
-    init() {
-        if #available(iOS 16, *) {
-            imageAnalyzer = ImageAnalyzer()
-            imageInteraction = ImageAnalysisInteraction()
-        }
-    }
-}
-
 class NCViewerMedia: UIViewController {
     @IBOutlet weak var detailViewTopConstraint: NSLayoutConstraint!
     @IBOutlet weak var imageViewTopConstraint: NSLayoutConstraint!
@@ -74,6 +49,7 @@ class NCViewerMedia: UIViewController {
     private var tipView: EasyTipView?
     private let player = VLCMediaPlayer()
     private let appDelegate = (UIApplication.shared.delegate as? AppDelegate)!
+    let analyzer = Analyzer()
     let utilityFileSystem = NCUtilityFileSystem()
     let utility = NCUtility()
     weak var viewerMediaPage: NCViewerMediaPage?
@@ -97,27 +73,7 @@ class NCViewerMedia: UIViewController {
 
     private var allowOpeningDetails = true
 
-    let analyzer = Analyzer()
-
     // MARK: - View Life Cycle
-
-    @available(iOS 16.0, *)
-    func analyzeCurrentImage() {
-            if let image = image {
-                Task {
-                    let configuration = ImageAnalyzer.Configuration([.text, .machineReadableCode, .visualLookUp])
-                    do {
-                        let analysis = try await analyzer.imageAnalyzer?.analyze(image, configuration: configuration)
-                        if image == self.image {
-                            analyzer.imageInteraction?.analysis = analysis
-                            analyzer.imageInteraction?.preferredInteractionTypes = .automatic
-                        }
-                    } catch {
-                        // Handle error…
-                    }
-                }
-            }
-        }
 
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
