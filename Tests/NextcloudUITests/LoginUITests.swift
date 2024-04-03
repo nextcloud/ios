875a02f6cba40c520269b7e5a8e9472e4c7ed330
Milen Pivchev <milen.pivchev@gmail.com>
2024-04-03 19:12:05 +0200

    WIP
    Signed-off-by: Milen Pivchev <milen.pivchev@gmail.com>

--- Tests/NextcloudUITests/LoginUITests.swift
+++ Tests/NextcloudUITests/LoginUITests.swift
@@ -71,37 +71,3 @@ final class LoginUITests: BaseUIXCTestCase {
         XCTAssert(app.tabBars["Tab Bar"].waitForExistence(timeout: timeoutSeconds))
     }
 }
-
-extension XCUIElement {
-    var hasFocus: Bool { value(forKey: "hasKeyboardFocus") as? Bool ?? false }
-
-    func waitUntilExists(timeout: TimeInterval = 600, file: StaticString = #file, line: UInt = #line) -> XCUIElement {
-            let elementExists = waitForExistence(timeout: timeout)
-            if elementExists {
-                return self
-            } else {
-                XCTFail("Could not find \(self) before timeout", file: file, line: line)
-            }
-
-            return self
-        }
-}
-
-extension XCTestCase {
-    func waitUntilElementHasFocus(element: XCUIElement, timeout: TimeInterval = 600, file: StaticString = #file, line: UInt = #line) -> XCUIElement {
-        let expectation = expectation(description: "waiting for element \(element) to have focus")
-
-        let timer = Timer(timeInterval: 1, repeats: true) { timer in
-            guard element.hasFocus else { return }
-
-            expectation.fulfill()
-            timer.invalidate()
-        }
-
-        RunLoop.current.add(timer, forMode: .common)
-
-        wait(for: [expectation], timeout: timeout)
-
-        return element
-    }
-}
