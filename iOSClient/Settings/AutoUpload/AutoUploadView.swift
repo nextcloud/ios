e0b7dfd1985b9e5dca1f5f7c800f9c91734e7db9
adityagi02 <adityagi02@yahoo.com>
2024-04-06 13:21:49 +0530

    Added standard File Header
    Signed-off-by: adityagi02 <adityagi02@yahoo.com>

--- iOSClient/Settings/AutoUpload/AutoUploadView.swift
+++ iOSClient/Settings/AutoUpload/AutoUploadView.swift
@@ -5,6 +5,21 @@
 //  Created by Aditya Tyagi on 06/03/24.
 //  Copyright © 2024 Marino Faggiana. All rights reserved.
 //
+//  Author Aditya Tyagi <adityagi02@yahoo.com>
+//
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
 
 import SwiftUI
 import UIKit
