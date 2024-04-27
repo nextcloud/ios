95004c3c9fa3f1c0e0b27148cec8780ed5a47092
Marino Faggiana <marino@marinofaggiana.com>
2024-04-27 11:19:24 +0200

    coding
    Signed-off-by: Marino Faggiana <marino@marinofaggiana.com>

--- iOSClient/Media/NCMedia.swift
+++ iOSClient/Media/NCMedia.swift
@@ -251,8 +251,8 @@ class NCMedia: UIViewController {
         guard let userInfo = notification.userInfo as NSDictionary?,
               let dragDrop = userInfo["dragdrop"] as? Bool, dragDrop else { return }
 
-        DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
-            self.reloadDataSource()
+        DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+            self.searchMediaUI()
         }
     }
 
@@ -260,8 +260,8 @@ class NCMedia: UIViewController {
         guard let userInfo = notification.userInfo as NSDictionary?,
               let dragDrop = userInfo["dragdrop"] as? Bool, dragDrop else { return }
 
-        DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
-            self.reloadDataSource()
+        DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+            self.searchMediaUI()
         }
     }
 
