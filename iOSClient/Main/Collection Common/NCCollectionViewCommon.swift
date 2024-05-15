3e6ca66675688a3e1448b5bd6d3d0e6808b340af
Marino Faggiana <marino@marinofaggiana.com>
2024-05-15 13:17:35 +0200

    coding
    Signed-off-by: Marino Faggiana <marino@marinofaggiana.com>

--- iOSClient/Main/Collection Common/NCCollectionViewCommon.swift
+++ iOSClient/Main/Collection Common/NCCollectionViewCommon.swift
@@ -546,6 +546,7 @@ class NCCollectionViewCommon: UIViewController, UIGestureRecognizerDelegate, UIS
                     self.collectionView.reloadData()
                 }
                 self.headerMenu?.progressTransfer.progress = progressNumber.floatValue
+                print("XXX \(progressNumber.floatValue) - \(self.headerMenu?.progressTransfer.progress)")
             } else {
                 guard let indexPath = self.dataSource.getIndexPathMetadata(ocId: ocId).indexPath,
                       let cell = self.collectionView?.cellForItem(at: indexPath),
