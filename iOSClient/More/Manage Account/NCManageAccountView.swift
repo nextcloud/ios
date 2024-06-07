23bf9841a7f8e58a4d45677b92e4c4515155d67b
Marino Faggiana <marino@marinofaggiana.com>
2024-06-07 14:13:28 +0200

    coding
    Signed-off-by: Marino Faggiana <marino@marinofaggiana.com>

--- iOSClient/More/Manage Account/NCManageAccountView.swift
+++ iOSClient/More/Manage Account/NCManageAccountView.swift
@@ -31,162 +31,186 @@ struct NCManageAccountView: View {
 
     var body: some View {
         Form {
-            TabView(selection: $model.indexActiveAccount) {
-                ForEach(0..<model.accounts.count, id: \.self) { index in
-                    let account = model.accounts[index]
-                    let status = model.getUserStatus(account: account)
-                    let avatar = NCUtility().loadUserImage(for: account.user, displayName: account.displayName, userBaseUrl: account)
-                    /// Avatar zone
-                    VStack {
-                        Image(uiImage: avatar)
-                            .resizable()
-                            .scaledToFit()
-                            .frame(width: UIScreen.main.bounds.width, height: 75)
-                            .clipped()
-                        Text(model.getUserName(account: account))
-                            .font(.system(size: 16))
-                        if let message = status.statusMessage {
-                            Spacer()
-                                .frame(height: 10)
-                            Text(message)
-                                .font(.system(size: 10))
-                        }
-                        ///
-                        Spacer()
-                            .frame(height: 50)
-                        /// Change alias
-                        HStack {
-                            Text(NSLocalizedString("_alias_", comment: ""))
-                                .font(.system(size: 17))
-                                .fontWeight(.medium)
-                            Spacer()
-                            TextField(NSLocalizedString("_alias_placeholder_", comment: ""), text: $model.alias)
-                                .onSubmit {
-                                    model.submitChangedAlias(account: account)
-                                }
-                                .font(.system(size: 16))
-                                .multilineTextAlignment(.trailing)
-                        }
-                        .padding(EdgeInsets(top: 0, leading: 15, bottom: 0, trailing: 15))
-
-                        Text(NSLocalizedString("_alias_footer_", comment: ""))
-                            .padding(EdgeInsets(top: 1, leading: 15, bottom: 0, trailing: 15))
-                            .frame(maxWidth: .infinity, alignment: .leading)
-                            .font(.system(size: 12))
-                            .foregroundStyle(Color(UIColor.lightGray))
-                        ///
-                        Divider()
-                            .padding(EdgeInsets(top: 5, leading: 15, bottom: 5, trailing: 5))
-                        /// User Status
-                        Button(action: {
-                            showUserStatus.toggle()
-                        }, label: {
-                            HStack {
-                                Image(systemName: "moon.fill")
-                                    .resizable()
-                                    .scaledToFit()
-                                    .font(Font.system(.body).weight(.light))
-                                    .frame(width: 25, height: 25)
-                                    .foregroundStyle(Color(NCBrandColor.shared.iconImageColor))
-                                Text(NSLocalizedString("_set_user_status_", comment: ""))
-                                    .lineLimit(1)
-                                    .truncationMode(.middle)
-                                    .foregroundStyle(Color(NCBrandColor.shared.textColor))
-                                    .padding(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 15))
-                            }
-                            .font(.system(size: 14))
-                        })
-                        .padding(EdgeInsets(top: 0, leading: 15, bottom: 0, trailing: 0))
-                        .frame(maxWidth: .infinity, alignment: .leading)
-                        .sheet(isPresented: $showUserStatus) {
-                        }
-                        ///
-                        Divider()
-                            .padding(EdgeInsets(top: 5, leading: 15, bottom: 5, trailing: 5))
-                        /// Certificate server
-                        Button(action: {
-                            showServerCertificate.toggle()
-                        }, label: {
-                            HStack {
-                                Image(systemName: "lock")
-                                    .resizable()
-                                    .scaledToFit()
-                                    .font(Font.system(.body).weight(.light))
-                                    .frame(width: 25, height: 25)
-                                    .foregroundStyle(Color(NCBrandColor.shared.iconImageColor))
-                                Text(NSLocalizedString("_certificate_details_", comment: ""))
-                                    .lineLimit(1)
-                                    .truncationMode(.middle)
-                                    .foregroundStyle(Color(NCBrandColor.shared.textColor))
-                                    .padding(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 15))
-                            }
-                            .font(.system(size: 14))
-                        })
-                        .padding(EdgeInsets(top: 0, leading: 15, bottom: 0, trailing: 0))
-                        .frame(maxWidth: .infinity, alignment: .leading)
-                        .sheet(isPresented: $showServerCertificate) {
-                        }
-                        ///
-                        Divider()
-                            .padding(EdgeInsets(top: 5, leading: 15, bottom: 5, trailing: 0))
-                        /// Certificate push
-                        Button(action: {
-                            showPushCertificate.toggle()
-                        }, label: {
-                            HStack {
-                                Image(systemName: "lock")
-                                    .resizable()
-                                    .scaledToFit()
-                                    .font(Font.system(.body).weight(.light))
-                                    .frame(width: 25, height: 25)
-                                    .foregroundStyle(Color(NCBrandColor.shared.iconImageColor))
-                                Text(NSLocalizedString("_certificate_pn_details_", comment: ""))
-                                    .lineLimit(1)
-                                    .truncationMode(.middle)
-                                    .foregroundStyle(Color(NCBrandColor.shared.textColor))
-                                    .padding(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 15))
-                            }
-                            .font(.system(size: 14))
-                        })
-                        .padding(EdgeInsets(top: 0, leading: 15, bottom: 0, trailing: 0))
-                        .frame(maxWidth: .infinity, alignment: .leading)
-                        .sheet(isPresented: $showPushCertificate) {
-                        }
-                        ///
-                        Divider()
-                            .padding(EdgeInsets(top: 5, leading: 15, bottom: 5, trailing: 0))
-                        /// Delete account
-                        Button(action: {
-
-                        }, label: {
-                        HStack {
-                            Image(systemName: "trash")
+            Section(content: {
+                TabView(selection: $model.indexActiveAccount) {
+                    ForEach(0..<model.accounts.count, id: \.self) { index in
+                        let account = model.accounts[index]
+                        let status = model.getUserStatus(account: account)
+                        let avatar = NCUtility().loadUserImage(for: account.user, displayName: account.displayName, userBaseUrl: account)
+                        /// Avatar zone
+                        VStack {
+                            Image(uiImage: avatar)
                                 .resizable()
                                 .scaledToFit()
-                                .font(Font.system(.body).weight(.light))
-                                .frame(width: 25, height: 25)
-                                .foregroundStyle(.red)
-                            Text(NSLocalizedString("_remove_local_account_", comment: ""))
-                                .lineLimit(1)
-                                .truncationMode(.middle)
-                                .foregroundStyle(.red)
-                                .padding(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 15))
+                                .frame(width: UIScreen.main.bounds.width, height: 75)
+                                .clipped()
+                            Text(model.getUserName(account: account))
+                                .font(.system(size: 16))
+                            if let message = status.statusMessage {
+                                Spacer()
+                                    .frame(height: 10)
+                                Text(message)
+                                    .font(.system(size: 10))
+                            }
+                            ///
+                            Spacer()
+                                .frame(height: 50)
+                            /// Change alias
+                            HStack {
+                                Text(NSLocalizedString("_alias_", comment: ""))
+                                    .font(.system(size: 17))
+                                    .fontWeight(.medium)
+                                Spacer()
+                                TextField(NSLocalizedString("_alias_placeholder_", comment: ""), text: $model.alias)
+                                    .onSubmit {
+                                        model.submitChangedAlias(account: account)
+                                    }
+                                    .font(.system(size: 16))
+                                    .multilineTextAlignment(.trailing)
+                            }
+                            .padding(EdgeInsets(top: 0, leading: 20, bottom: 0, trailing: 20))
+
+                            Text(NSLocalizedString("_alias_footer_", comment: ""))
+                                .padding(EdgeInsets(top: 1, leading: 20, bottom: 0, trailing: 20))
+                                .frame(maxWidth: .infinity, alignment: .leading)
+                                .font(.system(size: 12))
+                                .lineLimit(2)
+                                .foregroundStyle(Color(UIColor.lightGray))
+                            ///
+                            Divider()
+                                .padding(EdgeInsets(top: 5, leading: 20, bottom: 5, trailing: 0))
+                            /// User Status
+                            Button(action: {
+                                showUserStatus.toggle()
+                            }, label: {
+                                HStack {
+                                    Image(systemName: "moon.fill")
+                                        .resizable()
+                                        .scaledToFit()
+                                        .font(Font.system(.body).weight(.light))
+                                        .frame(width: 20, height: 20)
+                                        .foregroundStyle(Color(NCBrandColor.shared.iconImageColor))
+                                    Text(NSLocalizedString("_set_user_status_", comment: ""))
+                                        .lineLimit(1)
+                                        .truncationMode(.middle)
+                                        .foregroundStyle(Color(NCBrandColor.shared.textColor))
+                                        .padding(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 20))
+                                }
+                                .font(.system(size: 14))
+                            })
+                            .padding(EdgeInsets(top: 0, leading: 20, bottom: 0, trailing: 0))
+                            .frame(maxWidth: .infinity, alignment: .leading)
+                            .sheet(isPresented: $showUserStatus) {
+                            }
+                            ///
+                            Divider()
+                                .padding(EdgeInsets(top: 5, leading: 20, bottom: 5, trailing: 0))
+                            /// Certificate server
+                            Button(action: {
+                                showServerCertificate.toggle()
+                            }, label: {
+                                HStack {
+                                    Image(systemName: "lock")
+                                        .resizable()
+                                        .scaledToFit()
+                                        .font(Font.system(.body).weight(.light))
+                                        .frame(width: 20, height: 20)
+                                        .foregroundStyle(Color(NCBrandColor.shared.iconImageColor))
+                                    Text(NSLocalizedString("_certificate_details_", comment: ""))
+                                        .lineLimit(1)
+                                        .truncationMode(.middle)
+                                        .foregroundStyle(Color(NCBrandColor.shared.textColor))
+                                        .padding(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 20))
+                                }
+                                .font(.system(size: 14))
+                            })
+                            .padding(EdgeInsets(top: 0, leading: 20, bottom: 0, trailing: 0))
+                            .frame(maxWidth: .infinity, alignment: .leading)
+                            .sheet(isPresented: $showServerCertificate) {
+                            }
+                            ///
+                            Divider()
+                                .padding(EdgeInsets(top: 5, leading: 20, bottom: 5, trailing: 0))
+                            /// Certificate push
+                            Button(action: {
+                                showPushCertificate.toggle()
+                            }, label: {
+                                HStack {
+                                    Image(systemName: "lock")
+                                        .resizable()
+                                        .scaledToFit()
+                                        .font(Font.system(.body).weight(.light))
+                                        .frame(width: 20, height: 20)
+                                        .foregroundStyle(Color(NCBrandColor.shared.iconImageColor))
+                                    Text(NSLocalizedString("_certificate_pn_details_", comment: ""))
+                                        .lineLimit(1)
+                                        .truncationMode(.middle)
+                                        .foregroundStyle(Color(NCBrandColor.shared.textColor))
+                                        .padding(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 20))
+                                }
+                                .font(.system(size: 14))
+                            })
+                            .padding(EdgeInsets(top: 0, leading: 20, bottom: 0, trailing: 0))
+                            .frame(maxWidth: .infinity, alignment: .leading)
+                            .sheet(isPresented: $showPushCertificate) {
+                            }
+                            ///
+                            Divider()
+                                .padding(EdgeInsets(top: 5, leading: 20, bottom: 5, trailing: 0))
+                            /// Delete account
+                            Button(action: {
+
+                            }, label: {
+                                HStack {
+                                    Image(systemName: "trash")
+                                        .resizable()
+                                        .scaledToFit()
+                                        .font(Font.system(.body).weight(.light))
+                                        .frame(width: 20, height: 20)
+                                        .foregroundStyle(.red)
+                                    Text(NSLocalizedString("_remove_local_account_", comment: ""))
+                                        .lineLimit(1)
+                                        .truncationMode(.middle)
+                                        .foregroundStyle(.red)
+                                        .padding(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 20))
+                                }
+                                .font(.system(size: 14))
+                            })
+                            .padding(EdgeInsets(top: 0, leading: 20, bottom: 0, trailing: 0))
+                            .frame(maxWidth: .infinity, alignment: .leading)
                         }
-                        .font(.system(size: 14))
-                        })
-                        .padding(EdgeInsets(top: 0, leading: 15, bottom: 0, trailing: 0))
-                        .frame(maxWidth: .infinity, alignment: .leading)
                     }
                 }
-            }
-            .listRowInsets(EdgeInsets(top: 10, leading: 0, bottom: 10, trailing: 0))
-            .tabViewStyle(PageTabViewStyle(indexDisplayMode: .never))
-            .frame(height: 410)
-            .onChange(of: model.indexActiveAccount) { index in
-                if let account = model.getTableAccount(account: model.accounts[index].account) {
-                    model.alias = account.alias
+                .listRowInsets(EdgeInsets(top: 20, leading: 0, bottom: 20, trailing: 0))
+                .tabViewStyle(PageTabViewStyle(indexDisplayMode: .never))
+                .frame(height: 390)
+                .onChange(of: model.indexActiveAccount) { index in
+                    if let account = model.getTableAccount(account: model.accounts[index].account) {
+                        model.alias = account.alias
+                    }
                 }
-            }
+            })
+            /// All users
+            Section(content: {
+                Button(action: {
+
+                }, label: {
+                    HStack {
+                        Image(systemName: "plus")
+                            .resizable()
+                            .scaledToFit()
+                            .font(Font.system(.body).weight(.light))
+                            .frame(width: 20, height: 20)
+                            .foregroundStyle(Color(NCBrandColor.shared.iconImageColor))
+                        Text(NSLocalizedString("_add_account_", comment: ""))
+                            .lineLimit(1)
+                            .truncationMode(.middle)
+                            .foregroundStyle(Color(NCBrandColor.shared.iconImageColor))
+                            .padding(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 20))
+                    }
+                    .font(.system(size: 14))
+                })
+            })
         }
         .navigationBarTitle(NSLocalizedString("_credentials_", comment: ""))
         .defaultViewModifier(model)
