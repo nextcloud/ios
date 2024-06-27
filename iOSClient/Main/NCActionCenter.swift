f62cc5ac7e3100da9291b5ceadd396d4b0745805
Marino Faggiana <marino@marinofaggiana.com>
2024-06-27 17:38:56 +0200

    fix
    Signed-off-by: Marino Faggiana <marino@marinofaggiana.com>

--- iOSClient/Main/NCActionCenter.swift
+++ iOSClient/Main/NCActionCenter.swift
@@ -250,12 +250,10 @@ class NCActionCenter: NSObject, UIDocumentInteractionControllerDelegate, NCSelec
     // MARK: - Upload
 
     @objc func uploadedFile(_ notification: NSNotification) {
-
-        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else { return }
         guard let userInfo = notification.userInfo as NSDictionary?,
               let error = userInfo["error"] as? NKError,
               let account = userInfo["account"] as? String,
-              account == appDelegate.account
+              account == self.appDelegate?.account
         else { return }
 
         if error != .success, error.errorCode != NSURLErrorCancelled, error.errorCode != NCGlobal.shared.errorRequestExplicityCancelled {
