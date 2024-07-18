a477bc3dfb815f5b4613c68aa82962ec73d88c58
Marino Faggiana <marino@marinofaggiana.com>
2024-07-18 10:15:55 +0200

    fix db -> new build
    Signed-off-by: Marino Faggiana <marino@marinofaggiana.com>

--- Brand/Database.swift
+++ Brand/Database.swift
@@ -26,4 +26,4 @@ import Foundation
 // Database Realm
 //
 let databaseName                    = "nextcloud.realm"
-let databaseSchemaVersion: UInt64   = 350
+let databaseSchemaVersion: UInt64   = 354
