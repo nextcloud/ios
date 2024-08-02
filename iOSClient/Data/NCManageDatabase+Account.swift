50fbc9d5b1d7255accfa3a951e466e96f1c66e9b
Marino Faggiana <marino@marinofaggiana.com>
2024-08-02 09:56:28 +0200

    NCDomain
    Signed-off-by: Marino Faggiana <marino@marinofaggiana.com>

--- iOSClient/Data/NCManageDatabase+Account.swift
+++ iOSClient/Data/NCManageDatabase+Account.swift
@@ -131,6 +131,16 @@ extension NCManageDatabase {
         return nil
     }
 
+    func getActiveStringAccount() -> String? {
+        do {
+            let realm = try Realm()
+            return realm.objects(tableAccount.self).filter("active == true").first?.account
+        } catch let error as NSError {
+            NextcloudKit.shared.nkCommonInstance.writeLog("[ERROR] Could not access database: \(error)")
+        }
+        return nil
+    }
+
     func getAccounts() -> [String]? {
         do {
             let realm = try Realm()
