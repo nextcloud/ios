429adadd1bd1bf066e0c84e28460f70e54592230
Marino Faggiana <marino@marinofaggiana.com>
2024-08-10 12:20:58 +0200

    cod
    Signed-off-by: Marino Faggiana <marino@marinofaggiana.com>

--- iOSClient/Networking/NCNetworking+Download.swift
+++ iOSClient/Networking/NCNetworking+Download.swift
@@ -279,7 +279,7 @@ extension NCNetworking {
             return
         }
 
-        cell.fileAvatarImageView?.image = utility.loadUserImage(for: user, displayName: dispalyName, urlBase: NCSession.shared.getActiveSession().urlBase)
+        cell.fileAvatarImageView?.image = utility.loadUserImage(for: user, displayName: dispalyName, urlBase: NCSession.shared.getSession(account: account).urlBase)
 
         for case let operation as NCOperationDownloadAvatar in downloadAvatarQueue.operations where operation.fileName == fileName { return }
         downloadAvatarQueue.addOperation(NCOperationDownloadAvatar(user: user, fileName: fileName, fileNameLocalPath: fileNameLocalPath, account: account, cell: cell, view: view))
