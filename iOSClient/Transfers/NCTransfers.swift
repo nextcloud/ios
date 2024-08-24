c77884cbe7b187abdfbd92f73bd85e316979ea67
Marino Faggiana <marino@marinofaggiana.com>
2024-08-24 16:01:46 +0200

    cod
    Signed-off-by: Marino Faggiana <marino@marinofaggiana.com>

--- iOSClient/Transfers/NCTransfers.swift
+++ iOSClient/Transfers/NCTransfers.swift
@@ -189,6 +189,10 @@ class NCTransfers: NCCollectionViewCommon, NCTransferCellDelegate {
         // nothing
     }
 
+    override func collectionView(_ collectionView: UICollectionView, willDisplay cell: UICollectionViewCell, forItemAt indexPath: IndexPath) {
+        // nothing
+    }
+
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "transferCell", for: indexPath) as? NCTransferCell,
               let metadata = dataSource.cellForItemAt(indexPath: indexPath) else {
