69c9e781fbd28eb1aa3586b417426808451244d0
Marino Faggiana <marino@marinofaggiana.com>
2024-08-29 10:15:32 +0200

    cod
    Signed-off-by: Marino Faggiana <marino@marinofaggiana.com>

--- iOSClient/Menu/NCViewer+Menu.swift
+++ iOSClient/Menu/NCViewer+Menu.swift
@@ -68,7 +68,8 @@ extension NCViewer {
         // FAVORITE
         // Workaround: PROPPATCH doesn't work
         // https://github.com/nextcloud/files_lock/issues/68
-        if !metadata.lock {
+        if NCNetworking.shared.isOnline,
+           !metadata.lock {
             actions.append(
                 NCMenuAction(
                     title: metadata.favorite ? NSLocalizedString("_remove_favorites_", comment: "") : NSLocalizedString("_add_favorites_", comment: ""),
