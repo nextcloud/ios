2762443bec6ef411f359a0662aec8e9a27cfd260
Marino Faggiana <marino@marinofaggiana.com>
2024-09-17 11:34:16 +0200

    Pushnotification (#3058)
    * revert
    
    Signed-off-by: Marino Faggiana <marino@marinofaggiana.com>
    
    * fix
    
    Signed-off-by: Marino Faggiana <marino@marinofaggiana.com>
    
    * licence
    
    Signed-off-by: Marino Faggiana <marino@marinofaggiana.com>
    
    * cod
    
    Signed-off-by: Marino Faggiana <marino@marinofaggiana.com>
    
    ---------
    
    Signed-off-by: Marino Faggiana <marino@marinofaggiana.com>

--- iOSClient/Main/Collection Common/NCCollectionViewCommon+CollectionViewDataSourcePrefetching.swift
+++ iOSClient/Main/Collection Common/NCCollectionViewCommon+CollectionViewDataSourcePrefetching.swift
@@ -5,6 +5,21 @@
 //  Created by Marino Faggiana on 16/09/24.
 //  Copyright © 2024 Marino Faggiana. All rights reserved.
 //
+//  Author Marino Faggiana <marino.faggiana@nextcloud.com>
+//
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
 
 import Foundation
 import UIKit
@@ -13,11 +28,10 @@ extension NCCollectionViewCommon: UICollectionViewDataSourcePrefetching {
     func collectionView(_ collectionView: UICollectionView, prefetchItemsAt indexPaths: [IndexPath]) {
         guard !isSearchingMode else { return }
         let ext = global.getSizeExtension(width: self.sizeImage.width)
+        let metadatas = self.dataSource.getMetadatas(indexPaths: indexPaths)
 
         DispatchQueue.global(qos: .userInteractive).async {
-            let metadatas = self.dataSource.getResultMetadatas(indexPaths: indexPaths)
-
-            metadatas.forEach { metadata in
+            for metadata in metadatas where metadata.isImageOrVideo {
                 if self.imageCache.getImageCache(ocId: metadata.ocId, etag: metadata.etag, ext: ext) == nil,
                    let image = self.utility.getImage(ocId: metadata.ocId, etag: metadata.etag, ext: ext) {
                     self.imageCache.addImageCache(ocId: metadata.ocId, etag: metadata.etag, image: image, ext: ext)
