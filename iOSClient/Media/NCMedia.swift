4c0e041dd546087b2d3fcad419b171316fc3c64b
Marino Faggiana <marino@marinofaggiana.com>
2024-09-18 18:22:17 +0200

    cod
    Signed-off-by: Marino Faggiana <marino@marinofaggiana.com>

--- iOSClient/Media/NCMedia.swift
+++ iOSClient/Media/NCMedia.swift
@@ -71,7 +71,7 @@ class NCMedia: UIViewController {
 
     var lastScale: CGFloat = 1.0
     var currentScale: CGFloat = 1.0
-    let maxColumns: Int = 10
+    let maxColumns: Int = 12
     var transitionColumns = false
     var currentExt: String = ""
     var numberOfColumns: Int = 0 {
