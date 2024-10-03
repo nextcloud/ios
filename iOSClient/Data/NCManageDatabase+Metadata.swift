086268b8abd2de41404ac9b3c1777518e78d97c4
Marino Faggiana <marino@marinofaggiana.com>
2024-10-03 10:13:06 +0200

    e2ee
    Signed-off-by: Marino Faggiana <marino@marinofaggiana.com>

--- iOSClient/Data/NCManageDatabase+Metadata.swift
+++ iOSClient/Data/NCManageDatabase+Metadata.swift
@@ -555,18 +555,17 @@ extension NCManageDatabase {
 
     @discardableResult
     func addMetadata(_ metadata: tableMetadata) -> tableMetadata? {
-        let result = tableMetadata(value: metadata)
 
         do {
             let realm = try Realm()
             try realm.write {
-                realm.add(result, update: .all)
+                realm.add(metadata, update: .all)
             }
         } catch let error {
             NextcloudKit.shared.nkCommonInstance.writeLog("[ERROR] Could not write to database: \(error)")
             return nil
         }
-        return result
+        return tableMetadata(value: metadata)
     }
 
     func addMetadatas(_ metadatas: [tableMetadata]) {
