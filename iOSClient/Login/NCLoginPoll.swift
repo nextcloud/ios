698fe0427f543f72b04d25b0425744990e3a4fbe
Vitalii Tolkach <v.tolkach@viseven.com>
2024-10-09 11:54:01 +0200

    TELKOMAUST-641: increase login buttons width to fit french localization

--- iOSClient/Login/NCLoginPoll.swift
+++ iOSClient/Login/NCLoginPoll.swift
@@ -75,7 +75,7 @@ struct NCLoginPoll: View {
                     .buttonStyle(ButtonStylePrimary(maxWidth: .infinity))
                     
 				}
-                .frame(width: 210)
+                .frame(width: 250)
 				.padding(.bottom, size.height * 0.15)
                 .environment(\.colorScheme, .dark)
 			}
