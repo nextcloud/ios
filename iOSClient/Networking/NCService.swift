2e67ccd6db8adac8f87c6628817b188ec010a598
Marino Faggiana <marino@marinofaggiana.com>
2024-10-13 16:03:22 +0200

    removeAllKeyUserDefaultsData
    Signed-off-by: Marino Faggiana <marino@marinofaggiana.com>

--- iOSClient/Networking/NCService.swift
+++ iOSClient/Networking/NCService.swift
@@ -174,8 +174,8 @@ class NCService: NSObject {
             if NCNetworking.shared.isResponseDataChanged(account: account, responseData: presponseData) {
                 NCNetworking.shared.setResponseData(account: account, responseData: presponseData)
                 self.database.addCapabilitiesJSon(data, account: account)
-
             }
+
             guard let capability = self.database.setCapabilities(account: account, data: data) else { return }
 
             // Theming
