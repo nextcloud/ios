0947e90eeb3a373b0d307f6612bb93eade31b8dd
Marino Faggiana <marino@marinofaggiana.com>
2024-10-14 16:48:10 +0200

    fix
    Signed-off-by: Marino Faggiana <marino@marinofaggiana.com>

--- iOSClient/Main/Collection Common/NCCollectionViewCommon+CollectionViewDataSourcePrefetching.swift
+++ iOSClient/Main/Collection Common/NCCollectionViewCommon+CollectionViewDataSourcePrefetching.swift
@@ -32,13 +32,15 @@ extension NCCollectionViewCommon: UICollectionViewDataSourcePrefetching {
         else { return }
         let cost = indexPaths.first?.row ?? 0
 
-        for indexPath in indexPaths {
-            if let metadata = self.dataSource.getMetadata(indexPath: indexPath),
-               metadata.isImageOrVideo,
-               self.imageCache.getImageCache(ocId: metadata.ocId, etag: metadata.etag, ext: ext) == nil,
-               let image = self.utility.getImage(ocId: metadata.ocId, etag: metadata.etag, ext: ext) {
+        DispatchQueue.global().async {
+            for indexPath in indexPaths {
+                if let metadata = self.dataSource.getMetadata(indexPath: indexPath),
+                   metadata.isImageOrVideo,
+                   self.imageCache.getImageCache(ocId: metadata.ocId, etag: metadata.etag, ext: ext) == nil,
+                   let image = self.utility.getImage(ocId: metadata.ocId, etag: metadata.etag, ext: ext) {
 
-                self.imageCache.addImageCache(ocId: metadata.ocId, etag: metadata.etag, image: image, ext: ext, cost: cost)
+                    self.imageCache.addImageCache(ocId: metadata.ocId, etag: metadata.etag, image: image, ext: ext, cost: cost)
+                }
             }
         }
     }
