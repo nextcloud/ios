f7c130d16ce296f3ba48e4e4423a3eb787c1db0c
Oleh Shcherba <o.shcherba@viseven.com>
2024-10-25 15:52:19 +0300

    TELKOMAUST-648: Change app name part of notification body to HiDrive Next in  NotificationService

--- Notification Service Extension/NotificationService.swift
+++ Notification Service Extension/NotificationService.swift
@@ -33,7 +33,7 @@ class NotificationService: UNNotificationServiceExtension {
 
         if let bestAttemptContent = bestAttemptContent {
             bestAttemptContent.title = ""
-            bestAttemptContent.body = "Nextcloud notification"
+            bestAttemptContent.body = "HiDrive Next notification"
             do {
                 if let message = bestAttemptContent.userInfo["subject"] as? String {
                     let tableAccounts = NCManageDatabase.shared.getAllAccount()
@@ -67,7 +67,7 @@ class NotificationService: UNNotificationServiceExtension {
         // Use this as an opportunity to deliver your "best attempt" at modified content, otherwise the original push payload will be used.
         if let contentHandler = contentHandler, let bestAttemptContent = bestAttemptContent {
             bestAttemptContent.title = ""
-            bestAttemptContent.body = "Nextcloud notification"
+            bestAttemptContent.body = "HiDrive Next notification"
             contentHandler(bestAttemptContent)
         }
     }
