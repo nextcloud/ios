8d7d00cd6bdc046895092b1afd0576257f9eb282
Marino Faggiana <marino@marinofaggiana.com>
2024-10-26 13:48:32 +0200

    cleaning
    Signed-off-by: Marino Faggiana <marino@marinofaggiana.com>

--- iOSClient/SceneDelegate.swift
+++ iOSClient/SceneDelegate.swift
@@ -120,12 +120,12 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
         hidePrivacyProtectionWindow()
 
-        DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
-            NCService().startRequestServicesServer(account: session.account, controller: controller)
-        }
-
         NCAutoUpload.shared.initAutoUpload(controller: nil, account: session.account) { num in
             NextcloudKit.shared.nkCommonInstance.writeLog("[INFO] Initialize Auto upload with \(num) uploads")
+        }
+
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+            NCService().startRequestServicesServer(account: session.account, controller: controller)
         }
 
         DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
