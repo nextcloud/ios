84f7922766404a75c3da28a037eb933babc65e83
Marino Faggiana <marino@marinofaggiana.com>
2024-11-12 09:59:43 +0100

    Version 6.1.6 (#3185)

--- iOSClient/Login/NCLoginProvider.swift
+++ iOSClient/Login/NCLoginProvider.swift
@@ -181,8 +181,14 @@ extension NCLoginProvider: WKNavigationDelegate {
                         controller.account = account
                         controller.modalPresentationStyle = .fullScreen
                         controller.view.alpha = 0
+
                         window?.rootViewController = controller
                         window?.makeKeyAndVisible()
+
+                        if let scene = window?.windowScene {
+                            SceneManager.shared.register(scene: scene, withRootViewController: controller)
+                        }
+
                         UIView.animate(withDuration: 0.5) {
                             controller.view.alpha = 1
                         }
