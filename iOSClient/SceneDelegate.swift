e53c6459322331dffe0f5fd3256fdc3d0c534dfb
Marino Faggiana <marino@marinofaggiana.com>
2024-11-18 11:26:27 +0100

    https://github.com/nextcloud/ios/issues/3197
    Signed-off-by: Marino Faggiana <marino@marinofaggiana.com>

--- iOSClient/SceneDelegate.swift
+++ iOSClient/SceneDelegate.swift
@@ -365,14 +365,16 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     }
 
     private func showPrivacyProtectionWindow() {
-        guard let windowScene = self.window?.windowScene else {
-            return
-        }
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) {
+            guard let windowScene = self.window?.windowScene else {
+                return
+            }
 
-        privacyProtectionWindow = UIWindow(windowScene: windowScene)
-        privacyProtectionWindow?.rootViewController = UIStoryboard(name: "LaunchScreen", bundle: nil).instantiateInitialViewController()
-        privacyProtectionWindow?.windowLevel = .alert + 1
-        privacyProtectionWindow?.makeKeyAndVisible()
+            self.privacyProtectionWindow = UIWindow(windowScene: windowScene)
+            self.privacyProtectionWindow?.rootViewController = UIStoryboard(name: "LaunchScreen", bundle: nil).instantiateInitialViewController()
+            self.privacyProtectionWindow?.windowLevel = .alert + 1
+            self.privacyProtectionWindow?.makeKeyAndVisible()
+        }
     }
 
     private func hidePrivacyProtectionWindow() {
