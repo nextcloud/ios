cf61d552469bbb3d0631261e7e0dcbe632a80ecb
Milen Pivchev <milen.pivchev@gmail.com>
2024-12-17 14:13:23 +0100

    WIP
    Signed-off-by: Milen Pivchev <milen.pivchev@gmail.com>

--- iOSClient/Login/Poll/NCLoginPollModel.swift
+++ iOSClient/Login/Poll/NCLoginPollModel.swift
@@ -9,37 +9,9 @@
 import Foundation
 
 class NCLoginPollModel: ObservableObject {
-
-//    var loginFlowV2Token = ""
-//    var loginFlowV2Endpoint = ""
-
-//    @Published var pollFinished = false
     @Published var isLoading = false
-//    @Published var account = ""
-
-//    var timer: DispatchSourceTimer?
-
-//    init(delegate: NCLoginPoll, pollFinished: Bool = false, isLoading: Bool = false, account: String = "", timer: DispatchSourceTimer? = nil) {
-//        self.delegate = delegate
-//        self.pollFinished = pollFinished
-//        self.isLoading = isLoading
-//        self.account = account
-//        self.timer = timer
-//    }
-
-    func configure() {
-//        self.loginFlowV2Token = loginFlowV2Token
-//        self.loginFlowV2Endpoint = loginFlowV2Endpoint
-//        self.loginFlowV2Login = loginFlowV2Login
-//
-//        poll()
-    }
 
     func openLoginInBrowser(loginFlowV2Login: String = "") {
         UIApplication.shared.open(URL(string: loginFlowV2Login)!)
     }
-}
-
-protocol NCLoginDelegate: AnyObject {
-    func pollDidStart()
 }
