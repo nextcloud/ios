8f151e43fa8b27c879eb6b63a7af05c58a32b5d3
Marino Faggiana <marino@marinofaggiana.com>
2024-12-19 17:28:59 +0100

    replace semaphore with InProgress variable
    Signed-off-by: Marino Faggiana <marino@marinofaggiana.com>

--- iOSClient/Media/NCMedia.swift
+++ iOSClient/Media/NCMedia.swift
@@ -36,9 +36,6 @@ class NCMedia: UIViewController {
     @IBOutlet weak var menuButton: UIButton!
     @IBOutlet weak var gradientView: UIView!
 
-    let semaphoreSearchMedia = DispatchSemaphore(value: 1)
-    let semaphoreNotificationCenter = DispatchSemaphore(value: 1)
-
     let layout = NCMediaLayout()
     var layoutType = NCGlobal.shared.mediaLayoutRatio
     var documentPickerViewController: NCDocumentPickerViewController?
@@ -55,7 +52,7 @@ class NCMedia: UIViewController {
     var fileSelect: [String] = []
     var filesExists: ThreadSafeArray<String> = ThreadSafeArray()
     var ocIdDoNotExists: ThreadSafeArray<String> = ThreadSafeArray()
-    var hasRunSearchMedia: Bool = false
+    var searchMediaInProgress: Bool = false
     var attributesZoomIn: UIMenuElement.Attributes = []
     var attributesZoomOut: UIMenuElement.Attributes = []
     let gradient: CAGradientLayer = CAGradientLayer()
@@ -260,25 +257,13 @@ class NCMedia: UIViewController {
             return
         }
 
-        // This is only a fail safe "dead lock", I don't think the timeout will ever be called but at least nothing gets stuck, if after 5 sec. (which is a long time in this routine), the semaphore is still locked
-        //
-        if self.semaphoreNotificationCenter.wait(timeout: .now() + 5) == .timedOut {
-            self.semaphoreNotificationCenter.signal()
-        }
-
         if error.errorCode == self.global.errorResourceNotFound,
            let ocId = userInfo["ocId"] as? String {
             self.database.deleteMetadataOcId(ocId)
-            self.loadDataSource {
-                self.semaphoreNotificationCenter.signal()
-            }
+            self.loadDataSource()
         } else if error != .success {
             NCContentPresenter().showError(error: error)
-            self.loadDataSource {
-                self.semaphoreNotificationCenter.signal()
-            }
-        } else {
-            semaphoreNotificationCenter.signal()
+            self.loadDataSource()
         }
     }
 
