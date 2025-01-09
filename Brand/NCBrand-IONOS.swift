6ee0f4fc3122470ad8936b01efe80c8a2fc6222c
Vitalii Tolkach <v.tolkach@viseven.com>
2025-01-09 12:19:04 +0100

    TELKOMAUST-838: reverted redundant delete

--- Brand/NCBrand-IONOS.swift
+++ Brand/NCBrand-IONOS.swift
@@ -16,6 +16,8 @@ import UIKit
         
         brand = "IONOS HiDrive Next"
         loginBaseUrl = "https://nextcloud-aio.iocaste45.de"
+        privacy = "https://nextcloud.com/privacy"
+        sourceCode = "https://github.com/nextcloud/ios"
 
         disable_intro = true
         disable_request_login_url = true
