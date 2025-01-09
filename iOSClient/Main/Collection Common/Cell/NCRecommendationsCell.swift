14accd18cbf49cc76108082f71a7ad134774363d
Marino Faggiana <marino@marinofaggiana.com>
2025-01-09 11:43:16 +0100

    cod
    Signed-off-by: Marino Faggiana <marino@marinofaggiana.com>

--- iOSClient/Main/Collection Common/Cell/NCRecommendationsCell.swift
+++ iOSClient/Main/Collection Common/Cell/NCRecommendationsCell.swift
@@ -12,7 +12,11 @@ class NCRecommendationsCell: UICollectionViewCell {
     @IBOutlet weak var image: UIImageView!
     @IBOutlet weak var labelFilename: UILabel!
     @IBOutlet weak var labelInfo: UILabel!
+    @IBOutlet weak var buttonMenu: UIButton!
 
     var metadata: tableMetadata = tableMetadata()
     var recommendedFiles: tableRecommendedFiles = tableRecommendedFiles()
+
+    @IBAction func touchUpInsideButtonMenu(_ sender: Any) {
+    }
 }
