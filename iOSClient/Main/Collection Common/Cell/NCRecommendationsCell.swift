04254b78a3cea308a25872f3bfd30ff4f9067ec0
Marino Faggiana <marino@marinofaggiana.com>
2025-01-15 12:10:56 +0100

    color
    Signed-off-by: Marino Faggiana <marino@marinofaggiana.com>

--- iOSClient/Main/Collection Common/Cell/NCRecommendationsCell.swift
+++ iOSClient/Main/Collection Common/Cell/NCRecommendationsCell.swift
@@ -35,17 +35,19 @@ class NCRecommendationsCell: UICollectionViewCell, UIGestureRecognizerDelegate {
     }
 
     func initCell() {
-        if metadata.hasPreview, metadata.classFile == NKCommon.TypeClassFile.document.rawValue {
-            let imageButton = UIImage(systemName: "ellipsis.circle.fill", withConfiguration: UIImage.SymbolConfiguration(pointSize: 15, weight: .thin))?.applyingSymbolConfiguration(UIImage.SymbolConfiguration(paletteColors: [.white, .black]))
-            buttonMenu.setImage(imageButton, for: .normal)
+        let imageButton = UIImage(systemName: "ellipsis.circle.fill", withConfiguration: UIImage.SymbolConfiguration(pointSize: 15, weight: .thin))?.applyingSymbolConfiguration(UIImage.SymbolConfiguration(paletteColors: [.black, .white]))
 
+        buttonMenu.setImage(imageButton, for: .normal)
+        buttonMenu.layer.shadowColor = UIColor.black.cgColor
+        buttonMenu.layer.shadowOpacity = 0.2
+        buttonMenu.layer.shadowOffset = CGSize(width: 2, height: 2)
+        buttonMenu.layer.shadowRadius = 4
+
+        if metadata.hasPreview, metadata.classFile == NKCommon.TypeClassFile.document.rawValue {
             image.layer.cornerRadius = 10
             image.layer.masksToBounds = true
             image.layer.borderWidth = 0.5
             image.layer.borderColor = UIColor.separator.cgColor
-        } else {
-            let imageButton = UIImage(systemName: "ellipsis.circle.fill", withConfiguration: UIImage.SymbolConfiguration(pointSize: 15, weight: .thin))?.applyingSymbolConfiguration(UIImage.SymbolConfiguration(paletteColors: [.black, .white]))
-            buttonMenu.setImage(imageButton, for: .normal)
         }
 
         let longPressedGesture = UILongPressGestureRecognizer(target: self, action: #selector(longPress(gestureRecognizer:)))
