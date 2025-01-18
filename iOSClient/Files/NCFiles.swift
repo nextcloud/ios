57bf310f7ff9e0c1ce327e9c265e7dce859e5f62
Marino Faggiana <marino@marinofaggiana.com>
2025-01-18 11:40:26 +0100

    cod
    Signed-off-by: Marino Faggiana <marino@marinofaggiana.com>

--- iOSClient/Files/NCFiles.swift
+++ iOSClient/Files/NCFiles.swift
@@ -348,44 +348,6 @@ class NCFiles: NCCollectionViewCommon {
         }
     }
 
-    func createRecommendations(session: NCSession.Session) async {
-        let home = self.utilityFileSystem.getHomeServer(session: session)
-        var recommendationsToInsert: [NKRecommendation] = []
-        let options = NKRequestOptions(queue: NextcloudKit.shared.nkCommonInstance.backgroundQueue)
-
-        let results = await NCNetworking.shared.getRecommendedFiles(account: session.account, options: options)
-        if results.error == .success,
-           let recommendations = results.recommendations {
-            for recommendation in recommendations {
-                var metadata = database.getResultMetadataFromFileId(recommendation.id)
-                var serverUrlFileName = ""
-
-                if metadata == nil || metadata?.fileName != recommendation.name {
-                    if recommendation.directory.last == "/" {
-                        serverUrlFileName = home + recommendation.directory + recommendation.name
-                    } else {
-                        serverUrlFileName = home + recommendation.directory + "/" + recommendation.name
-                    }
-                    let results = await NCNetworking.shared.readFileOrFolder(serverUrlFileName: serverUrlFileName, depth: "0", showHiddenFiles: NCKeychain().showHiddenFiles, account: session.account)
-
-                    if results.error == .success, let file = results.files?.first {
-                        let isDirectoryE2EE = self.utilityFileSystem.isDirectoryE2EE(file: file)
-                        let metadataConverted = self.database.convertFileToMetadata(file, isDirectoryE2EE: isDirectoryE2EE)
-                        metadata = metadataConverted
-
-                        self.database.addMetadata(metadataConverted)
-                        recommendationsToInsert.append(recommendation)
-                    }
-                } else {
-                    recommendationsToInsert.append(recommendation)
-                }
-            }
-            self.database.createRecommendedFiles(account: session.account, recommendations: recommendationsToInsert)
-        }
-
-        NotificationCenter.default.postOnMainThread(name: NCGlobal.shared.notificationCenterReloadRecommendedFiles, userInfo: nil)
-    }
-
     // MARK: - NCAccountSettingsModelDelegate
 
     override func accountSettingsDidDismiss(tableAccount: tableAccount?, controller: NCMainTabBarController?) {
--- iOSClient/Networking/NCNetworking+Recommendations.swift
+++ iOSClient/Networking/NCNetworking+Recommendations.swift
@@ -8,12 +8,15 @@ import Alamofire
 import NextcloudKit
 
 extension NCNetworking {
-    func createRecommendations(session: NCSession.Session) async {
-        let homeServer = self.utilityFileSystem.getHomeServer(session: session)
+    func createRecommendations(account: String) async {
+        guard let tblAccount = NCManageDatabase.shared.getTableAccount(account: account) else {
+            return
+        }
+        let homeServer = self.utilityFileSystem.getHomeServer(urlBase: tblAccount.urlBase, userId: tblAccount.userId)
         var recommendationsToInsert: [NKRecommendation] = []
         let options = NKRequestOptions(queue: NextcloudKit.shared.nkCommonInstance.backgroundQueue)
 
-        let results = await NCNetworking.shared.getRecommendedFiles(account: session.account, options: options)
+        let results = await NCNetworking.shared.getRecommendedFiles(account: account, options: options)
         if results.error == .success,
            let recommendations = results.recommendations {
             for recommendation in recommendations {
@@ -26,7 +29,7 @@ extension NCNetworking {
                     } else {
                         serverUrlFileName = homeServer + recommendation.directory + "/" + recommendation.name
                     }
-                    let results = await NCNetworking.shared.readFileOrFolder(serverUrlFileName: serverUrlFileName, depth: "0", showHiddenFiles: NCKeychain().showHiddenFiles, account: session.account)
+                    let results = await NCNetworking.shared.readFileOrFolder(serverUrlFileName: serverUrlFileName, depth: "0", showHiddenFiles: NCKeychain().showHiddenFiles, account: account)
 
                     if results.error == .success, let file = results.files?.first {
                         let isDirectoryE2EE = self.utilityFileSystem.isDirectoryE2EE(file: file)
@@ -40,7 +43,7 @@ extension NCNetworking {
                     recommendationsToInsert.append(recommendation)
                 }
             }
-            self.database.createRecommendedFiles(account: session.account, recommendations: recommendationsToInsert)
+            self.database.createRecommendedFiles(account: account, recommendations: recommendationsToInsert)
         }
 
         NotificationCenter.default.postOnMainThread(name: NCGlobal.shared.notificationCenterReloadRecommendedFiles, userInfo: nil)
