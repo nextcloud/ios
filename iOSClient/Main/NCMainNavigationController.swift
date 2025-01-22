fd8f0ab14fdd8b2cddcb761315f0c76f1c32fb27
Marino Faggiana <marino@marinofaggiana.com>
2025-01-22 12:42:13 +0100

    cleaninf
    Signed-off-by: Marino Faggiana <marino@marinofaggiana.com>

--- iOSClient/Main/NCMainNavigationController.swift
+++ iOSClient/Main/NCMainNavigationController.swift
@@ -10,10 +10,12 @@ class NCMainNavigationController: UINavigationController {
 
         self.timerProcess = Timer.scheduledTimer(withTimeInterval: 1, repeats: true, block: { _ in
             var color = NCBrandColor.shared.iconImageColor
+
             if let results = NCManageDatabase.shared.getResultsMetadatas(predicate: NSPredicate(format: "status != %i", NCGlobal.shared.metadataStatusNormal)),
                results.count > 0 {
-                color = .red
+                color = .systemBlue
             }
+
             for viewController in self.viewControllers {
                 if let rightBarButtonItems = viewController.navigationItem.rightBarButtonItems {
                     if let buttonTransfer = rightBarButtonItems.first(where: { $0.tag == 2 }) {
