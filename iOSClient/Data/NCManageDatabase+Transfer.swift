ea7823a76e178194bf3081611b1fd653fec3baa0
Marino Faggiana <marino@marinofaggiana.com>
2025-04-02 12:05:18 +0200

    TableTransfer
    Signed-off-by: Marino Faggiana <marino@marinofaggiana.com>

--- iOSClient/Data/NCManageDatabase+Transfer.swift
+++ iOSClient/Data/NCManageDatabase+Transfer.swift
@@ -25,11 +25,12 @@ class TableTransfer: Object {
     @Persisted var userId = ""
     @Persisted var serverUrl = ""
 
+    //@Persisted var serverUrlTo = ""
+
     @Persisted var sceneIdentifier: String?
     @Persisted var livePhotoFile: String?
     @Persisted var classFile = ""
     @Persisted var contentType = ""
-    @Persisted var iconName = ""
     @Persisted var size: Int64 = 0
 
     @Persisted var e2eEncrypted: Bool = false
@@ -67,133 +68,45 @@ class TableTransfer: Object {
 }
 
 extension NCManageDatabase {
-    func createTransferForAutoUpload(session: NCSession.Session,
-                                     serverUrl: String,
-                                     fileName: String,
-                                     livePhoto: Bool,
-                                     localIdentifier: String,
-                                     uploadSession: String,
-                                     sceneIdentifier: String?) -> TableTransfer? {
-        /// MOST COMPATIBLE SEARCH --> HEIC --> JPG
-        var fileNameFormatCompatibility = fileName
-        let ext = (fileNameFormatCompatibility as NSString).pathExtension.lowercased()
+    func autoUpload(session: NCSession.Session, serverUrl: String, fileName: String, livePhotoFile: Bool, localIdentifier: String, uploadSession: String, sceneIdentifier: String?) {
+        // MOST COMPATIBLE SEARCH --> HEIC --> JPG
+        var fileNameSearchMetadata = fileName
+        let ext = (fileNameSearchMetadata as NSString).pathExtension.lowercased()
         if ext == "heic", NCKeychain().formatCompatibility {
-            fileNameFormatCompatibility = (fileNameFormatCompatibility as NSString).deletingPathExtension + ".jpg"
+            fileNameSearchMetadata = (fileNameSearchMetadata as NSString).deletingPathExtension + ".jpg"
         }
-        let predicate = NSPredicate(format: "account == %@ AND serverUrl == %@ AND fileNameView == %@", session.account, serverUrl, fileNameFormatCompatibility)
 
+        let predicate = NSPredicate(format: "account == %@ AND serverUrl == %@ AND fileNameView == %@", session.account, serverUrl, fileNameSearchMetadata)
         do {
             let realm = try Realm()
             /// verify if already exists
             if realm.objects(TableTransfer.self).filter(predicate).first != nil {
-                return nil
+                return
             }
-
-            let result = TableTransfer()
-            result.account = session.account
-            result.fileName = fileName
-            result.fileNameView = fileName
-
-            result.serverUrl = serverUrl
-            result.urlBase = session.urlBase
-            result.user = session.user
-            result.userId = session.userId
-
-            if livePhoto {
-                result.livePhotoFile = (fileName as NSString).deletingPathExtension + ".mov"
-            }
-
-            let (_, classFile, iconName, _, _, _) = NextcloudKit.shared.nkCommonInstance.getInternalType(fileName: fileName, mimeType: "", directory: false, account: session.account)
-
-            result.classFile = classFile
-            result.iconName = iconName
-            result.assetLocalIdentifier = localIdentifier
-
-            result.session = uploadSession
-            result.sessionSelector = NCGlobal.shared.selectorUploadAutoUpload
-            result.sessionStatus = NCGlobal.shared.metadataStatusWaitUpload
-
-            result.sceneIdentifier = sceneIdentifier
-
-            return result
-        } catch let error as NSError {
-            NextcloudKit.shared.nkCommonInstance.writeLog("[ERROR] Could not write database: \(error)")
-        }
-
-        return nil
-    }
-
-    func createTransferForUpload(session: NCSession.Session,
-                                 serverUrl: String,
-                                 fileName: String,
-                                 livePhoto: Bool,
-                                 nativeFormat: Bool,
-                                 localIdentifier: String,
-                                 uploadSession: String,
-                                 sceneIdentifier: String?) -> TableTransfer? {
-        let predicate = NSPredicate(format: "account == %@ AND serverUrl == %@ AND fileNameView == %@", session.account, serverUrl, fileName)
-
-        do {
-            let realm = try Realm()
-            /// verify if already exists
-            if realm.objects(TableTransfer.self).filter(predicate).first != nil {
-                return nil
-            }
-
-            let result = TableTransfer()
-            result.account = session.account
-            result.fileName = fileName
-            result.fileNameView = fileName
-
-            result.serverUrl = serverUrl
-            result.urlBase = session.urlBase
-            result.user = session.user
-            result.userId = session.userId
-
-            if livePhoto {
-                result.livePhotoFile = (fileName as NSString).deletingPathExtension + ".mov"
-            }
-
-            let (_, classFile, iconName, _, _, _) = NextcloudKit.shared.nkCommonInstance.getInternalType(fileName: fileName, mimeType: "", directory: false, account: session.account)
-
-            result.classFile = classFile
-            result.iconName = iconName
-            result.nativeFormat = nativeFormat
-            result.assetLocalIdentifier = localIdentifier
-
-            result.session = uploadSession
-            result.sessionSelector = NCGlobal.shared.selectorUploadFile
-            result.sessionStatus = NCGlobal.shared.metadataStatusWaitUpload
-
-            result.sceneIdentifier = sceneIdentifier
-
-            return result
-        } catch let error as NSError {
-            NextcloudKit.shared.nkCommonInstance.writeLog("[ERROR] Could not write database: \(error)")
-        }
-
-        return nil
-    }
-
-    func createTransferProcessUploads(transfers: [TableTransfer], with verify: Bool = false, completion: @escaping (_ items: Int) -> Void = {_ in}) {
-        var counter: Int = 0
-        do {
-            let realm = try Realm()
             try realm.write {
-                for transfer in transfers {
-                    if verify {
-                        let predicate = NSPredicate(format: "account == %@ AND serverUrl == %@ AND fileNameView == %@", transfer.account, transfer.serverUrl, transfer.fileName)
-                        if realm.objects(TableTransfer.self).filter(predicate).first != nil {
-                            continue
-                        }
-                    }
-                    realm.create(TableTransfer.self, value: transfer, update: .all)
-                    counter += 1
+                let result = TableTransfer()
+                result.account = session.account
+                result.fileName = fileName
+                result.fileNameView = fileName
+
+                result.serverUrl = serverUrl
+                result.urlBase = session.urlBase
+                result.user = session.user
+                result.userId = session.userId
+
+                if livePhotoFile {
+                    result.livePhotoFile = (fileName as NSString).deletingPathExtension + ".mov"
                 }
+
+                result.assetLocalIdentifier = localIdentifier
+                result.session = uploadSession
+                result.sessionSelector = NCGlobal.shared.selectorUploadAutoUpload
+                result.sessionStatus = NCGlobal.shared.metadataStatusWaitUpload
+
+                result.sceneIdentifier = sceneIdentifier
             }
         } catch let error as NSError {
             NextcloudKit.shared.nkCommonInstance.writeLog("[ERROR] Could not write database: \(error)")
         }
-        completion(counter)
     }
 }
