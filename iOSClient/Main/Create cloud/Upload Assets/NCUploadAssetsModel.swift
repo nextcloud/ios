42b4f6897f955d39e5191a891368f7a0d2752ccb
Marino Faggiana <marino@marinofaggiana.com>
2025-04-30 14:20:49 +0200

    cod
    Signed-off-by: Marino Faggiana <marino@marinofaggiana.com>

--- iOSClient/Main/Create cloud/Upload Assets/NCUploadAssetsModel.swift
+++ iOSClient/Main/Create cloud/Upload Assets/NCUploadAssetsModel.swift
@@ -1,25 +1,6 @@
-//
-//  NCUploadAssetsModel.swift
-//  Nextcloud
-//
-//  Created by Marino Faggiana on 04/01/23.
-//  Copyright © 2023 Marino Faggiana. All rights reserved.
-//
-//  Author Marino Faggiana <marino.faggiana@nextcloud.com>
-//
-//  This program is free software: you can redistribute it and/or modify
-//  it under the terms of the GNU General Public License as published by
-//  the Free Software Foundation, either version 3 of the License, or
-//  (at your option) any later version.
-//
-//  This program is distributed in the hope that it will be useful,
-//  but WITHOUT ANY WARRANTY; without even the implied warranty of
-//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-//  GNU General Public License for more details.
-//
-//  You should have received a copy of the GNU General Public License
-//  along with this program.  If not, see <http://www.gnu.org/licenses/>.
-//
+// SPDX-FileCopyrightText: Nextcloud GmbH
+// SPDX-FileCopyrightText: 2023 Marino Faggiana
+// SPDX-License-Identifier: GPL-3.0-or-later
 
 import SwiftUI
 import NextcloudKit
@@ -92,14 +73,6 @@ class NCUploadAssetsModel: ObservableObject, NCCreateFormUploadConflictDelegate 
             return (metadata.fileNameView)
         } else {
             return (serverUrl as NSString).lastPathComponent
-        }
-    }
-
-    func getOriginalFilenameForPreview() -> NSString {
-        if let asset = assets.first?.phAsset {
-            return asset.originalFilename
-        } else {
-            return ""
         }
     }
 
@@ -208,7 +181,7 @@ class NCUploadAssetsModel: ObservableObject, NCCreateFormUploadConflictDelegate 
             let assetFileName = asset.originalFilename
             var livePhoto: Bool = false
             let creationDate = asset.creationDate ?? Date()
-            let ext = assetFileName.pathExtension.lowercased()
+            let ext = (assetFileName as NSString).pathExtension.lowercased()
             let fileName = previewStore.fileName.isEmpty ? utilityFileSystem.createFileName(assetFileName as String, fileDate: creationDate, fileType: asset.mediaType)
             : (previewStore.fileName + "." + ext)
 
