c90b08b63548d4d683ee9c0c9d21632513dee336
Marino Faggiana <marino@marinofaggiana.com>
2025-05-06 17:45:34 +0200

    TableGroupfolders
    Signed-off-by: Marino Faggiana <marino@marinofaggiana.com>

--- iOSClient/Data/NCManageDatabase+Groupfolders.swift
+++ iOSClient/Data/NCManageDatabase+Groupfolders.swift
@@ -1,25 +1,6 @@
-//
-//  NCManageDatabase+LayoutForView.swift
-//  Nextcloud
-//
-//  Created by Marino Faggiana on 28/11/22.
-//  Copyright © 2022 Marino Faggiana. All rights reserved.
-//
-//  Author Marino Faggiana <marino.faggiana@nextcloud.com>
-//
-//  This program is free software: you can redistribute it and/or modify
-//  it under the terms of the GNU General Public License as published by
-//  the Free Software Foundation, either version 3 of the License, or
-//  (at your option) any later version.
-//
-//  This program is distributed in the hope that it will be useful,
-//  but WITHOUT ANY WARRANTY; without even the implied warranty of
-//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-//  GNU General Public License for more details.
-//
-//  You should have received a copy of the GNU General Public License
-//  along with this program.  If not, see <http://www.gnu.org/licenses/>.
-//
+// SPDX-FileCopyrightText: Nextcloud GmbH
+// SPDX-FileCopyrightText: 2023 Marino Faggiana
+// SPDX-License-Identifier: GPL-3.0-or-later
 
 import Foundation
 import UIKit
@@ -52,35 +33,34 @@ class TableGroupfoldersGroups: Object {
 }
 
 extension NCManageDatabase {
+
+    // MARK: - Realm write
+
     func addGroupfolders(account: String, groupfolders: [NKGroupfolders]) {
-        do {
-            let realm = try Realm()
-            try realm.write {
+        performRealmWrite { realm in
+            let tableGroupfolders = realm.objects(TableGroupfolders.self).filter("account == %@", account)
+            realm.delete(tableGroupfolders)
 
-                let tableGroupfolders = realm.objects(TableGroupfolders.self).filter("account == %@", account)
-                realm.delete(tableGroupfolders)
+            let tableGroupfoldersGroups = realm.objects(TableGroupfoldersGroups.self).filter("account == %@", account)
+            realm.delete(tableGroupfoldersGroups)
 
-                let tableGroupfoldersGroups = realm.objects(TableGroupfoldersGroups.self).filter("account == %@", account)
-                realm.delete(tableGroupfoldersGroups)
+            for groupfolder in groupfolders {
+                let obj = TableGroupfolders()
+                obj.account = account
+                obj.acl = groupfolder.acl
 
-                for groupfolder in groupfolders {
-                    let obj = TableGroupfolders()
-                    obj.account = account
-                    obj.acl = groupfolder.acl
-                    for group in groupfolder.groups ?? [:] {
-                        let objGroups = TableGroupfoldersGroups(account: account, group: group.key, permission: (group.value as? Int ?? 0))
-                        obj.groups.append(objGroups)
-                    }
-                    obj.id = groupfolder.id
-                    obj.manage = groupfolder.manage
-                    obj.mountPoint = groupfolder.mountPoint
-                    obj.quota = groupfolder.quota
-                    obj.size = groupfolder.size
-                    realm.add(obj)
+                groupfolder.groups?.forEach { group in
+                    let objGroups = TableGroupfoldersGroups(account: account, group: group.key, permission: (group.value as? Int ?? 0))
+                    obj.groups.append(objGroups)
                 }
+
+                obj.id = groupfolder.id
+                obj.manage = groupfolder.manage
+                obj.mountPoint = groupfolder.mountPoint
+                obj.quota = groupfolder.quota
+                obj.size = groupfolder.size
+                realm.add(obj)
             }
-        } catch let error {
-            NextcloudKit.shared.nkCommonInstance.writeLog("[ERROR] Could not write to database: \(error)")
         }
     }
 }
