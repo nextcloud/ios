aace0902e5c455dca2a0e0940f4fe7f2daa3144b
Marino Faggiana <marino@marinofaggiana.com>
2025-05-09 13:28:48 +0200

    change name
    Signed-off-by: Marino Faggiana <marino@marinofaggiana.com>

--- iOSClient/Data/NCManageDatabase+AutoUpload.swift
+++ iOSClient/Data/NCManageDatabase+AutoUpload.swift
@@ -7,7 +7,7 @@ import UIKit
 import RealmSwift
 import NextcloudKit
 
-class tableAutoUpload: Object {
+class tableAutoUploadTransfer: Object {
     @Persisted(primaryKey: true) var primaryKey: String
     @Persisted var account: String
     @Persisted var serverUrl: String
@@ -31,23 +31,23 @@ extension NCManageDatabase {
 
     // MARK: - Realm Write
 
-    func addAutoUpload(account: String, serverUrl: String, fileName: String, assetLocalIdentifier: String, date: Date, sync: Bool = false) {
+    func addAutoUploadTransfer(account: String, serverUrl: String, fileName: String, assetLocalIdentifier: String, date: Date, sync: Bool = false) {
         performRealmWrite(sync: sync) { realm in
-            let newAutoUpload = tableAutoUpload(account: account, serverUrl: serverUrl, fileName: fileName, assetLocalIdentifier: assetLocalIdentifier, date: date)
+            let newAutoUpload = tableAutoUploadTransfer(account: account, serverUrl: serverUrl, fileName: fileName, assetLocalIdentifier: assetLocalIdentifier, date: date)
             realm.add(newAutoUpload, update: .all)
         }
     }
 
     // MARK: - Realm Read
 
-    func shouldSkipAutoUpload(account: String, serverUrl: String, fileName: String) -> Bool {
+    func shouldSkipAutoUploadTransfer(account: String, serverUrl: String, fileName: String) -> Bool {
         var shouldSkip = false
         performRealmRead { realm in
             if realm.objects(tableMetadata.self)
                 .filter("account == %@ AND serverUrl == %@ AND fileNameView == %@", account, serverUrl, fileName)
                 .first != nil {
                 shouldSkip = true
-            } else if realm.objects(tableAutoUpload.self)
+            } else if realm.objects(tableAutoUploadTransfer.self)
                         .filter("account == %@ AND serverUrl == %@ AND fileName == %@", account, serverUrl, fileName)
                         .first != nil {
                 shouldSkip = true
