151ff20f3a274038101165a076125b22e60b8446
Milen Pivchev <milen.pivchev@gmail.com>
2025-05-14 13:36:29 +0200

    Refactor
    Signed-off-by: Milen Pivchev <milen.pivchev@gmail.com>

--- iOSClient/Share/NCShareCommon.swift
+++ iOSClient/Share/NCShareCommon.swift
@@ -31,10 +31,10 @@ class NCShareCommon: NSObject {
     let SHARE_TYPE_LINK = 3
     let SHARE_TYPE_EMAIL = 4
     let SHARE_TYPE_CONTACT = 5
-    let SHARE_TYPE_REMOTE = 6
+    let SHARE_TYPE_FEDERATED = 6
     let SHARE_TYPE_CIRCLE = 7
     let SHARE_TYPE_GUEST = 8
-    let SHARE_TYPE_REMOTE_GROUP = 9
+    let SHARE_TYPE_FEDERATED_GROUP = 9
     let SHARE_TYPE_ROOM = 10
     // swiftlint:enable identifier_name
 
@@ -82,13 +82,13 @@ class NCShareCommon: NSObject {
             return UIImage(named: "shareTypeEmail")?.withTintColor(NCBrandColor.shared.textColor, renderingMode: .alwaysOriginal)
         case self.SHARE_TYPE_CONTACT:
             return UIImage(named: "shareTypeUser")?.withTintColor(NCBrandColor.shared.textColor, renderingMode: .alwaysOriginal)
-        case self.SHARE_TYPE_REMOTE:
+        case self.SHARE_TYPE_FEDERATED:
             return UIImage(named: "shareTypeUser")?.withTintColor(NCBrandColor.shared.textColor, renderingMode: .alwaysOriginal)
         case self.SHARE_TYPE_CIRCLE:
             return UIImage(named: "shareTypeCircles")?.withTintColor(NCBrandColor.shared.textColor, renderingMode: .alwaysOriginal)
         case self.SHARE_TYPE_GUEST:
             return UIImage(named: "shareTypeUser")?.withTintColor(NCBrandColor.shared.textColor, renderingMode: .alwaysOriginal)
-        case self.SHARE_TYPE_REMOTE_GROUP:
+        case self.SHARE_TYPE_FEDERATED_GROUP:
             return UIImage(named: "shareTypeGroup")?.withTintColor(NCBrandColor.shared.textColor, renderingMode: .alwaysOriginal)
         case self.SHARE_TYPE_ROOM:
             return UIImage(named: "shareTypeRoom")?.withTintColor(NCBrandColor.shared.textColor, renderingMode: .alwaysOriginal)
