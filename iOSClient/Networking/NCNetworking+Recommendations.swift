b55a62a6a56e35df377518748dc764927e61281b
Marino Faggiana <marino@marinofaggiana.com>
2025-05-27 16:04:39 +0200

    improvements
    Signed-off-by: Marino Faggiana <marino@marinofaggiana.com>

--- iOSClient/Networking/NCNetworking+Recommendations.swift
+++ iOSClient/Networking/NCNetworking+Recommendations.swift
@@ -15,26 +15,23 @@ extension NCNetworking {
 
         var recommendationsToInsert: [NKRecommendation] = []
         let options = NKRequestOptions(queue: NextcloudKit.shared.nkCommonInstance.backgroundQueue)
-        let showHiddenFiles = NCKeychain().getShowHiddenFiles(account: session.account)
-
         let results = await NextcloudKit.shared.getRecommendedFiles(account: session.account, options: options)
-        if results.error == .success,
-           let recommendations = results.recommendations {
-            for recommendation in recommendations {
-                var serverUrlFileName = ""
+        var serverUrlFileName = ""
 
+        if results.error == .success, let recommendations = results.recommendations {
+            for recommendation in recommendations {
                 if recommendation.directory.last == "/" {
                     serverUrlFileName = homeServer + recommendation.directory + recommendation.name
                 } else {
                     serverUrlFileName = homeServer + recommendation.directory + "/" + recommendation.name
                 }
 
-                let results = await NextcloudKit.shared.readFileOrFolder(serverUrlFileName: serverUrlFileName, depth: "0", showHiddenFiles: showHiddenFiles, account: session.account)
+                let result = await fileExists(serverUrlFileName: serverUrlFileName, account: session.account)
 
-                if results.error == .success, let file = results.files?.first {
+                if result.exists, let file = result.file {
                     let isDirectoryE2EE = self.utilityFileSystem.isDirectoryE2EE(file: file)
                     let metadata = self.database.convertFileToMetadata(file, isDirectoryE2EE: isDirectoryE2EE)
-                    self.database.addMetadata(metadata)
+                    self.database.addMetadata(metadata, sync: false)
 
                     if metadata.isLivePhoto, metadata.isVideo {
                         continue
@@ -43,7 +40,7 @@ extension NCNetworking {
                     }
                 }
             }
-            self.database.createRecommendedFiles(account: session.account, recommendations: recommendationsToInsert)
+            self.database.createRecommendedFiles(account: session.account, recommendations: recommendationsToInsert, sync: false)
 
             await collectionView.reloadData()
         }
