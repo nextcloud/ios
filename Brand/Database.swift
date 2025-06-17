2bb43a20c42c74cd55a6a2b981a5085b38377499
Marino Faggiana <marino@marinofaggiana.com>
2025-06-17 09:09:33 +0200

    build13
    Signed-off-by: Marino Faggiana <marino@marinofaggiana.com>

--- Brand/Database.swift
+++ Brand/Database.swift
@@ -8,4 +8,4 @@ import Foundation
 //
 let databaseName                    = "nextcloud.realm"
 let tableAccountBackup              = "tableAccountBackup.json"
-let databaseSchemaVersion: UInt64   = 386
+let databaseSchemaVersion: UInt64   = 387
