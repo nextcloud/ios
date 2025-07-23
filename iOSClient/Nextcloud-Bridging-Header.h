4696e548311deb21f0ce3bcb04807132b09e4a2b
Marino Faggiana <marino.faggiana@nextcloud.com>
2025-07-23 16:49:12 +0200

    cod
    Signed-off-by: Marino Faggiana <marino.faggiana@nextcloud.com>

--- iOSClient/Nextcloud-Bridging-Header.h
+++ iOSClient/Nextcloud-Bridging-Header.h
@@ -1,6 +1,6 @@
-//
-//  Use this file to import your target's public headers that you would like to expose to Swift.
-//
+// SPDX-FileCopyrightText: Nextcloud GmbH
+// SPDX-FileCopyrightText: 2025 Marino Faggiana
+// SPDX-License-Identifier: GPL-3.0-or-later
 
 #import "NCEndToEndEncryption.h"
 #import "NYMnemonic.h"
