b8e1a5162fba0fe8e0983a41ba7f72baf2e7542e
Marino Faggiana <marino.faggiana@nextcloud.com>
2025-08-14 16:50:24 +0200

    remove old code

--- Share/NCShareExtension+Files.swift
+++ Share/NCShareExtension+Files.swift
@@ -11,7 +11,7 @@ import NextcloudKit
 extension NCShareExtension {
     func reloadData() async {
         let session = self.extensionData.getSession()
-        let layoutForView = await NCManageDatabase.shared.getLayoutForViewAsync(account: session.account, key: keyLayout, serverUrl: serverUrl)
+        let layoutForView = NCManageDatabase.shared.getLayoutForView(account: session.account, key: keyLayout, serverUrl: serverUrl)
         let predicate = NSPredicate(format: "account == %@ AND serverUrl == %@ AND fileName != %@ AND directory == true", session.account, serverUrl, NextcloudKit.shared.nkCommonInstance.rootFileName)
         let metadatas = await self.database.getMetadatasAsync(predicate: predicate,
                                                               withLayout: layoutForView,
