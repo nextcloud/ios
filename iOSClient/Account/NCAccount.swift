c939b36357e06352f4a74d00e8319b6f1c22a774
Marino Faggiana <marino.faggiana@nextcloud.com>
2025-08-23 13:51:25 +0200

    Update NCAccount.swift

--- iOSClient/Account/NCAccount.swift
+++ iOSClient/Account/NCAccount.swift
@@ -91,6 +91,8 @@ class NCAccount: NSObject {
             if let userProfile {
                 await database.setAccountUserProfileAsync(account: account, userProfile: userProfile)
             }
+            // Networking Certificate
+            NCNetworking.shared.activeAccountCertificate(account: account)
             // Subscribing Push Notification
             appDelegate.subscribingPushNotification(account: tblAccount.account, urlBase: tblAccount.urlBase, user: tblAccount.user)
             // Start the service
@@ -107,6 +109,7 @@ class NCAccount: NSObject {
             nkLog(start: "Auto upload with \(num) photo")
             // Networking Process
             await NCNetworkingProcess.shared.setCurrentAccount(account)
+
             // Color
             NotificationCenter.default.postOnMainThread(name: self.global.notificationCenterChangeTheming, userInfo: ["account": account])
             // Notification
