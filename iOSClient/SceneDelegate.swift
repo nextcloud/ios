9afcc21e7973133004afb7ae9c3e75d7d4139844
Marino Faggiana <marino.faggiana@nextcloud.com>
2025-08-25 08:17:46 +0200

    Update SceneDelegate.swift

--- iOSClient/SceneDelegate.swift
+++ iOSClient/SceneDelegate.swift
@@ -18,8 +18,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     private let versionApp = NCUtility().getVersionApp(withBuild: false)
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        guard let windowScene = (scene as? UIWindowScene)
-        else {
+        guard let windowScene = (scene as? UIWindowScene) else {
             return
         }
 
