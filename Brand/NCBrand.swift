5408697cec7607de5e3f6ba695d4f5015eb4798d
Marino Faggiana <marino.faggiana@nextcloud.com>
2025-09-25 12:29:28 +0200

    50 task
    Signed-off-by: Marino Faggiana <marino.faggiana@nextcloud.com>

--- Brand/NCBrand.swift
+++ Brand/NCBrand.swift
@@ -76,7 +76,7 @@ final class NCBrandOptions: @unchecked Sendable {
     let httpMaximumConnectionsPerHostInUpload: Int = 8
 
     // Max request/download/upload process
-    let numMaximumProcess: Int = 100
+    let numMaximumProcess: Int = 50
 
     // Number of failed attempts after reset app
     let resetAppPasscodeAttempts: Int = 10
