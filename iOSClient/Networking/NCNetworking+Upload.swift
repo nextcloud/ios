6e1d76c575677577dde0fd13eacf074b2016f45a
Marino Faggiana <marino.faggiana@nextcloud.com>
2025-10-07 11:50:19 +0200

    test
    Signed-off-by: Marino Faggiana <marino.faggiana@nextcloud.com>

--- iOSClient/Networking/NCNetworking+Upload.swift
+++ iOSClient/Networking/NCNetworking+Upload.swift
@@ -202,9 +202,18 @@ extension NCNetworking {
 
     @discardableResult
     func uploadFileInBackground(metadata: tableMetadata,
+                                withFileExistsCheck: Bool = false,
                                 taskHandler: @escaping (_ task: URLSessionUploadTask?) -> Void = { _ in },
                                 start: @escaping () -> Void = { })
     async -> NKError {
+        if withFileExistsCheck {
+            let error = await self.fileExists(serverUrlFileName: metadata.serverUrlFileName, account: metadata.account)
+            if error == .success {
+                await uploadCancelFile(metadata: metadata)
+                return (.success)
+            }
+        }
+
         let fileNameLocalPath = utilityFileSystem.getDirectoryProviderStorageOcId(metadata.ocId, fileName: metadata.fileName, userId: metadata.userId, urlBase: metadata.urlBase)
 
         start()
