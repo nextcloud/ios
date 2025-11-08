7b6e7b17275100284c19410d616948a263695e7a
Marino Faggiana <marino.faggiana@nextcloud.com>
2025-11-08 08:37:41 +0100

    prj
    Signed-off-by: Marino Faggiana <marino.faggiana@nextcloud.com>

--- File Provider Extension/FileProviderDomain.swift
+++ File Provider Extension/FileProviderDomain.swift
@@ -77,17 +77,12 @@ class FileProviderDomain: NSObject {
     /// Compares registered domains with the actual accounts in the database, and removes the obsolete ones.
     func cleanOrphanedFileProviderDomains() async {
         do {
-            var validAccounts: [tableAccount] = []
-
             // Get all registered domains
             let registeredDomains = try await NSFileProviderManager.domains()
 
             // Get all valid accounts
-#if EXTENSION_FILE_PROVIDER_EXTENSION
-            validAccounts = await NCManageDatabaseFPE.shared.getAllTableAccountAsync()
-#else
-            validAccounts = await NCManageDatabase.shared.getAllTableAccountAsync()
-#endif
+            let validAccounts = await NCManageDatabase.shared.getAllTableAccountAsync()
+
             // Build the list of valid domain identifiers
             let validIdentifiers: Set<String> = Set(validAccounts.compactMap { tblAccount -> String? in
                 guard let host = NSURL(string: tblAccount.urlBase)?.host else {
