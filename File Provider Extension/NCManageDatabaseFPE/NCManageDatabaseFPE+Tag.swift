4777769049482f0e649beb9d504cb80e74a5ce2e
Marino Faggiana <marino.faggiana@nextcloud.com>
2025-11-08 08:18:26 +0100

    clean
    Signed-off-by: Marino Faggiana <marino.faggiana@nextcloud.com>

---
+++ File Provider Extension/NCManageDatabaseFPE/NCManageDatabaseFPE+Tag.swift
@@ -0,0 +1,56 @@
+// SPDX-FileCopyrightText: Nextcloud GmbH
+// SPDX-FileCopyrightText: 2025 Marino Faggiana
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+import Foundation
+import NextcloudKit
+import RealmSwift
+
+extension NCManageDatabaseFPE {
+    func addTagAsunc(_ ocId: String, tagIOS: Data?, account: String) async {
+        await core.performRealmWriteAsync { realm in
+            let addObject = tableTag()
+            addObject.account = account
+            addObject.ocId = ocId
+            addObject.tagIOS = tagIOS
+            realm.add(addObject, update: .all)
+        }
+    }
+
+    func getTagsAsync(predicate: NSPredicate) async -> [tableTag]? {
+        await core.performRealmReadAsync { realm in
+            let results = realm.objects(tableTag.self)
+                .filter(predicate)
+            return results.compactMap { tableTag(value: $0) }
+        }
+    }
+
+    func getTags(predicate: NSPredicate) -> [tableTag]? {
+        core.performRealmRead { realm in
+            let results = realm.objects(tableTag.self)
+                .filter(predicate)
+            return results.compactMap { tableTag(value: $0) }
+        }
+    }
+
+    func getTagAsync(predicate: NSPredicate) async -> tableTag? {
+        await core.performRealmReadAsync { realm in
+            return realm.objects(tableTag.self)
+                .filter(predicate)
+                .first.map { tableTag(value: $0) }
+        }
+    }
+
+    func getTag(predicate: NSPredicate) -> tableTag? {
+        var tag: tableTag?
+
+        core.performRealmRead { realm in
+            tag = realm.objects(tableTag.self)
+                .filter(predicate)
+                .first.map {
+                    tableTag(value: $0)
+                }
+        }
+        return tag
+    }
+}
