b5be688a8c3833d2aafb93d6abb715aba29f7f1f
Marino Faggiana <marino.faggiana@nextcloud.com>
2025-11-13 11:48:57 +0100

    License
    Signed-off-by: Marino Faggiana <marino.faggiana@nextcloud.com>

--- iOSClient/Viewer/NCViewerQuickLook/NCViewerQuickLook.swift
+++ iOSClient/Viewer/NCViewerQuickLook/NCViewerQuickLook.swift
@@ -1,28 +1,6 @@
-//
-//  NCViewerQuickLook.swift
-//  Nextcloud
-//
-//  Created by Marino Faggiana on 03/05/2020.
-//  Copyright © 2020 Marino Faggiana. All rights reserved.
-//  Copyright © 2022 Henrik Storch. All rights reserved.
-//  Copyright © 2023 Marino Faggiana. All rights reserved.
-//
-//  Author Marino Faggiana <marino.faggiana@nextcloud.com>
-//  Author Henrik Storch <henrik.storch@nextcloud.com>
-//
-//  This program is free software: you can redistribute it and/or modify
-//  it under the terms of the GNU General Public License as published by
-//  the Free Software Foundation, either version 3 of the License, or
-//  (at your option) any later version.
-//
-//  This program is distributed in the hope that it will be useful,
-//  but WITHOUT ANY WARRANTY; without even the implied warranty of
-//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-//  GNU General Public License for more details.
-//
-//  You should have received a copy of the GNU General Public License
-//  along with this program.  If not, see <http://www.gnu.org/licenses/>.
-//
+// SPDX-FileCopyrightText: Nextcloud GmbH
+// SPDX-FileCopyrightText: 2020 Marino Faggiana, 2022 Henrik Storch, 2023 Marino Faggiana
+// SPDX-License-Identifier: GPL-3.0-or-later
 
 import UIKit
 import QuickLook
