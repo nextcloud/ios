4f4051ca3a31a74e2770f448067a540ecd3679a7
Marino Faggiana <marino.faggiana@nextcloud.com>
2026-01-16 15:20:38 +0100

    showMessageBanner
    Signed-off-by: Marino Faggiana <marino.faggiana@nextcloud.com>

--- iOSClient/Networking/NCNetworking+NextcloudKitDelegate.swift
+++ iOSClient/Networking/NCNetworking+NextcloudKitDelegate.swift
@@ -20,11 +20,11 @@ extension NCNetworking {
                     await showMessageBanner(scene: scene,
                                             title: NSLocalizedString("_info_", comment: ""),
                                             subtitle: NSLocalizedString("_network_not_available_", comment: ""),
-                                            textColor: NCBrandColor.shared.textColor,
-                                            image: "wifi.circle",
-                                            imageAnimation: .variableColor,
+                                            textColor: .white,
+                                            image: "wifi.exclamationmark.circle",
+                                            imageAnimation: .bounce,
                                             imageColor: .white,
-                                            backgroundColor: UIColor.systemBlue.withAlphaComponent(0.75))
+                                            backgroundColor: UIColor.lightGray.withAlphaComponent(0.75))
                 }
             }
             lastReachability = false
