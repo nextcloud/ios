745bc2a7c5bdc691aa4861a41c0f6568e850a09b
Marino Faggiana <marino.faggiana@nextcloud.com>
2026-02-27 14:53:44 +0100

    cleaning
    Signed-off-by: Marino Faggiana <marino.faggiana@nextcloud.com>

--- iOSClient/GUI/Lucid Banner/HelperBanner.swift
+++ iOSClient/GUI/Lucid Banner/HelperBanner.swift
@@ -122,3 +122,42 @@ func horizontalLayoutBanner(bounds: CGRect,
         return .stretch(margins: phoneSideMargin)
     }
 }
+
+#if !EXTENSION
+
+// Error 401 (maintenance mode)
+// Error 423 (locked)
+// Error 507 (insufficient storage)
+// Error -1009 (NSURLErrorNotConnectedToInternet)
+// Error -1003 (NSURLError​Cannot​Find​Host)
+
+func bannerContainsError(errorCode: Int?, afError: AFError? = nil) -> Bool {
+    guard let errorCode else {
+        return false
+    }
+    // List of errors not to be displayed
+    if errorCode == -999 || errorCode == 423 {
+        return true
+    }
+    if let afError, case .explicitlyCancelled = afError {
+        return true
+    }
+    // Prevent repeated display of the same user-facing error during the current foreground session.
+    // If this error code has already been shown, do nothing.
+    // Otherwise, record it and allow the UX notification to be displayed once.
+    if shownErrors.contains(errorCode) {
+        return true
+    } else {
+        // Coalesce user-facing errors across the current foreground session.
+        // The same error code is shown to the user only once.
+        if errorCode == 401 ||
+            errorCode == 423 ||
+            errorCode == 507 ||
+            errorCode == NSURLErrorNotConnectedToInternet ||
+            errorCode == NSURLErrorCannotFindHost {
+            shownErrors.insert(errorCode)
+        }
+        return false
+    }
+}
+#endif
