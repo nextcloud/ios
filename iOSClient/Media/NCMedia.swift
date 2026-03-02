035f26a56773a4867d0c9e26162add3f15ed9727
Marino Faggiana <marino@marinofaggiana.com>
2026-03-02 10:08:15 +0100

    Improved Recent view (#4014)
    * code
    
    Signed-off-by: Marino Faggiana <marino.faggiana@nextcloud.com>
    
    * code
    
    Signed-off-by: Marino Faggiana <marino.faggiana@nextcloud.com>
    
    * fix
    
    Signed-off-by: Marino Faggiana <marino.faggiana@nextcloud.com>
    
    * code
    
    Signed-off-by: Marino Faggiana <marino.faggiana@nextcloud.com>
    
    * clean
    
    Signed-off-by: Marino Faggiana <marino.faggiana@nextcloud.com>
    
    * build 3
    
    Signed-off-by: Marino Faggiana <marino.faggiana@nextcloud.com>
    
    ---------
    
    Signed-off-by: Marino Faggiana <marino.faggiana@nextcloud.com>

--- iOSClient/Media/NCMedia.swift
+++ iOSClient/Media/NCMedia.swift
@@ -100,7 +100,7 @@ class NCMedia: UIViewController {
 
         layout.sectionInset = UIEdgeInsets(top: 0, left: 2, bottom: 0, right: 2)
         collectionView.collectionViewLayout = layout
-        layoutType = database.getLayoutForView(account: session.account, key: global.layoutViewMedia, serverUrl: "", layout: global.mediaLayoutRatio).layout
+        layoutType = database.getLayoutForView(account: session.account, key: global.layoutViewMedia, serverUrl: "", layoutType: global.mediaLayoutRatio).layout
 
         // Gradient Layer
         gradientLayer.startPoint = CGPoint(x: 0, y: 0)
