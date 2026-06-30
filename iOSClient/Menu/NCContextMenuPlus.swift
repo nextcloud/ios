5fb65c6c0711471dda46b729d5a882dad5def70a
Marino Faggiana <marino.faggiana@nextcloud.com>
2026-06-30 08:33:30 +0200

    new plus menu
    Signed-off-by: Marino Faggiana <marino.faggiana@nextcloud.com>

--- iOSClient/Menu/NCContextMenuPlus.swift
+++ iOSClient/Menu/NCContextMenuPlus.swift
@@ -54,15 +54,18 @@ class NCContextMenuPlus: NSObject {
         let isNetworkReachable = NextcloudKit.shared.isNetworkReachable()
         let titleCreateFolder = isDirectoryE2EE ? NSLocalizedString("_create_folder_e2ee_", comment: "") : NSLocalizedString("_create_folder_", comment: "")
         let imageCreateFolder = isDirectoryE2EE ? NCImageCache.shared.getFolderEncrypted(account: session.account) : NCImageCache.shared.getFolder(account: session.account)
+        let creatorsByEditor = Dictionary(grouping: capabilities.directEditingCreators, by: \.editor)
 
         var menuActionElements: [UIMenuElement] = []
         var menuFolderElements: [UIMenuElement] = []
+        var menuFolderInfoElements: [UIMenuElement] = []
         var menuTextElements: [UIMenuElement] = []
-        var menuDirectEditingElements: [UIMenuElement] = []
         var menuRichDocumentElements: [UIMenuElement] = []
+        var menuDirectEditingTextElements: [UIMenuElement] = []
+        var menuDirectEditingOthersElements: [UIMenuElement] = []
 
-        // ------------------------------- ACTION
-
+        // ACTION
+        //
         menuActionElements.append(UIAction(title: NSLocalizedString("_upload_photos_videos_", comment: ""),
                                            image: utility.loadImage(named: "photo", colors: [NCBrandColor.shared.iconImageColor])) { _ in
             NCAskAuthorization().askAuthorizationPhotoLibrary(controller: controller) { hasPermission in
@@ -124,8 +127,8 @@ class NCContextMenuPlus: NSObject {
             }
         })
 
-        // ------------------------------- E2EE
-
+        // E2EE
+        //
         if serverUrl == utilityFileSystem.getHomeServer(session: session),
            NCPreferences().isEndToEndEnabled(account: session.account),
            isNetworkReachable {
@@ -151,52 +154,89 @@ class NCContextMenuPlus: NSObject {
             })
         }
 
-        // ------------------------------- RICHDOCUMENT TEXT
+        // FOLDER INFO
+        //
+        if NCBrandOptions.shared.isServerVersion(capabilities, greaterOrEqualTo: .v33) {
+            // USE DIRECT EDITING
+            if let textCreators = creatorsByEditor["text"],
+               !textCreators.isEmpty,
+               directory?.richWorkspace == nil,
+               !isDirectoryE2EE,
+               isNetworkReachable {
+                menuFolderInfoElements.append(
+                    UIAction(
+                        title: NSLocalizedString("_add_folder_info_", comment: ""),
+                        image: utility.loadImage(named: "list.dash.header.rectangle", colors: [NCBrandColor.shared.iconImageColor])) { _ in
+                            Task { @MainActor in
+                                let createDocument = NCCreate()
+                                let fileName = await NCNetworking.shared.createFileName(
+                                    fileNameBase: NCGlobal.shared.fileNameRichWorkspace,
+                                    account: session.account,
+                                    serverUrl: serverUrl
+                                )
+                                let fileNamePath = utilityFileSystem.getRelativeFilePath(
+                                    String(describing: fileName),
+                                    serverUrl: serverUrl,
+                                    session: session)
 
-        if NCBrandOptions.shared.isServerVersion(capabilities, greaterOrEqualTo: .v18),
-           directory?.richWorkspace == nil,
-           !isDirectoryE2EE,
-           isNetworkReachable {
-            menuTextElements.append(UIAction(title: NSLocalizedString("_add_folder_info_", comment: ""),
-                                             image: utility.loadImage(named: "list.dash.header.rectangle", colors: [NCBrandColor.shared.iconImageColor])) { _ in
-                Task { @MainActor in
-                    let richWorkspaceCommon = NCRichWorkspaceCommon()
-                    if let viewController = controller.currentViewController() {
-                        if await NCManageDatabase.shared.getMetadataAsync(
-                            predicate: NSPredicate(format: "account == %@ AND serverUrl == %@ AND fileNameView LIKE[c] %@",
-                                                   session.account,
-                                                   serverUrl,
-                                                   NCGlobal.shared.fileNameRichWorkspace.lowercased())) == nil {
-                            richWorkspaceCommon.createViewerNextcloudText(serverUrl: serverUrl, viewController: viewController, controller: controller, session: session)
-                        } else {
-                            richWorkspaceCommon.openViewerNextcloudText(serverUrl: serverUrl, viewController: viewController, controller: controller, session: session)
+                                await createDocument.createDocument(
+                                    controller: controller,
+                                    fileNamePath: fileNamePath,
+                                    fileName: String(describing: fileName),
+                                    editorId: "text",
+                                    creatorId: "textdocument",
+                                    templateId: "",
+                                    account: session.account)
+                            }
+                })
+            }
+        } else {
+            // USE RICHDOCUMENT TEXT
+            if NCBrandOptions.shared.isServerVersion(capabilities, greaterOrEqualTo: .v18),
+               directory?.richWorkspace == nil,
+               !isDirectoryE2EE,
+               isNetworkReachable {
+                menuFolderInfoElements.append(UIAction(title: NSLocalizedString("_add_folder_info_", comment: ""),
+                                                       image: utility.loadImage(named: "list.dash.header.rectangle", colors: [NCBrandColor.shared.iconImageColor])) { _ in
+                    Task { @MainActor in
+                        let richWorkspaceCommon = NCRichWorkspaceCommon()
+                        if let viewController = controller.currentViewController() {
+                            if await NCManageDatabase.shared.getMetadataAsync(
+                                predicate: NSPredicate(format: "account == %@ AND serverUrl == %@ AND fileNameView LIKE[c] %@",
+                                                       session.account,
+                                                       serverUrl,
+                                                       NCGlobal.shared.fileNameRichWorkspace.lowercased())) == nil {
+                                richWorkspaceCommon.createViewerNextcloudText(serverUrl: serverUrl, viewController: viewController, controller: controller, session: session)
+                            } else {
+                                richWorkspaceCommon.openViewerNextcloudText(serverUrl: serverUrl, viewController: viewController, controller: controller, session: session)
+                            }
                         }
                     }
-                }
-            })
+                })
+            }
+
+            if isNetworkReachable,
+               let creator = capabilities.directEditingCreators.first(where: { $0.editor == "text" }),
+               !isDirectoryE2EE {
+                menuTextElements.append(UIAction(title: NSLocalizedString("_create_nextcloudtext_document_", comment: ""),
+                                                 image: utility.loadImage(named: "doc.text", colors: [NCBrandColor.shared.iconImageColor])) { _ in
+                    Task {
+                        let fileName = await NCNetworking.shared.createFileName(fileNameBase: NSLocalizedString("_untitled_", comment: "") + "." + creator.ext, account: session.account, serverUrl: serverUrl)
+                        let fileNamePath = utilityFileSystem.getRelativeFilePath(String(describing: fileName), serverUrl: serverUrl, session: session)
+
+                        await NCCreate().createDocument(controller: controller, fileNamePath: fileNamePath, fileName: String(describing: fileName), editorId: "text", creatorId: creator.identifier, templateId: "document", account: session.account)
+                    }
+                })
+            }
         }
 
-        if isNetworkReachable,
-           let creator = capabilities.directEditingCreators.first(where: { $0.editor == "text" }),
-           !isDirectoryE2EE {
-            menuTextElements.append(UIAction(title: NSLocalizedString("_create_nextcloudtext_document_", comment: ""),
-                                             image: utility.loadImage(named: "doc.text", colors: [NCBrandColor.shared.iconImageColor])) { _ in
-                Task {
-                    let fileName = await NCNetworking.shared.createFileName(fileNameBase: NSLocalizedString("_untitled_", comment: "") + "." + creator.ext, account: session.account, serverUrl: serverUrl)
-                    let fileNamePath = utilityFileSystem.getRelativeFilePath(String(describing: fileName), serverUrl: serverUrl, session: session)
-
-                    await NCCreate().createDocument(controller: controller, fileNamePath: fileNamePath, fileName: String(describing: fileName), editorId: "text", creatorId: creator.identifier, templateId: "document", account: session.account)
-                }
-            })
-        }
-
-        // ------------------------------- WEB EDITORS
-
+        // OFFICE
+        //
         if isNetworkReachable,
            !isDirectoryE2EE {
-
-            // ------------------------------- COLLABORA
             if capabilities.richDocumentsEnabled {
+                // COLLABORA
+                //
                 menuRichDocumentElements.append(UIAction(title: NSLocalizedString("_create_new_document_", comment: ""),
                                                         image: utility.loadImage(named: "doc.richtext", colors: [NCBrandColor.shared.documentIconColor])) { _ in
                     Task { @MainActor in
@@ -234,12 +274,13 @@ class NCContextMenuPlus: NSObject {
                 })
             }
 
-            // ------------------------------- DIRECT EDITING CREATORS (onlyoffice, eurooffice, …)
-
-            let creatorsByEditor = Dictionary(grouping: capabilities.directEditingCreators, by: \.editor)
+            // DIRECT EDITING (eurooffice, onlyoffice)
+            //
             for editorId in creatorsByEditor.keys.sorted() {
                 guard NCDirectEditorAdapter.resolve(from: [editorId]) != nil,
-                      editorId != "text" else { continue }
+                      editorId != "text" else {
+                    continue
+                }
 
                 let sortedCreators = creatorsByEditor[editorId]!
                     .compactMap { creator -> (NKEditorDetailsCreator, CreatorMenuInfo)? in
@@ -272,31 +313,110 @@ class NCContextMenuPlus: NSObject {
                     }
                 }
 
-                menuDirectEditingElements.append(contentsOf: editorActions)
+                menuDirectEditingTextElements.append(contentsOf: editorActions)
+            }
+
+            // DIRECT EDITING OTHERS
+            //
+            let filteredCreatorsByEditor = creatorsByEditor.filter {
+                $0.key != "eurooffice"
+            }
+            let creators = filteredCreatorsByEditor.values.flatMap { $0 }
+            let sortedCreators = creators.sorted {
+                $0.name < $1.name
+            }
+
+            for creator in sortedCreators {
+                let image: UIImage?
+                switch creator.ext {
+                    case "md":
+                        image = UIImage(systemName: "text.document")
+                    case "whiteboard":
+                        image = UIImage(systemName: "pencil.and.scribble")
+                    default:
+                        image = UIImage(systemName: "doc")
+                    }
+
+                let action = UIAction(
+                    title: creator.name,
+                    image: image
+                ) { _ in
+                    Task { @MainActor in
+                        let createDocument = NCCreate()
+                        let fileName = await NCNetworking.shared.createFileName(
+                            fileNameBase: NSLocalizedString("_untitled_", comment: "") + "." + creator.ext,
+                            account: session.account,
+                            serverUrl: serverUrl
+                        )
+                        let fileNamePath = utilityFileSystem.getRelativeFilePath(
+                            String(describing: fileName),
+                            serverUrl: serverUrl,
+                            session: session)
+
+                        await createDocument.createDocument(
+                            controller: controller,
+                            fileNamePath: fileNamePath,
+                            fileName: String(describing: fileName),
+                            editorId: creator.editor,
+                            creatorId: creator.identifier,
+                            templateId: "",
+                            account: session.account)
+                    }
+                }
+
+                menuDirectEditingOthersElements.append(action)
             }
         }
 
+        // ACTIONS
         let menuAction = UIMenu(title: "", options: .displayInline, children: menuActionElements)
+
+        // FOLDER INFO
+        let menuFolderInfo = UIMenu(title: "", options: .displayInline, children: menuFolderInfoElements)
+
         // FOLDER
         let menuFolder = UIMenu(title: "", options: .displayInline, children: menuFolderElements)
         if menuFolderElements.count > 1 {
             menuFolder.preferredElementSize = .medium
         }
-        // TEXT
+
+        // NEXTCLOUD TEXT
         let menuText = UIMenu(title: "", options: .displayInline, children: menuTextElements)
         if menuTextElements.count > 1 {
             menuText.preferredElementSize = .medium
         }
-        // EURO OFFICE
-        let menuDirectEditing = UIMenu(title: "", options: .displayInline, children: menuDirectEditingElements)
-        menuDirectEditing.preferredElementSize = .medium
+
+        // EUROOFFICE - ONLYOFFICE
+        var menuOffice: UIMenu?
+        if menuDirectEditingTextElements.count > 0 || menuDirectEditingOthersElements.count > 0 {
+            // OFFICE
+            let menuDirectEditingOffice = UIMenu(title: "", options: .displayInline, children: menuDirectEditingTextElements)
+            menuDirectEditingOffice.preferredElementSize = .medium
+            // OTHER
+            let menuDirectEditingOthers = UIMenu(title: "", options: .displayInline, children: menuDirectEditingOthersElements)
+            menuDirectEditingOthers.preferredElementSize = .medium
+
+            menuOffice = UIMenu(
+                title: "Office",
+                image: UIImage(systemName: "doc.richtext"),
+                children: [menuDirectEditingOthers, menuDirectEditingOffice]
+            )
+        }
+
         // COLLABORA
-        let menuRichDocument = UIMenu(title: "", options: .displayInline, children: menuRichDocumentElements)
-        menuRichDocument.preferredElementSize = .medium
+        let menuRichDocumentOffice = UIMenu(title: "", options: .displayInline, children: menuRichDocumentElements)
+        menuRichDocumentOffice.preferredElementSize = .medium
 
         // MENU PLUS
-        let plusMenu = UIMenu(children: [menuAction, menuFolder, menuText, menuRichDocument, menuDirectEditing])
+        var plusMenuElements: [UIMenuElement] = [menuAction, menuFolderInfo]
+        if let menuOffice {
+            plusMenuElements.append(menuOffice)
+        }
+        plusMenuElements.append(contentsOf: [menuFolder, menuText, menuRichDocumentOffice])
 
+        let plusMenu = UIMenu(children: plusMenuElements)
+
+        // TOOLBAR
         let config = UIImage.SymbolConfiguration(pointSize: 25, weight: .thin)
         let plusImage = UIImage(systemName: "plus.circle.fill", withConfiguration: config)
 
