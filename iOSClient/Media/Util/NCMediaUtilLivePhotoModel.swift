af2fd354c33628f1a8aeeae3fe35c9fedaf1c4ce
Marino Faggiana <marino.faggiana@nextcloud.com>
2026-07-17 09:04:26 +0200

    chore: scaffold Live Photo media utilities
    Signed-off-by: Marino Faggiana <marino.faggiana@nextcloud.com>

---
+++ iOSClient/Media/Util/NCMediaUtilLivePhotoModel.swift
@@ -0,0 +1,8 @@
+//
+//  NCMediaUtilLivePhotoModel.swift
+//  Nextcloud
+//
+//  Created by Marino Faggiana on 17/07/2026.
+//  Copyright © 2026 Marino Faggiana. All rights reserved.
+//
+
---
+++ iOSClient/Media/Util/NCMediaUtilLivePhotoView.swift
@@ -0,0 +1,8 @@
+//
+//  NCMediaUtilLivePhotoView.swift
+//  Nextcloud
+//
+//  Created by Marino Faggiana on 17/07/2026.
+//  Copyright © 2026 Marino Faggiana. All rights reserved.
+//
+
