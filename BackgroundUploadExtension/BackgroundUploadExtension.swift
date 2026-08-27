f2af31ec7e33fbb4bdb1f2b618d28ce20fdfc610
Marino Faggiana <marino.faggiana@nextcloud.com>
2026-08-27 11:35:36 +0200

    test
    Signed-off-by: Marino Faggiana <marino.faggiana@nextcloud.com>

--- BackgroundUploadExtension/BackgroundUploadExtension.swift
+++ BackgroundUploadExtension/BackgroundUploadExtension.swift
@@ -10,17 +10,49 @@ final class BackgroundUploadExtension: PHBackgroundResourceUploadJobExtension {
         category: "TEST"
     )
 
+    private static var processCount = 0
+
     required init() {
-        Self.logger.error("🔥🔥🔥 BGUPLOAD INIT 🔥🔥🔥")
+        Self.logger.error("""
+        🔥🔥🔥 BGUPLOAD INIT 🔥🔥🔥
+        Bundle: \(Bundle.main.bundleIdentifier ?? "nil")
+        Time: \(Date().formatted())
+        """)
     }
 
     func processJobs() async -> PHBackgroundResourceUploadProcessingResult {
-        Self.logger.error("🔥🔥🔥 BGUPLOAD processJobs() 🔥🔥🔥")
+        Self.processCount += 1
+
+        let options = PHFetchOptions()
+        options.sortDescriptors = [
+            NSSortDescriptor(key: "creationDate", ascending: false)
+        ]
+        options.fetchLimit = 1
+
+        let assets = PHAsset.fetchAssets(with: .image, options: options)
+
+        Self.logger.error("""
+        🔥 BGUPLOAD processJobs()
+
+        Invocation : \(Self.processCount)
+        Images     : \(assets.count)
+        """)
+
+        if let asset = assets.firstObject {
+            Self.logger.error("""
+            Latest asset:
+            id   : \(asset.localIdentifier, privacy: .public)
+            date : \(String(describing: asset.creationDate), privacy: .public)
+            """)
+        }
 
         return .completed
     }
 
     func willTerminate() async {
-        Self.logger.error("🔥🔥🔥 BGUPLOAD willTerminate() 🔥🔥🔥")
+        Self.logger.error("""
+        🔥🔥🔥 BGUPLOAD willTerminate() 🔥🔥🔥
+        Time: \(Date().formatted())
+        """)
     }
 }
