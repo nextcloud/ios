e325cd30f51e110be57bb5ac823e7a6d9ece529a
Milen Pivchev <milen.pivchev@gmail.com>
2026-09-22 13:15:01 +0200

    WIP
    Signed-off-by: Milen Pivchev <milen.pivchev@gmail.com>

--- Brand/NCBrand.swift
+++ Brand/NCBrand.swift
@@ -64,7 +64,7 @@ final class NCBrandOptions: @unchecked Sendable {
     var brandUserAgent: String = ""
     var textCopyrightNextcloudiOS: String = "Nextcloud Matheria for iOS %@ © 2026"
     var textCopyrightNextcloudServer: String = "Nextcloud Server %@"
-    var loginBaseUrl: String = ""
+    var loginBaseUrl: String = "cloud.nextcloud.com"
     var pushNotificationServerProxy: String = ""
     var linkLoginHost: String = "https://nextcloud.com/install"
     var linkloginPreferredProviders: String = "https://nextcloud.com/signup-ios"
@@ -73,6 +73,8 @@ final class NCBrandOptions: @unchecked Sendable {
     var sourceCode: String = "https://github.com/nextcloud/ios"
     var mobileconfig: String = "/remote.php/dav/provisioning/apple-provisioning.mobileconfig"
     var appStoreUrl: String = "https://apps.apple.com/in/app/nextcloud/id1125420102"
+
+    var doNotAskPasscodeAtStartup: Bool = false
 
     // Auto Upload default folder
     var folderDefaultAutoUpload: String = "Photos"
@@ -89,7 +91,7 @@ final class NCBrandOptions: @unchecked Sendable {
 
     // MDM settings
     var disable_intro: Bool = false
-    var disable_request_login_url: Bool = false
+    var disable_request_login_url: Bool = true
     var disable_multiaccount: Bool = false
     var disable_more_external_site: Bool = false
     var disable_openin_file: Bool = false
@@ -97,7 +99,6 @@ final class NCBrandOptions: @unchecked Sendable {
     var disable_log: Bool = false
     var disable_mobileconfig: Bool = false  
     var disable_show_more_nextcloud_apps_in_settings: Bool = false
-    var doNotAskPasscodeAtStartup: Bool = false
     var disable_source_code_in_settings: Bool = false
     var enforce_passcode_lock = false
     var enforce_privacyScreenEnabled = false
